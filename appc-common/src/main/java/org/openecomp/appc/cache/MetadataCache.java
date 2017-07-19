89a5c737d731a8ea70dd4238132cca60932b9d06
Patrick Brady <pb071s@att.com>
2017-07-19 12:09:28 -0700

    Applying license changes to all files
    [APPC-87] Fix Maven License Plugin
    
    Change-Id: I23a95b6c1020618d05f17c63c72152cf86e4d4a0
    Signed-off-by: Patrick Brady <pb071s@att.com>

--- appc-common/src/main/java/org/openecomp/appc/cache/MetadataCache.java
+++ appc-common/src/main/java/org/openecomp/appc/cache/MetadataCache.java
@@ -1,10 +1,11 @@
 /*-
  * ============LICENSE_START=======================================================
- * APPC
+ * ONAP : APPC
  * ================================================================================
  * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
- * Copyright (C) 2017 Amdocs
  * ================================================================================
+ * Copyright (C) 2017 Amdocs
+ * =============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -16,8 +17,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ============LICENSE_END=========================================================
+ * 
  * ECOMP is a trademark and service mark of AT&T Intellectual Property.
+ * ============LICENSE_END=========================================================
  */
 
 package org.openecomp.appc.cache;
