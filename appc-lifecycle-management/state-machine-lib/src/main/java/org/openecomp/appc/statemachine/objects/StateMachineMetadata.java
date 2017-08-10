fb1b96ebe2c4983147a85dec3f8e985d328b7f57
beili.zhou <beili.zhou@amdocs.com>
2017-08-10 14:54:42 -0400

    [APPC-44] APPC Lifecycle Management refactor
    Pull APPC lifecycle Management out of APPC dispatcher, so that it can be
    used by both APPC dispatcher and APPC OAM. This is the pre-step of
    introducing APPC OAM operations.
    
    Issue-Id: APPC-44
    Change-Id: Icbde399b5121fae9e1919cfdd5c77bbe55d61188
    Signed-off-by: beili.zhou <beili.zhou@amdocs.com>
    
    [APPC-44] update for review comments
    
    Update based on review comments, and added feature dependency on
    appc-common in APPC lifecycle management
    
    Issue-Id: APPC-44
    Change-Id: If0439ac0f1013cdc8a9d8fb0a672c1b7695ace07
    Signed-off-by: beili.zhou <beili.zhou@amdocs.com>

---
+++ appc-lifecycle-management/state-machine-lib/src/main/java/org/openecomp/appc/statemachine/objects/StateMachineMetadata.java
@@ -0,0 +1,78 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : APPC
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Copyright (C) 2017 Amdocs
+ * =============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * ECOMP is a trademark and service mark of AT&T Intellectual Property.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.openecomp.appc.statemachine.objects;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class StateMachineMetadata {
+
+    private Set<State> states;
+    private Set<Event> events;
+
+    private StateMachineMetadata(StateMachineMetadataBuilder builder){
+        states = builder.states;
+        events = builder.events;
+    }
+
+    public Set<State> getStates() {
+        return states;
+    }
+
+    public Set<Event> getEvents() {
+        return events;
+    }
+
+    public static class StateMachineMetadataBuilder{
+
+        private Set<State> states;
+        private Set<Event> events;
+
+        public StateMachineMetadataBuilder(){
+            states = new HashSet<>();
+            events = new HashSet<>();
+        }
+
+        public StateMachineMetadataBuilder addState(State state){
+            this.states.add(state);
+            return this;
+        }
+
+        public StateMachineMetadataBuilder addEvent(Event event){
+            this.events.add(event);
+            return this;
+        }
+
+        public StateMachineMetadataBuilder addTransition(State currentState,Event event,State nextState){
+            Transition transition = new Transition(event,nextState);
+            currentState.addTransition(transition);
+            return this;
+        }
+
+        public StateMachineMetadata build(){
+            return new StateMachineMetadata(this);
+        }
+    }
+}
