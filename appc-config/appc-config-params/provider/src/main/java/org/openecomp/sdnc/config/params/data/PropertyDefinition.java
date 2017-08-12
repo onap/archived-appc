790e8719da2d699f7ec0b28c6b625a544262dd94
Anand Chaturvedi <ac204h@att.com>
2017-08-12 01:31:10 -0400

    Initial commit for appc-config-params
    Change-Id: I5805591892184b4456dd30639417d783d353a6a3
    Signed-off-by: Anand Chaturvedi <ac204h@att.com>
    Issue-Id:APPC-9

---
+++ appc-config/appc-config-params/provider/src/main/java/org/openecomp/sdnc/config/params/data/PropertyDefinition.java
@@ -0,0 +1,54 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : APPC
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * 
+ *  ECOMP is a trademark and service mark of AT&T Intellectual Property.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.openecomp.sdnc.config.params.data;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class PropertyDefinition {
+	public String kind = "Property Definition";
+	public String version = "V1";
+	
+	@JsonProperty("vnf-parameter-list")
+	public List<Parameter> parameters;
+	public String getKind() {
+		return kind;
+	}
+	public void setKind(String kind) {
+		this.kind = kind;
+	}
+	public String getVersion() {
+		return version;
+	}
+	public void setVersion(String version) {
+		this.version = version;
+	}
+	public List<Parameter> getParameters() {
+		return parameters;
+	}
+	public void setParameters(List<Parameter> parameters) {
+		this.parameters = parameters;
+	}	
+	
+}
