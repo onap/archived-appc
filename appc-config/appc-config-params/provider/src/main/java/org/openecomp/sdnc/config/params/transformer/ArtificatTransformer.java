899d84646dfcab974cc593a75f2cc829bf486e10
Anand Chaturvedi <ac204h@att.com>
2017-08-11 22:20:06 -0400

    Initial commit for appc-config-params
    Change-Id: I5805591892184b4456dd30639417d783d353a6a3
    Signed-off-by: Anand Chaturvedi <ac204h@att.com>
    Issue-Id:APPC-9

---
+++ appc-config/appc-config-params/provider/src/main/java/org/openecomp/sdnc/config/params/transformer/ArtificatTransformer.java
@@ -0,0 +1,85 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : APPC
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * 
+ *  ECOMP is a trademark and service mark of AT&T Intellectual Property.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.openecomp.sdnc.config.params.transformer;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.openecomp.sdnc.config.params.data.Parameter;
+import org.openecomp.sdnc.config.params.data.PropertyDefinition;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+public class ArtificatTransformer {
+
+
+	public String convertPDToYaml(PropertyDefinition propertyDefinition) throws JsonParseException, JsonMappingException, IOException{
+		String yamlContent = null;
+		if(propertyDefinition != null){
+			ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+			mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+			yamlContent = mapper.writeValueAsString(propertyDefinition);	        
+		}
+		return yamlContent;
+	}
+
+	public String transformYamlToJson(String yaml) throws JsonParseException, JsonMappingException, IOException {
+		ObjectMapper yamlReader = new ObjectMapper(new YAMLFactory());
+		Object obj = yamlReader.readValue(yaml, Object.class);
+		ObjectMapper jsonWriter = new ObjectMapper();
+		jsonWriter.enable(SerializationFeature.INDENT_OUTPUT);	    
+		return jsonWriter.writeValueAsString(obj);
+	}
+
+	public PropertyDefinition convertYAMLToPD(String pdContent) throws JsonParseException, JsonMappingException, IOException{
+		PropertyDefinition propertyDefinition = null;
+		if(StringUtils.isNotBlank(pdContent)){
+			ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+			propertyDefinition = mapper.readValue(pdContent, PropertyDefinition.class);
+		}
+		return propertyDefinition;
+	}
+
+	public String convertYAMLToParams(String pdContent) throws JsonParseException, JsonMappingException, IOException{
+		String paramJson = null;
+		if(StringUtils.isNotBlank(pdContent)){
+			paramJson = convertPdToParams(convertYAMLToPD(pdContent));
+		}
+		return paramJson;
+	}
+
+	public String convertPdToParams(PropertyDefinition propertyDefinition) throws JsonParseException, JsonMappingException, IOException{
+		String paramJson = null;
+		if(propertyDefinition != null  && propertyDefinition.getParameters() != null){
+			List<Parameter> parameters = propertyDefinition.getParameters();
+
+		}
+		return paramJson;
+	}
+
+}
---
+++ appc-config/appc-config-params/provider/src/test/java/org/openecomp/sdnc/config/params/parser/TestPropertyDefinitionNode.java
@@ -0,0 +1,131 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : APPC
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * 
+ *  ECOMP is a trademark and service mark of AT&T Intellectual Property.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.openecomp.sdnc.config.params.parser;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+import org.junit.Ignore;
+import org.openecomp.sdnc.config.params.ParamsHandlerConstant;
+import org.openecomp.sdnc.config.params.data.PropertyDefinition;
+import org.openecomp.sdnc.config.params.parser.PropertyDefinitionNode;
+import org.openecomp.sdnc.config.params.transformer.ArtificatTransformer;
+
+import org.openecomp.sdnc.sli.SvcLogicContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class TestPropertyDefinitionNode {
+
+@Ignore
+	public void testProcessMissingParamKeys() throws Exception {
+		PropertyDefinitionNode propertyDefinitionNode = new PropertyDefinitionNode();
+		Map<String, String> inParams = new HashMap<String, String>();
+		inParams.put(ParamsHandlerConstant.INPUT_PARAM_RESPONSE_PRIFIX, "test");
+
+		String yamlData = IOUtils.toString(TestPropertyDefinitionNode.class.getClassLoader().getResourceAsStream("parser/pd.yaml"), Charset.defaultCharset());
+		inParams.put(ParamsHandlerConstant.INPUT_PARAM_PD_CONTENT, yamlData);
+
+		String jsonData = IOUtils.toString(TestPropertyDefinitionNode.class.getClassLoader().getResourceAsStream("parser/request-param.json"), Charset.defaultCharset());
+		inParams.put(ParamsHandlerConstant.INPUT_PARAM_JSON_DATA, jsonData);
+
+		SvcLogicContext ctx = new SvcLogicContext();
+		propertyDefinitionNode.processMissingParamKeys(inParams, ctx);
+		assertEquals(ctx.getAttribute("test."+ParamsHandlerConstant.OUTPUT_PARAM_STATUS), ParamsHandlerConstant.OUTPUT_STATUS_SUCCESS);
+
+	}
+
+	public void testProcessExternalSystemParamKeys() throws Exception {
+		PropertyDefinitionNode propertyDefinitionNode = new PropertyDefinitionNode();
+		Map<String, String> inParams = new HashMap<String, String>();
+		inParams.put(ParamsHandlerConstant.INPUT_PARAM_RESPONSE_PRIFIX, "test");
+
+		String yamlData = IOUtils.toString(TestPropertyDefinitionNode.class.getClassLoader().getResourceAsStream("parser/pd.yaml"), Charset.defaultCharset());
+		inParams.put(ParamsHandlerConstant.INPUT_PARAM_PD_CONTENT, yamlData);
+
+		String jsonData = IOUtils.toString(TestPropertyDefinitionNode.class.getClassLoader().getResourceAsStream("parser/request-param.json"), Charset.defaultCharset());
+		inParams.put(ParamsHandlerConstant.INPUT_PARAM_JSON_DATA, jsonData);
+
+		inParams.put(ParamsHandlerConstant.INPUT_PARAM_SYSTEM_NAME, "INSTAR");
+
+		SvcLogicContext ctx = new SvcLogicContext();
+		propertyDefinitionNode.processExternalSystemParamKeys(inParams, ctx);
+		assertEquals(ctx.getAttribute("test."+ParamsHandlerConstant.OUTPUT_PARAM_STATUS), ParamsHandlerConstant.OUTPUT_STATUS_SUCCESS);
+
+		System.out.println("Result: " + ctx.getAttributeKeySet());
+		System.out.println("INSTAR.keys : " + ctx.getAttribute("INSTAR.keys"));
+		System.out.println("INSTAR.LOCAL_CORE_ALT_IP_ADDR.request-logic : " + ctx.getAttribute("INSTAR.LOCAL_ACCESS_IP_ADDR"));
+		System.out.println("INSTAR.LOCAL_CORE_ALT_IP_ADDR.request-logic : " + ctx.getAttribute("INSTAR.LOCAL_CORE_ALT_IP_ADDR"));
+
+	}
+
+	public void mergeJsonData() throws Exception {
+		PropertyDefinitionNode propertyDefinitionNode = new PropertyDefinitionNode();
+		Map<String, String> inParams = new HashMap<String, String>();
+		inParams.put(ParamsHandlerConstant.INPUT_PARAM_RESPONSE_PRIFIX, "test");
+
+		String jsonData = IOUtils.toString(TestPropertyDefinitionNode.class.getClassLoader().getResourceAsStream("parser/request-param.json"), Charset.defaultCharset());
+		inParams.put(ParamsHandlerConstant.INPUT_PARAM_JSON_DATA, jsonData);
+
+		String mergeJsonData = IOUtils.toString(TestPropertyDefinitionNode.class.getClassLoader().getResourceAsStream("parser/merge-param.json"), Charset.defaultCharset());
+		inParams.put(ParamsHandlerConstant.INPUT_PARAM_MERGE__JSON_DATA, mergeJsonData);
+
+		SvcLogicContext ctx = new SvcLogicContext();
+		propertyDefinitionNode.mergeJsonData(inParams, ctx);
+		assertEquals(ctx.getAttribute("test."+ParamsHandlerConstant.OUTPUT_PARAM_STATUS), ParamsHandlerConstant.OUTPUT_STATUS_SUCCESS);
+
+		System.out.println("Result: " + ctx.getAttributeKeySet()); 
+		System.out.println("Merged Value : " + ctx.getAttribute("test." +ParamsHandlerConstant.OUTPUT_PARAM_CONFIGURATION_PARAMETER) );
+
+
+	}
+
+	@Test
+	public void testArtificatTransformer() throws Exception {
+		ArtificatTransformer transformer = new ArtificatTransformer();
+		String yamlData = IOUtils.toString(TestPropertyDefinitionNode.class.getClassLoader().getResourceAsStream("parser/pd.yaml"), Charset.defaultCharset());
+
+		PropertyDefinition propertyDefinition = transformer.convertYAMLToPD(yamlData);
+
+		//		String json = transformer.transformYamlToJson(yamlData);
+		//		System.out.println("TestPropertyDefinitionNode.testArtificatTransformer()" + json);
+		String yaml = transformer.convertPDToYaml(propertyDefinition);
+		System.out.println("TestPropertyDefinitionNode.testArtificatTransformer():\n" + yaml);
+
+	}
+	
+	
+
+
+}
