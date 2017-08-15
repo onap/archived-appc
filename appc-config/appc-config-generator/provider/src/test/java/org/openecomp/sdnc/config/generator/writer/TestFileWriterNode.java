d8a932c50d3dabc12e144c7ded4d21103ee92f89
Skip Wonnell <kw5258@att.com>
2017-08-15 18:07:13 -0500

    Initial commit for config-generator bundle
    This bundle contains logic for creating the configuration,
    reading templates, and merging the data together
    
    Issue-ID: APPC-59
    Change-Id: Ifd745964f659f075250d6acaf1b5f033da28e1c4
    Signed-off-by: Skip Wonnell <kw5258@att.com>

---
+++ appc-config/appc-config-generator/provider/src/test/java/org/openecomp/sdnc/config/generator/writer/TestFileWriterNode.java
@@ -0,0 +1,55 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : APP-C
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property.  All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.openecomp.sdnc.config.generator.writer;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+import org.junit.Test;
+import org.openecomp.sdnc.config.generator.ConfigGeneratorConstant;
+import org.openecomp.sdnc.config.generator.writer.FileWriterNode;
+
+import org.openecomp.sdnc.sli.SvcLogicContext;
+
+public class TestFileWriterNode {
+
+
+    //@Test
+    public void writeFile() throws Exception {
+        FileWriterNode FileWriterNode = new FileWriterNode();
+        Map<String, String> inParams = new HashMap<String, String>();
+        inParams.put(ConfigGeneratorConstant.INPUT_PARAM_FILE_NAME, "src/test/resources/writer/testcvaas.json");
+        inParams.put(ConfigGeneratorConstant.INPUT_PARAM_REQUEST_DATA,"{'name':'Name','role':'admin'}");
+        inParams.put(ConfigGeneratorConstant.INPUT_PARAM_RESPONSE_PRIFIX, "test");
+
+        
+
+        SvcLogicContext ctx = new SvcLogicContext();
+        FileWriterNode.writeFile(inParams, ctx);
+        assertEquals(ctx.getAttribute("test."+ConfigGeneratorConstant.OUTPUT_PARAM_STATUS), ConfigGeneratorConstant.OUTPUT_STATUS_SUCCESS);
+
+        
+
+    }
+}
