8f00d5f31c2f56dfade6e48b9a9d9a9cb72be9e6
Kalpesh Chaniyara <kalpeshkumar.chaniyara@amdocs.com>
2017-08-15 23:40:34 +0530

    Initial commit for Sequence Generator
    Added sequence generator for start and stop
    action with dependency model
    JIRA Issues : APPC-33,APPC-106
    
    Issue-Id : APPC-106
    Change-Id: I7f12d39b705d77d559790a10b914b8108f5faa5e
    Signed-off-by: Kalpesh Chaniyara <kalpeshkumar.chaniyara@amdocs.com>

---
+++ appc-sequence-generator/appc-sequence-generator-bundle/src/main/java/org/openecomp/appc/seqgen/objects/Response.java
@@ -0,0 +1,65 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : APP-C
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property.  All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.openecomp.appc.seqgen.objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import java.util.Map;
+
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Response {
+
+    @JsonProperty("response-code")
+    private String responseCode;
+
+    @JsonProperty("response-message")
+    private String responseMessage;
+
+    @JsonProperty("response-action")
+    private Map<String,String> responseAction;
+
+    public String getResponseCode() {
+        return responseCode;
+    }
+
+    public void setResponseCode(String responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    public String getResponseMessage() {
+        return responseMessage;
+    }
+
+    public void setResponseMessage(String responseMessage) {
+        this.responseMessage = responseMessage;
+    }
+
+    public Map<String, String> getResponseAction() {
+        return responseAction;
+    }
+
+    public void setResponseAction(Map<String, String> responseAction) {
+        this.responseAction = responseAction;
+    }
+}
