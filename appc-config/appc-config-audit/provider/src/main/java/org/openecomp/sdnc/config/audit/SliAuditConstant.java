ee98b50de904e0e7c18015312944023e8473b1fc
Skip Wonnell <kw5258@att.com>
2017-08-15 19:57:50 -0500

    Initial commit for appc-config-audit bundle
    This Bundle is used for Auditing against
    a running configurition
    
    Change-Id: If91f25675121d75b3213bed812f0bd1436fcbd2c
    Signed-off-by: Skip Wonnell <kw5258@att.com>
    Issue-ID: APPC-22

---
+++ appc-config/appc-config-audit/provider/src/main/java/org/openecomp/sdnc/config/audit/SliAuditConstant.java
@@ -0,0 +1,64 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : APP-C
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property.  All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.openecomp.sdnc.config.audit;
+
+public class SliAuditConstant {
+
+    public static String STRING_ENCODING = "utf-8";
+    public static String Y = "Y";
+    public static String N = "N";
+    public static String DATA_TYPE_TEXT = "TEXT";
+    public static String DATA_TYPE_JSON = "JSON";
+    public static String DATA_TYPE_XML = "XML";
+    public static String DATA_TYPE_SQL = "SQL";
+
+    public static String INPUT_PARAM_JSON_DATA = "jsonData";
+    public static String INPUT_PARAM_IS_ESCAPED = "isEscaped";
+    public static String INPUT_PARAM_BLOCK_KEYS = "blockKeys";
+    public static String INPUT_PARAM_LOG_DATA = "logData";
+    public static String INPUT_PARAM_CHECK_DATA = "checkData";
+    public static String INPUT_PARAM_ESCAPE_DATA = "escapeData";
+    public static String INPUT_PARAM_UNESCAPE_DATA = "unEscapeData";
+    public static String INPUT_PARAM_DATA_TYPE = "dataType";
+    public static String INPUT_PARAM_FILE_NAME = "fileName";
+
+    public static String INPUT_PARAM_TEMPLATE_DATA = "templateData";
+    public static String INPUT_PARAM_TEMPLATE_FILE = "templateFile";
+    public static String INPUT_PARAM_TEMPLATE_TYPE = "templateType";
+    public static String INPUT_PARAM_DO_PRETTY_OUTPUT = "doPrettyOutput";
+    public static String INPUT_PARAM_REQUEST_DATA = "requestData";
+    public static String INPUT_PARAM_RESPONSE_PRIFIX = "responsePrefix";
+
+
+    public static String OUTPUT_PARAM_MERGED_DATA = "mergedData";
+    public static String OUTPUT_PARAM_TRANSFORMED_DATA = "transformedData";
+    public static String OUTPUT_PARAM_FILE_DATA = "fileData";
+    public static String OUTPUT_PARAM_PARSED_ERROR = "parsedError";
+    public static String OUTPUT_PARAM_DATA_TYPE = "dataType";
+    public static String OUTPUT_PARAM_STATUS = "status";
+    public static String OUTPUT_PARAM_ERROR_MESSAGE = "error-message";
+    public static String OUTPUT_PARAM_ESCAPE_DATA = "escapeData";
+    public static String OUTPUT_PARAM_UNESCAPE_DATA = "unEscapeData";
+
+    public static String OUTPUT_STATUS_SUCCESS = "success";
+    public static String OUTPUT_STATUS_FAILURE = "failure";
+
+}
