eb74e20129543c6510a142cc26f1b51953a60679
Skip Wonnell <kw5258@att.com>
2017-08-15 19:57:50 -0500

    Initial commit for appc-config-audit bundle
    This Bundle is used for Auditing against
    a running configurition
    
    Change-Id: If91f25675121d75b3213bed812f0bd1436fcbd2c
    Signed-off-by: Skip Wonnell <kw5258@att.com>
    Issue-ID: APPC-22

---
+++ appc-config/appc-config-adaptor/provider/src/main/java/org/openecomp/appc/ccadaptor/DebugLog.java
@@ -0,0 +1,102 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : APP-C
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property.  All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.openecomp.appc.ccadaptor;
+
+import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class DebugLog
+{
+  private static String fileName = "/tmp/rt.log";
+
+  public static void main (String args[])
+  {
+    DebugLog debugLog = new DebugLog();
+    debugLog.printAriDebug("DebugLog", "The Message");
+  }
+
+  public static void printAriDebug(String fn, String messg)
+  {
+    String logMessg = getDateTime() +" " +fn +" " + messg;
+    appendToFile(logMessg +"\n");
+
+  }
+
+  public static void printRTAriDebug(String fn, String messg)
+  {
+    // System.out.println (getDateTime() +" " +fn +" " + messg);
+    String logMessg = getDateTime() +" " +fn +" " + messg;
+    appendToFile(logMessg +"\n");
+  }
+  public static String getDateTime()
+  {
+    DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+    // DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+    Date date = new Date();
+    return dateFormat.format(date);
+  }
+
+  public static void appendToFile (String dataToWrite)
+  {
+    String fn = "DebugLog.appendToFile";
+    try
+    {
+      // First check to see if a file 'fileName' exist, if it does
+      // write to it. If it does not exist, don't write to it.
+      File tmpFile = new File(fileName);
+      if (tmpFile.exists())
+      {
+        BufferedWriter out = new BufferedWriter(new FileWriter(fileName, true));
+        out.write(dataToWrite);
+        out.close();
+      }
+    }
+    catch (IOException e)
+    {
+      DebugLog.printRTAriDebug (fn, "writeToFile() exception: " + e);
+      //System.err.println("writeToFile() exception: " + e);
+      e.printStackTrace();
+    }
+  }
+
+  public void outputStackTrace(Exception e)
+  {
+    String fn = "DebugLog.outputStackTrace";
+    StringWriter sw = new StringWriter();
+    e.printStackTrace(new PrintWriter(sw));
+    String stackTrace = sw.toString();
+    DebugLog.printRTAriDebug(fn, "Stack trace::: "+stackTrace);
+  }
+
+  public static String getStackTraceString(Exception e)
+  {
+    String fn = "DebugLog.outputStackTrace";
+    StringWriter sw = new StringWriter();
+    e.printStackTrace(new PrintWriter(sw));
+    String stackTrace = sw.toString();
+    return(stackTrace);
+  }
+
+}
+
---
+++ appc-config/appc-config-audit/provider/src/test/java/org/openecomp/sdnc/config/audit/node/TestCompareNodeXml.java
@@ -0,0 +1,127 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : APP-C
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property.  All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.openecomp.sdnc.config.audit.node;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Properties;
+
+import org.junit.Test;
+import org.openecomp.sdnc.config.audit.node.CompareNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.openecomp.sdnc.sli.SvcLogicContext;
+import org.openecomp.sdnc.sli.SvcLogicException;
+
+
+public class TestCompareNodeXml
+{
+    private static final Logger log = LoggerFactory.getLogger(TestCompareNodeXml.class);
+
+    //@Test
+    public void TestCompareExtactXML()
+    {
+        log.debug("TestCompareNode.TestCompareExtactXML()");
+        SvcLogicContext ctx  = new SvcLogicContext();
+        HashMap<String, String> testMap = new HashMap<String, String>();
+        CompareNode cmp  = new CompareNode();
+        try
+        {
+            String s="<configuration  xmlns="
+                    + "\"http://xml.juniper.net/xnm/1.1/xnm\""
+                    + " junos:commit-seconds="
+                    +"\"1473957536\" "
+                    +"junos:commit-localtime="
+                    +"\"2016-09-15 16:38:56 UTC\" "
+                    + "junos:commit-user="
+                    +"\"root\""
+                    +"><name>Test</name></configuration>";
+
+            String t ="<configuration  xmlns="
+                    + "\"http://xml.juniper.net/xnm/1.1/xnm\""
+                    + " junos:commit-seconds="
+                    +"\"1473957536\" "
+                    +"junos:commit-localtime="
+                    +"\"2016-09-15 16:38:56 UTC\" "
+                    + "junos:commit-user="
+                    +"\"root\""
+                    +"><name>Test</name></configuration>";
+
+            System.out.println("s=" + s);
+
+            testMap.put("compareDataType", "RESTCONF-XML");
+            testMap.put("requestIdentifier", "123");
+            testMap.put("sourceData", s);
+            testMap.put("targetData", t);
+            cmp.compare(testMap, ctx);
+            assert(ctx.getAttribute("123." + "STATUS").equals("SUCCESS"));
+        }
+        catch (SvcLogicException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    //@Test
+    public void TestCompareforAttributeOrder() throws IOException
+    {
+        log.debug("TestCompareNode.TestCompareforAttributeOrder()");
+        SvcLogicContext ctx  = new SvcLogicContext();
+        HashMap<String, String> testMap = new HashMap<String, String>();
+        CompareNode cmp  = new CompareNode();
+        testMap.put("compareDataType", "XML");
+        testMap.put("sourceData", "<SipIfTermination><id>2</id><udpPortInUse>true</udpPortInUse><udpPort>5060</udpPort><tcpPortInUse>true</tcpPortInUse><tcpPort>5060</tcpPort></SipIfTermination>");
+        testMap.put("targetData", "<SipIfTermination><udpPortInUse>true</udpPortInUse><udpPort>5060</udpPort><tcpPortInUse>true</tcpPortInUse><tcpPort>5060</tcpPort><id>2</id></SipIfTermination>");
+        try
+        {
+            cmp.compare(testMap, ctx);
+        }
+        catch (SvcLogicException e)
+        {
+            e.printStackTrace();
+        }
+        assert(ctx.getAttribute("STATUS").equals("SUCCESS"));
+    }
+
+    //@Test
+    public void TestCompareForComments()
+    {
+        log.debug("TestCompareNode.TestCompareForComments()");
+        SvcLogicContext ctx  = new SvcLogicContext();
+        HashMap<String, String> testMap = new HashMap<String, String>();
+        CompareNode cmp  = new CompareNode();
+
+        try
+        {
+            testMap.put("compareDataType", "XML");
+            testMap.put("sourceData", "<SipIfTermination><id>2</id><!--this is a commnect --></SipIfTermination>");
+            testMap.put("targetData", "<SipIfTermination><id>2</id></SipIfTermination>");
+            cmp.compare(testMap, ctx);
+            assert(ctx.getAttribute("STATUS").equals("SUCCESS"));
+        }
+        catch (SvcLogicException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+}
