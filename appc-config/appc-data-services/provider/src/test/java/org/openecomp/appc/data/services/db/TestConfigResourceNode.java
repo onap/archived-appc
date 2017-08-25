4090327dd8b7d1cc555fdfd906eeaa72c6dc8368
Anand Chaturvedi <ac204h@att.com>
2017-08-25 19:17:38 -0400

    Junit for APPC  for Network Client
    Issue-ID: APPC-168
    Change-Id: I0654a0d6123b5827458cd3e15e12a1b5b254f918
    Signed-off-by: Anand Chaturvedi <ac204h@att.com>

--- appc-config/appc-data-services/provider/src/test/java/org/openecomp/appc/data/services/db/TestConfigResourceNode.java
+++ appc-config/appc-data-services/provider/src/test/java/org/openecomp/appc/data/services/db/TestConfigResourceNode.java
@@ -1,115 +1,151 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : APPC
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Copyright (C) 2017 Amdocs
+ * =============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * ECOMP is a trademark and service mark of AT&T Intellectual Property.
+ * ============LICENSE_END=========================================================
+ */
+
 package org.openecomp.appc.data.services.db;
 
-import java.io.InputStream;
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
-
 import org.junit.Test;
 import org.openecomp.appc.data.services.AppcDataServiceConstant;
-import org.openecomp.appc.data.services.db.DGGeneralDBService;
 import org.openecomp.appc.data.services.node.ConfigResourceNode;
-import org.apache.commons.lang.StringEscapeUtils;
-import org.junit.After;
-import org.junit.Before;
 import org.openecomp.sdnc.sli.SvcLogicContext;
 import org.openecomp.sdnc.sli.SvcLogicException;
-import org.openecomp.sdnc.sli.SvcLogicResource;
-import org.openecomp.sdnc.sli.SvcLogicResource.QueryStatus;
-import org.openecomp.sdnc.sli.resource.sql.SqlResource;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
-
-import java.io.File;
 
 public class TestConfigResourceNode {
-	
-	@Test(expected = Exception.class)
-	public void testGetUploadConfig1() throws SvcLogicException{
-	SvcLogicContext ctx = new SvcLogicContext();
-	ctx.setAttribute("test","test");
-		ConfigResourceNode dbService = new ConfigResourceNode() ;
-		Map<String,String> map = new HashMap<String,String>();
-	     dbService.getConfigFileReference(map,ctx);
-		//System.out.println(status);
-	
-		
-	}
 
-@Test(expected = Exception.class)
+    @Test(expected = Exception.class)
+    public void testgetConfigFileReferenc() throws SvcLogicException {
+        SvcLogicContext ctx = new SvcLogicContext();
+        ctx.setAttribute("test", "test");
+        ConfigResourceNode dbService = new ConfigResourceNode();
+        Map<String, String> map = new HashMap<String, String>();
+        dbService.getConfigFileReference(map, ctx);
+    }
 
-public void testGetUploadConfig2() throws SvcLogicException{
-SvcLogicContext ctx = new SvcLogicContext();
-ctx.setAttribute("test","test");
-	ConfigResourceNode dbService = new ConfigResourceNode() ;
-	Map<String,String> map = new HashMap<String,String>();
-     dbService.getTemplate(map,ctx);
-	//System.out.println(status);
-}
+    @Test(expected = Exception.class)
 
-@Test(expected = Exception.class)
+    public void testgetTemplate() throws SvcLogicException {
+        SvcLogicContext ctx = new SvcLogicContext();
+        ctx.setAttribute("test", "test");
+        ctx.setAttribute(AppcDataServiceConstant.INPUT_PARAM_FILE_CATEGORY, "test");
+        ctx.setAttribute(AppcDataServiceConstant.INPUT_PARAM_RESPONSE_PREFIX, "tmp.test");
+        ctx.setAttribute("template-name", "test.json");
 
-public void testGetUploadConfig3() throws SvcLogicException{
-SvcLogicContext ctx = new SvcLogicContext();
-ctx.setAttribute("test","test");
-	ConfigResourceNode dbService = new ConfigResourceNode() ;
-	Map<String,String> map = new HashMap<String,String>();
-     dbService.getVnfcReference(map, ctx);
-	//System.out.println(status);
+        ConfigResourceNode dbService = new ConfigResourceNode();
+        Map<String, String> map = new HashMap<String, String>();
+        dbService.getTemplate(map, ctx);
 
-	
-}
+    }
 
-@Test(expected = Exception.class)
+    @Test(expected = Exception.class)
+    public void testgetVnfcReference() throws SvcLogicException {
+        SvcLogicContext ctx = new SvcLogicContext();
+        ctx.setAttribute("test", "test");
+        ConfigResourceNode dbService = new ConfigResourceNode();
+        Map<String, String> map = new HashMap<String, String>();
+        map.put(AppcDataServiceConstant.INPUT_PARAM_RESPONSE_PREFIX, "tmp.test");
+        dbService.getVnfcReference(map, ctx);
+    }
 
-public void testGetUploadConfig4() throws SvcLogicException{
-SvcLogicContext ctx = new SvcLogicContext();
-ctx.setAttribute("test","test");
-	ConfigResourceNode dbService = new ConfigResourceNode() ;
-	Map<String,String> map = new HashMap<String,String>();
-     dbService.getSmmChainKeyFiles(map, ctx);
-	//System.out.println(status);
+    @Test(expected = Exception.class)
+    public void testgetSmmChainKeyFiles() throws SvcLogicException {
+        SvcLogicContext ctx = new SvcLogicContext();
+        ctx.setAttribute("test", "test");
+        ctx.setAttribute("site-location","test/location");
+        ConfigResourceNode dbService = new ConfigResourceNode();
+        Map<String, String> map = new HashMap<String, String>();
+        map.put(AppcDataServiceConstant.INPUT_PARAM_RESPONSE_PREFIX, "tmp.test");
+        dbService.getSmmChainKeyFiles(map, ctx);
+    }
 
-	
-}
+    @Test(expected = Exception.class)
 
-@Test(expected = Exception.class)
+    public void testgetDownloadConfigTemplateByVnf() throws SvcLogicException {
+        SvcLogicContext ctx = new SvcLogicContext();
+        ctx.setAttribute("test", "test");    
+        ConfigResourceNode dbService = new ConfigResourceNode();
+        Map<String, String> map = new HashMap<String, String>();
+        map.put(AppcDataServiceConstant.INPUT_PARAM_RESPONSE_PREFIX, "tmp.test");
+        dbService.getDownloadConfigTemplateByVnf(map, ctx);
 
-public void testGetUploadConfig5() throws SvcLogicException{
-SvcLogicContext ctx = new SvcLogicContext();
-ctx.setAttribute("test","test");
-	ConfigResourceNode dbService = new ConfigResourceNode() ;
-	Map<String,String> map = new HashMap<String,String>();
-     dbService.getDownloadConfigTemplateByVnf(map, ctx);
-	//System.out.println(status);
+    }
 
-	
-}
+    @Test(expected = Exception.class)
+    public void testgetCommonConfigInfo() throws SvcLogicException {
+        SvcLogicContext ctx = new SvcLogicContext();
+        ctx.setAttribute("test", "test");
+        ConfigResourceNode dbService = new ConfigResourceNode();
+        Map<String, String> map = new HashMap<String, String>();
+        dbService.getCommonConfigInfo(map, ctx);
 
-@Test(expected = Exception.class)
-public void testGetUploadConfig6() throws SvcLogicException{
-SvcLogicContext ctx = new SvcLogicContext();
-ctx.setAttribute("test","test");
-	ConfigResourceNode dbService = new ConfigResourceNode() ;
-	Map<String,String> map = new HashMap<String,String>();
-     dbService.getCommonConfigInfo(map, ctx);
-	//System.out.println(status);
+    }
 
-	
-}
+    @Test(expected = Exception.class)
+    public void testupdateUploadConfigss() throws SvcLogicException {
+        SvcLogicContext ctx = new SvcLogicContext();
+        ctx.setAttribute("test", "test");
+        ConfigResourceNode dbService = new ConfigResourceNode();
+        Map<String, String> map = new HashMap<String, String>();
+        dbService.updateUploadConfig(map, ctx);
 
-@Test(expected = Exception.class)
-public void testGetUploadConfig7() throws SvcLogicException{
-SvcLogicContext ctx = new SvcLogicContext();
-ctx.setAttribute("test","test");
-	ConfigResourceNode dbService = new ConfigResourceNode() ;
-	Map<String,String> map = new HashMap<String,String>();
-     dbService.updateUploadConfig(map, ctx);
-	//System.out.println(status);
+    }
 
-	
-}
+    @Test(expected = Exception.class)
+    public void testgetConfigFilesByVnfVmNCategory() throws SvcLogicException {
+        SvcLogicContext ctx = new SvcLogicContext();
+        ConfigResourceNode node = new ConfigResourceNode();
+        Map<String, String> inParams = new HashMap<String, String>();
+        inParams.put(AppcDataServiceConstant.INPUT_PARAM_RESPONSE_PREFIX, "response-prefix");
+        inParams.put(AppcDataServiceConstant.INPUT_PARAM_FILE_CATEGORY, "config_template");
+        inParams.put((String) AppcDataServiceConstant.INPUT_PARAM_VNF_ID, "test");
+        inParams.put((String) AppcDataServiceConstant.INPUT_PARAM_VM_NAME, "test");
+        node.getConfigFilesByVnfVmNCategory(inParams, ctx);
+    }
 
+    @Test(expected = Exception.class)
+    public void testsaveConfigTransactionLog() throws SvcLogicException {
+        SvcLogicContext ctx = new SvcLogicContext();
+        ConfigResourceNode node = new ConfigResourceNode();
+        Map<String, String> inParams = new HashMap<String, String>();
+        inParams.put(AppcDataServiceConstant.INPUT_PARAM_MESSAGE, "testMessage");
+        inParams.put(AppcDataServiceConstant.INPUT_PARAM_RESPONSE_PREFIX, "response-prefix");
+        inParams.put(AppcDataServiceConstant.INPUT_PARAM_MESSAGE_TYPE, "testmessage");
+        ctx.setAttribute("request-id", "tets-id");
+        node.saveConfigTransactionLog(inParams, ctx);
+
+    }
+
+    @Test(expected = Exception.class)
+    public void testsaveConfigBlock() throws SvcLogicException {
+        SvcLogicContext ctx = new SvcLogicContext();
+        ConfigResourceNode node = new ConfigResourceNode();
+        Map<String, String> inParams = new HashMap<String, String>();
+        inParams.put(AppcDataServiceConstant.INPUT_PARAM_RESPONSE_PREFIX, "tmp");
+        ctx.setAttribute("configuration", "test");
+        ctx.setAttribute("tmp.convertconfig.escapeData", "test");
+        ctx.setAttribute("tmp.merge.mergedData", "test");
+        node.saveConfigBlock(inParams, ctx);
+    }
 
 }
--- appc-config/appc-encryption-tool/provider/src/test/java/org/openecomp/appc/encryptiontool/TestEncryptionTool.java
+++ appc-config/appc-encryption-tool/provider/src/test/java/org/openecomp/appc/encryptiontool/TestEncryptionTool.java
@@ -1,9 +1,11 @@
 /*-
  * ============LICENSE_START=======================================================
- * ONAP : APP-C
+ * ONAP : APPC
  * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property.  All rights reserved.
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
+ * Copyright (C) 2017 Amdocs
+ * =============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,51 +17,59 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * 
+ * ECOMP is a trademark and service mark of AT&T Intellectual Property.
  * ============LICENSE_END=========================================================
  */
 
 package org.openecomp.appc.encryptiontool;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
-import java.util.Properties;
-
 import org.junit.Test;
+import org.openecomp.appc.encryptiontool.wrapper.DbServiceUtil;
 import org.openecomp.appc.encryptiontool.wrapper.EncryptionToolDGWrapper;
 import org.openecomp.appc.encryptiontool.wrapper.WrapperEncryptionTool;
 import org.openecomp.sdnc.sli.SvcLogicContext;
-import org.osgi.framework.BundleActivator;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceRegistration;
-
-import com.att.eelf.configuration.EELFLogger;
-import com.att.eelf.configuration.EELFManager;
 
 public class TestEncryptionTool {
 
-    //@Test
-    public void testEncryptionTool() throws Exception{
+	@Test
+	public void testEncryptionTool() throws Exception{
+		String [] input = new String[] {"testVnf_Type","testUser","testPassword11", "testAction1", "8080", "http://localhost:8080/restconf/healthcheck"};
+		WrapperEncryptionTool.main(input);
 
-        String [] input = new String[] {"testVnf_Type","testUser","testPassword11", "testAction1", "8080", "http://localhost:8080/restconf/healthcheck"};
-        WrapperEncryptionTool.main(input);
-
-    }
-    //@Test
-    public void testgetPropertyDG() throws Exception{
-        EncryptionToolDGWrapper encryptionToolDBWrapper = new EncryptionToolDGWrapper();
-        SvcLogicContext ctx = new SvcLogicContext();
-        Map<String, String> inParams = new HashMap<String, String>();
-
-        inParams.put("prefix", "test");
-        inParams.put("propertyName", "testVnf_Type.testAction1.url");
-
-        encryptionToolDBWrapper.getProperty(inParams, ctx);
-
-        System.out.println("propertyValue :" + ctx.getAttribute("test.propertyName"));
-
-        System.out.println("All  propertyValue :" + ctx.getAttributeKeySet());
-    }
-
+	}
+	@Test(expected=Exception.class)
+	public void testgetPropertyDG() throws Exception{
+		EncryptionToolDGWrapper et = new EncryptionToolDGWrapper();		
+		SvcLogicContext ctx = new SvcLogicContext();		
+		Map<String, String> inParams = new HashMap<String, String>();
+		inParams.put("prefix", "test");
+		inParams.put("propertyName", "testVnf_Type.testAction1.url");
+		et.getProperty(inParams, ctx);
+	}
+	@Test(expected=Exception.class)
+	public void testgetData() throws Exception
+	{
+		DbServiceUtil d = new DbServiceUtil();
+		ArrayList argList = null;
+		String schema ="sdnctl";
+		String tableName ="dual";
+		String getselectData ="123";
+		String getDataClasue="123='123'";
+		d.getData(tableName, argList, schema, getselectData, getDataClasue);
+	}
+	@Test(expected=Exception.class)
+	public void testupdateDB() throws Exception
+	{
+		DbServiceUtil d = new DbServiceUtil();
+		String setCluase = null;
+		String schema ="sdnctl";
+		String tableName ="dual";
+		ArrayList inputArgs = null;
+		String whereClause="123='123'";
+		d.updateDB(tableName, inputArgs, schema, whereClause, setCluase);
+	}
 }
