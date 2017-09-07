6668a459a6467a78a8952699d3201b0fac8c2417
Skip Wonnell <skip@att.com>
2017-09-07 14:42:29 -0500

    Fix sonar block in Encryption Tool
    Fix blockers and other Sonar findings.
    Update license text
    
    Issue-ID: APPC-186
    Change-Id: Ib45020d85bde95f79b112ebd85fedfe62469b58f
    Signed-off-by: Skip Wonnell <skip@att.com>

--- appc-config/appc-encryption-tool/provider/src/main/java/org/openecomp/appc/encryptiontool/wrapper/EncryptionToolDGWrapper.java
+++ appc-config/appc-encryption-tool/provider/src/main/java/org/openecomp/appc/encryptiontool/wrapper/EncryptionToolDGWrapper.java
@@ -1,9 +1,11 @@
 /*-
  * ============LICENSE_START=======================================================
- * ONAP : APP-C
+ * ONAP : APPC
  * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property.  All rights reserved.
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
+ * Copyright (C) 2017 Amdocs
+ * =============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,6 +17,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * 
+ * ECOMP is a trademark and service mark of AT&T Intellectual Property.
  * ============LICENSE_END=========================================================
  */
 
@@ -24,56 +28,51 @@ import java.util.Map;
 
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.commons.lang.StringUtils;
-
-import com.att.eelf.configuration.EELFLogger;
-import com.att.eelf.configuration.EELFManager;
 import org.onap.ccsdk.sli.core.sli.SvcLogicContext;
 import org.onap.ccsdk.sli.core.sli.SvcLogicException;
 import org.onap.ccsdk.sli.core.sli.SvcLogicJavaPlugin;
+import com.att.eelf.configuration.EELFLogger;
+import com.att.eelf.configuration.EELFManager;
 
 public class EncryptionToolDGWrapper implements SvcLogicJavaPlugin {
 
     private static final EELFLogger log = EELFManager.getInstance().getLogger(EncryptionToolDGWrapper.class);
 
-    public void runEncryption(Map<String, String> inParams, SvcLogicContext ctx) throws SvcLogicException
-    {
-        String responsePrefix = inParams.get("prefix");
+    public void runEncryption(Map<String, String> inParams, SvcLogicContext ctx) throws SvcLogicException {
         String userName = inParams.get("userName");
         String password = inParams.get("password");
-        String vnf_type = inParams.get("vnf_type");
+        String vnfType = inParams.get("vnf_type");
 
-        try{
-            responsePrefix = StringUtils.isNotBlank(responsePrefix) ? (responsePrefix+".") : ""  ;
-            if(StringUtils.isBlank(userName) || StringUtils.isBlank(password) || StringUtils.isBlank(vnf_type)){
-                throw new Exception("username or Password is missing");
+        try {
+            if (StringUtils.isBlank(userName) || StringUtils.isBlank(password) || StringUtils.isBlank(vnfType)) {
+                throw new SvcLogicException("username or Password is missing");
             }
 
-            String [] input = new String[] {vnf_type,userName,password};
+            String[] input = new String[] {vnfType, userName, password};
             WrapperEncryptionTool.main(input);
-        }
-        catch (Exception e)
-        {
+
+        } catch (Exception e) {
             throw new SvcLogicException(e.getMessage());
         }
 
     }
-    public void getProperty(Map<String, String> inParams, SvcLogicContext ctx) throws SvcLogicException
-    {
+
+    public void getProperty(Map<String, String> inParams, SvcLogicContext ctx) throws SvcLogicException {
         String responsePrefix = inParams.get("prefix");
         String propertyName = inParams.get("propertyName");
 
-        try{
-            responsePrefix = StringUtils.isNotBlank(responsePrefix) ? (responsePrefix+".") : ""  ;
-            PropertiesConfiguration conf = new PropertiesConfiguration(Constants.APPC_CONFIG_DIR + "/appc_southbound.properties");
+        try {
+            responsePrefix = StringUtils.isNotBlank(responsePrefix) ? (responsePrefix + ".") : "";
+            PropertiesConfiguration conf =
+                    new PropertiesConfiguration(Constants.APPC_CONFIG_DIR + "/appc_southbound.properties");
             conf.setBasePath(null);
             EncryptionTool et = EncryptionTool.getInstance();
 
             ctx.setAttribute(responsePrefix + "propertyName", et.decrypt(conf.getProperty(propertyName).toString()));
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             ctx.setAttribute(responsePrefix + "status", "failure");
             ctx.setAttribute(responsePrefix + "error-message", e.getMessage());
-            e.printStackTrace();
+            log.info("Caught exception", e);
             throw new SvcLogicException(e.getMessage());
         }
     }
--- appc-config/appc-encryption-tool/provider/src/main/java/org/openecomp/appc/encryptiontool/wrapper/WrapperEncryptionTool.java
+++ appc-config/appc-encryption-tool/provider/src/main/java/org/openecomp/appc/encryptiontool/wrapper/WrapperEncryptionTool.java
@@ -1,9 +1,11 @@
 /*-
  * ============LICENSE_START=======================================================
- * ONAP : APP-C
+ * ONAP : APPC
  * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property.  All rights reserved.
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
+ * Copyright (C) 2017 Amdocs
+ * =============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,128 +17,113 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * 
+ * ECOMP is a trademark and service mark of AT&T Intellectual Property.
  * ============LICENSE_END=========================================================
  */
 
 package org.openecomp.appc.encryptiontool.wrapper;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Properties;
 
 import javax.sql.rowset.CachedRowSet;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.onap.ccsdk.sli.core.dblib.DBResourceManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class WrapperEncryptionTool {
 
-    private static final Logger log = LoggerFactory
-            .getLogger(WrapperEncryptionTool.class);
+    private static final Logger log = LoggerFactory.getLogger(WrapperEncryptionTool.class);
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int rowCount = 0;
-        String vnf_type=args[0];
+        String vnfType = args[0];
         String user = args[1];
         String password = args[2];
         String action = args[3];
         String port = args[4];
         String url = args[5];
 
-        if("".equals(vnf_type))
-        {
-            System.out.println("ERROR-VNF_TYPE can not be null");
+        if ("".equals(vnfType)) {
+            log.info("ERROR-VNF_TYPE can not be null");
             return;
         }
-        if("".equals(user))
-        {
-            System.out.println("ERROR-USER can not be null");
+        if ("".equals(user)) {
+            log.info("ERROR-USER can not be null");
             return;
         }
-        if("".equals(password))
-        {
-            System.out.println("ERROR-PASSWORD can not be null");
+        if ("".equals(password)) {
+            log.info("ERROR-PASSWORD can not be null");
             return;
         }
 
-        EncryptionTool encryptionTool = EncryptionTool.getInstance();
-        String enPass = encryptionTool.encrypt(password);
+        EncryptionTool et = EncryptionTool.getInstance();
+        String enPass = et.encrypt(password);
 
-        if(action != null && !action.isEmpty()){
-            updateProperties(user,vnf_type , enPass, action, port, url);
-            return ;
+        if (action != null && !action.isEmpty()) {
+            updateProperties(user, vnfType, enPass, action, port, url);
+            return;
         }
 
         ArrayList<String> argList = new ArrayList<>();
-        argList.add(vnf_type);
+        argList.add(vnfType);
         argList.add(user);
-        String clause = " vnf_type = ? and user_name = ? ";
+        String clause = " vnfType = ? and user_name = ? ";
         String setClause = " password = ? ";
         String getselectData = " * ";
         DBResourceManager dbResourceManager = null;
-        try
-        {
+        try (CachedRowSet data = DbServiceUtil.getData(Constants.DEVICE_AUTHENTICATION, argList,
+                Constants.SCHEMA_SDNCTL, getselectData, clause);) {
             dbResourceManager = DbServiceUtil.initDbLibService();
-            CachedRowSet data = DbServiceUtil.getData(Constants.DEVICE_AUTHENTICATION, 
-                argList, Constants.SCHEMA_SDNCTL, getselectData,clause );
-            while(data.next())
-            {
-                rowCount ++;
+
+            while (data.next()) {
+                rowCount++;
             }
-            if(rowCount == 0)
-                log.info("APPC-MESSAGE: ERROR - No record Found for VNF_TYPE: " + vnf_type + ", User " + user );
-            else
-            {
+            if (rowCount == 0)
+                log.info("APPC-MESSAGE: ERROR - No record Found for VNF_TYPE: %, User % ", vnfType, user);
+            else {
                 argList.clear();
                 argList.add(enPass);
-                argList.add(vnf_type);
+                argList.add(vnfType);
                 argList.add(user);
-                DbServiceUtil.updateDB(Constants.DEVICE_AUTHENTICATION, argList, 
-                    Constants.SCHEMA_SDNCTL, clause, setClause);
+                DbServiceUtil.updateDB(Constants.DEVICE_AUTHENTICATION, argList, Constants.SCHEMA_SDNCTL, clause,
+                        setClause);
                 log.info("APPC-MESSAGE: Password Updated Successfully");
             }
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
+        } catch (Exception e) {
+            log.info("Caught exception", e);
             log.info("APPC-MESSAGE:" + e.getMessage());
-        }
-        finally
-        {
-            dbResourceManager.cleanUp();
-            System.exit(0);
+        } finally {
+            if (dbResourceManager != null) {
+                dbResourceManager.cleanUp();
+            }
         }
     }
 
-    private static void updateProperties(String user, String vnf_type, String password, 
-            String action, String port, String url) {
+    private static void updateProperties(String user, String vnfType, String password, String action, String port,
+            String url) {
 
-        log.info("Received Inputs User:" + user + " vnf_type:"  + vnf_type + " action:" + action );
+        log.info("Received Inputs User:%s vnfType:%s action:%s", user, vnfType, action);
+        String property = vnfType + "." + action + ".";
 
-        String property =  vnf_type + "." + action + ".";
 
         try {
-            PropertiesConfiguration conf = new PropertiesConfiguration(Constants.APPC_CONFIG_DIR  + "/appc_southbound.properties");
-            conf.setProperty(property + "user",  user);
-            if(port != null && !port.isEmpty() )
-                conf.setProperty(property + "port",  port);
-            if(password != null && !password.isEmpty() )
-                conf.setProperty(property + "password",  password);
-            if(url != null && !url.isEmpty() )
-            conf.setProperty(property + "url",  url);
+            PropertiesConfiguration conf =
+                    new PropertiesConfiguration(Constants.APPC_CONFIG_DIR + "/appc_southbound.properties");
+            conf.setProperty(property + "user", user);
+            if (port != null && !port.isEmpty())
+                conf.setProperty(property + "port", port);
+            if (password != null && !password.isEmpty())
+                conf.setProperty(property + "password", password);
+            if (url != null && !url.isEmpty())
+                conf.setProperty(property + "url", url);
 
             conf.save();
 
+        } catch (Exception e) {
+            log.info("Caught Exception", e);
         }
-        catch (Exception e ) {
-            e.printStackTrace();
-            log.info("APPC-MESSAGE:" + e.getMessage());
-        }
-
     }
 }
