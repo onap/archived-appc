15144a3a3ee2c3a59d4e1884446ec04b20287428
Marcus G K Williams <marcus.williams@intel.com>
2017-09-07 17:16:09 -0700

    Add Unit Tests for appc-chef-adapter
    Issue-Id: APPC-181
    Change-Id: Iee824ca929ea46944ac1673e709074687ad812f9
    Signed-off-by: Marcus G K Williams <marcus.williams@intel.com>

---
+++ appc-adapters/appc-chef-adapter/appc-chef-adapter-bundle/src/test/java/org/openecomp/appc/adapter/chef/chefclient/TestChefApiClient.java
@@ -0,0 +1,107 @@
+package org.openecomp.appc.adapter.chef.chefclient;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+import java.util.TimeZone;
+import java.util.regex.Pattern;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openecomp.appc.adapter.chef.chefapi.ApiMethod;
+import org.openecomp.appc.adapter.chef.chefapi.Delete;
+import org.openecomp.appc.adapter.chef.chefapi.Get;
+import org.openecomp.appc.adapter.chef.chefapi.Post;
+import org.openecomp.appc.adapter.chef.chefapi.Put;
+
+public class TestChefApiClient {
+
+    private ChefApiClient client;
+    private Properties props;
+
+    @Before
+    public void setup() throws IllegalArgumentException, IllegalAccessException {
+        props = new Properties();
+        InputStream propStr = getClass().getResourceAsStream("/test.properties");
+        if (propStr == null) {
+            fail("src/test/resources/test.properties missing");
+        }
+
+        try {
+            props.load(propStr);
+            propStr.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Could not initialize properties");
+        }
+        client = new ChefApiClient(
+                props.getProperty("org.openecomp.appc.adapter.chef.chefclient.userId"),
+                System.getProperty("user.dir") +
+                        props.getProperty("org.openecomp.appc.adapter.chef.chefclient.pemPath"),
+                props.getProperty("org.openecomp.appc.adapter.chef.chefclient.endPoint"),
+                props.getProperty("org.openecomp.appc.adapter.chef.chefclient.organizations"));
+    }
+
+    @Test
+    public void testGet(){
+        Get get = client.get(props.getProperty("org.openecomp.appc.adapter.chef.chefclient.path"));
+        ApiMethod method = get.createRequest();
+        String[] response = method.test.split("\n");
+
+        thenStringShouldMatch("GET", response);
+    }
+
+    @Test
+    public void testPut(){
+        Put put = client.put(props.getProperty("org.openecomp.appc.adapter.chef.chefclient.path"));
+        ApiMethod method = put.createRequest();
+        String[] response = method.test.split("\n");
+
+        thenStringShouldMatch("PUT", response);
+    }
+
+    @Test
+    public void testPost() {
+        Post post = client.post(props.getProperty("org.openecomp.appc.adapter.chef.chefclient.path"));
+        ApiMethod method = post.createRequest();
+        String[] response = method.test.split("\n");
+
+        thenStringShouldMatch("POST", response);
+    }
+
+    @Test
+    public void testDelete(){
+        Delete delete = client.delete(props.getProperty("org.openecomp.appc.adapter.chef.chefclient.path"));
+        ApiMethod method = delete.createRequest();
+        String[] response = method.test.split("\n");
+
+        thenStringShouldMatch("DELETE", response);
+    }
+
+    private String timestamp(){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+        String timeStamp = sdf.format(new Date());
+        timeStamp = timeStamp.replace(" ", "T");
+        timeStamp = timeStamp + "Z";
+        return timeStamp;
+    }
+
+    private void thenStringShouldMatch(String method, String[] response){
+        assertEquals("sb Method:" + method, response[0]);
+        assertEquals("Hashed Path:+JEk1y2gXwqZRweNjXYtx4ojxW8=", response[1]);
+        assertEquals("X-Ops-Content-Hash:2jmj7l5rSw0yVb/vlWAYkK/YBwk=", response[2]);
+        String timestamp = timestamp().substring(0, timestamp().length() - 3);
+        String regEx = "X-Ops-Timestamp:" +
+                 timestamp +
+                "...";
+        assertTrue(Pattern.matches(regEx, response[3]));
+        assertEquals("X-Ops-UserId:test", response[4]);
+    }
+}
