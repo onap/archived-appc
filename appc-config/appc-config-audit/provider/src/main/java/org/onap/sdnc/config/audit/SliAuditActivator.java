0abd431b629f7ce30484f284b05d49a6c7724bf2
Takamune Cho <tc012c@att.com>
2017-12-18 16:16:43 -0600

    Repackage appc-config-audit to onap
    Change-Id: I535761d86a0abb4a6555672710a414bd3f9bb362
    Issue-ID: APPC-13
    Signed-off-by: Takamune Cho <tc012c@att.com>

--- appc-config/appc-config-audit/provider/src/main/java/org/onap/sdnc/config/audit/SliAuditActivator.java
+++ appc-config/appc-config-audit/provider/src/main/java/org/onap/sdnc/config/audit/SliAuditActivator.java
@@ -1,9 +1,11 @@
 /*-
  * ============LICENSE_START=======================================================
- * ONAP : APP-C
+ * ONAP : APPC
  * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property.  All rights reserved.
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
+ * Copyright (C) 2017 Amdocs
+ * =============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,15 +17,17 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * 
+ * ECOMP is a trademark and service mark of AT&T Intellectual Property.
  * ============LICENSE_END=========================================================
  */
 
-package org.openecomp.sdnc.config.audit;
+package org.onap.sdnc.config.audit;
 
 import java.util.LinkedList;
 import java.util.List;
 
-import org.openecomp.sdnc.config.audit.node.CompareNode;
+import org.onap.sdnc.config.audit.node.CompareNode;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
