7be2f06aa804284910820be8bb466f24a15ffe23
Michal Kabaj <michal.kabaj@nokia.com>
2017-12-22 14:38:01 +0100

    Reformat code according to Java Code Guidelines
    Align appc-config-adaptor-provider implementation classes with ONAP
    and Google Java Style guidelines: K&R Style brackets, 120 column limit,
    indentation fixes, method and property reorder according to visibility.
    
    Change-Id: I079e6d9eec5f400db00c5bf41937ca20dd778e72
    Issue-ID: APPC-361
    Signed-off-by: Michal Kabaj <michal.kabaj@nokia.com>

--- appc-config/appc-config-adaptor/provider/src/main/java/org/onap/appc/ccadaptor/TelnetListener.java
+++ appc-config/appc-config-adaptor/provider/src/main/java/org/onap/appc/ccadaptor/TelnetListener.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,9 +20,10 @@
 
 package org.onap.appc.ccadaptor;
 
-public interface TelnetListener
-{
+public interface TelnetListener {
+    
     public void receivedString(String s);
+    
     public void filterStrings(String s1, String s2);
 }
 
