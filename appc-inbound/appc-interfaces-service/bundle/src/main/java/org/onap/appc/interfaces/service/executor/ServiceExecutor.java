e2bd0a6428cc424dbb80d3d6748a67acb695fcd5
Dilip kumar Pampana <dp583p@att.com>
2018-01-08 15:08:21 -0500

    Appc Interface Services
    Scope Overlap API Or REST service
    
    Issue-ID: APPC-348
    Change-Id: Id32b4c88f57a9f9ce2234598d03dcca5946d4683
    Signed-off-by: Dilip kumar Pampana <dp583p@att.com>

---
+++ appc-inbound/appc-interfaces-service/bundle/src/main/java/org/onap/appc/interfaces/service/executor/ServiceExecutor.java
@@ -0,0 +1,79 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : APPC
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Copyright (C) 2017 Amdocs
+ * =============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * ECOMP is a trademark and service mark of AT&T Intellectual Property.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.appc.interfaces.service.executor;
+
+import java.io.IOException;
+
+import org.onap.appc.interfaces.service.InterfacesServiceProviderImpl;
+import org.onap.appc.interfaces.service.executorImpl.ServiceExecutorImpl;
+import org.onap.appc.interfaces.service.utils.ServiceConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+public class ServiceExecutor {
+
+    private static final Logger log = LoggerFactory.getLogger(ServiceExecutor.class);
+    public String execute(String action, String requestData, String requestDataType) throws Exception{
+        String response = null;
+        log.info("Received execute request for action : " + action + "  with Payload : " + requestData);
+        try{
+        RequestValidator.validate(action, requestData, requestData);
+        switch (action) {
+        case ServiceConstants.REQUESTOVERLAP:
+            response = isRequestOverLap(requestData);
+            break;
+        case ServiceConstants.GEDATABYMODEL:
+            response = getDataByModel(action, requestData, requestDataType);
+            break;
+        default:
+            throw new Exception(" Action " + action + " not found while processing request ");
+        }
+    }catch(Exception e){
+        log.info("Error while checking for ScopeOverlap " + e.getMessage());
+        e.printStackTrace();
+        throw e;
+    }
+        return response;
+    }
+
+    private String getDataByModel(String action, String requestData, String requestDataType) {
+
+        return null;
+    }
+
+    private String isRequestOverLap(String requestData) throws Exception {
+
+        ServiceExecutorImpl serviceExecutor = new ServiceExecutorImpl();
+        try {
+            return serviceExecutor.isRequestOverLap(requestData);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+}
