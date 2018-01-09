8a10978ba382816c1497639034617928a2c003f8
Dilip kumar Pampana <dp583p@att.com>
2018-01-09 17:24:35 -0500

    Network Inventory Client Updates
    Supporting IPv4/v6 Subnet and Gateway Addresses From INSTAR,Added code to process A&AI as source in PD file,New Requirement to extract vnfc information for single vserver for VM level operations
    
    Issue-ID: APPC-351
    Change-Id: I5c91d03a64fc6987e58ab48681f51dc5795bed07
    Signed-off-by: Dilip kumar Pampana <dp583p@att.com>

--- appc-outbound/appc-network-inventory-client/provider/src/main/java/org/onap/appc/aai/data/AaiVmInfo.java
+++ appc-outbound/appc-network-inventory-client/provider/src/main/java/org/onap/appc/aai/data/AaiVmInfo.java
@@ -9,40 +9,49 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ * 
  * ECOMP is a trademark and service mark of AT&T Intellectual Property.
  * ============LICENSE_END=========================================================
  */
 
-package org.onap.appc.instar.node;
+package org.onap.appc.aai.data;
 
-import static org.junit.Assert.assertTrue;
+import java.util.ArrayList;
 
-import java.util.HashMap;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.onap.appc.instar.dme2client.Dme2Client;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.modules.junit4.PowerMockRunner;
-
-@RunWith(PowerMockRunner.class)
-public class TestDme2Client {
-
-    @Test(expected=Exception.class)
-    public void testSendtoInstar() throws Exception {
-
-        HashMap<String,String> data = new HashMap<String,String>();
-        data.put("subtext","value");
-        PowerMockito.mockStatic(System.class);
-        PowerMockito.when((System.getenv("test"))).thenReturn("test");
-        Dme2Client dme2 = new Dme2Client("opt","subtext",data);
+public class AaiVmInfo {
+    public String getVserverId() {
+        return vserverId;
     }
+
+    public void setVserverId(String vserverId) {
+        this.vserverId = vserverId;
+    }
+
+    public String getVserverName() {
+        return vserverName;
+    }
+
+    public void setVserverName(String vserverName) {
+        this.vserverName = vserverName;
+    }
+
+    public ArrayList<AaiVnfcInfo> getVnfcInfo() {
+        return vnfcInfo;
+    }
+
+    public void setVnfcInfo(ArrayList<AaiVnfcInfo> vnfcInfo) {
+        this.vnfcInfo = vnfcInfo;
+    }
+
+    String vserverId;
+    String vserverName;
+    ArrayList<AaiVnfcInfo> vnfcInfo;
 }
--- appc-outbound/appc-network-inventory-client/provider/src/main/java/org/onap/appc/aai/data/AaiVnfInfo.java
+++ appc-outbound/appc-network-inventory-client/provider/src/main/java/org/onap/appc/aai/data/AaiVnfInfo.java
@@ -9,40 +9,60 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ * 
  * ECOMP is a trademark and service mark of AT&T Intellectual Property.
  * ============LICENSE_END=========================================================
  */
 
-package org.onap.appc.instar.node;
+package org.onap.appc.aai.data;
 
-import static org.junit.Assert.assertTrue;
+import java.util.ArrayList;
 
-import java.util.HashMap;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.onap.appc.instar.dme2client.Dme2Client;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.modules.junit4.PowerMockRunner;
+public class AaiVnfInfo {
 
-@RunWith(PowerMockRunner.class)
-public class TestDme2Client {
+    String vnfId;
 
-    @Test(expected=Exception.class)
-    public void testSendtoInstar() throws Exception {
-
-        HashMap<String,String> data = new HashMap<String,String>();
-        data.put("subtext","value");
-        PowerMockito.mockStatic(System.class);
-        PowerMockito.when((System.getenv("test"))).thenReturn("test");
-        Dme2Client dme2 = new Dme2Client("opt","subtext",data);
+    public String getVnfId() {
+        return vnfId;
     }
+
+    public void setVnfId(String vnfId) {
+        this.vnfId = vnfId;
+    }
+
+    public String getVnfName() {
+        return vnfName;
+    }
+
+    public void setVnfName(String vnfName) {
+        this.vnfName = vnfName;
+    }
+
+    public String getVnfOamIpAddress() {
+        return vnfOamIpAddress;
+    }
+
+    public void setVnfOamIpAddress(String vnfOamIpAddress) {
+        this.vnfOamIpAddress = vnfOamIpAddress;
+    }
+
+    public ArrayList<AaiVmInfo> getVmInfo() {
+        return vmInfo;
+    }
+
+    public void setVmInfo(ArrayList<AaiVmInfo> vmInfo) {
+        this.vmInfo = vmInfo;
+    }
+
+    String vnfName;
+    String vnfOamIpAddress;
+    ArrayList<AaiVmInfo> vmInfo;
 }
