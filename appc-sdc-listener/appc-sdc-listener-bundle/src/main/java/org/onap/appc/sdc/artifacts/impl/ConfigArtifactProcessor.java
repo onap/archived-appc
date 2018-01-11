a83502e0c431f97f021743dc645083c4ab02080d
Skip Wonnell <skip@att.com>
2018-01-10 18:13:30 -0600

    Sonar blocker issue resolution
    Sonar issue resolutions for ConfigArtifactProcessor.java
    
    Resolve this blocker issue:
    
    https://sonar.onap.org/issues/search#issues=AWDHkCDV-08if2a6w3sT
    
    And other sonar findings:
    
    https://sonar.onap.org/issues/search#issues=AWDHkCDV-08if2a6w3sU
    https://sonar.onap.org/issues/search#issues=AWDHkCDU-08if2a6w3sR
    https://sonar.onap.org/issues/search#issues=AWDHkCDV-08if2a6w3sS
    
    Change-Id: I2c4f2bb38a5d1fc07fd13c5220eac9fa16afe04a
    Issue-ID: APPC-386
    Signed-off-by: Skip Wonnell <skip@att.com>

--- appc-sdc-listener/appc-sdc-listener-bundle/src/main/java/org/onap/appc/sdc/artifacts/impl/ConfigArtifactProcessor.java
+++ appc-sdc-listener/appc-sdc-listener-bundle/src/main/java/org/onap/appc/sdc/artifacts/impl/ConfigArtifactProcessor.java
@@ -9,37 +9,35 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * ECOMP is a trademark and service mark of AT&T Intellectual Property.
  * ============LICENSE_END=========================================================
  */
 
 package org.onap.appc.sdc.artifacts.impl;
 
-import org.apache.commons.lang.StringUtils;
+import java.net.MalformedURLException;
+import java.net.URI;
 import org.onap.appc.adapter.message.EventSender;
+import org.onap.appc.exceptions.APPCException;
+import org.onap.appc.sdc.artifacts.object.SDCArtifact;
 import org.onap.appc.sdc.listener.ProviderOperations;
 import org.onap.appc.sdc.listener.ProviderResponse;
 import org.onap.appc.sdc.listener.Util;
-import org.onap.appc.exceptions.APPCException;
-import com.att.eelf.configuration.EELFLogger;
-import com.att.eelf.configuration.EELFManager;
-import org.onap.appc.sdc.artifacts.object.SDCArtifact;
 import org.openecomp.sdc.api.IDistributionClient;
 import org.openecomp.sdc.api.notification.IArtifactInfo;
 import org.openecomp.sdc.api.notification.INotificationData;
 import org.openecomp.sdc.api.notification.IResourceInstance;
-
-import java.net.MalformedURLException;
-import java.net.URI;
+import com.att.eelf.configuration.EELFLogger;
+import com.att.eelf.configuration.EELFManager;
 
 /**
  * Artifact processor for config artifact type
@@ -50,6 +48,7 @@ public class ConfigArtifactProcessor extends AbstractArtifactProcessor {
 
     /**
      * returns an instance of ConfigArtifactProcessor
+     *
      * @param client an instance of IDistributionClient
      * @param eventSender an instance of EventSender
      * @param notification an instance of INotificationData
@@ -57,31 +56,36 @@ public class ConfigArtifactProcessor extends AbstractArtifactProcessor {
      * @param artifact an instance of IArtifactInfo
      * @param storeUri an instance of URI
      */
-    public ConfigArtifactProcessor(IDistributionClient client, EventSender eventSender, INotificationData notification, IResourceInstance resource, IArtifactInfo artifact, URI storeUri) {
-        super(client,eventSender,notification,resource,artifact,storeUri);
+    public ConfigArtifactProcessor(IDistributionClient client, EventSender eventSender, INotificationData notification,
+            IResourceInstance resource, IArtifactInfo artifact, URI storeUri) {
+        super(client, eventSender, notification, resource, artifact, storeUri);
     }
 
     @Override
     public void processArtifact(SDCArtifact artifact) throws APPCException {
-        String postData = Util.toSdcStoreDocumentInput(notification, resource, super.artifact, artifact.getArtifactContent());
-        logger.debug("ConfigArtifactProcessor::processArtifact::postData="+postData);
+        String postData =
+                Util.toSdcStoreDocumentInput(notification, resource, super.artifact, artifact.getArtifactContent());
+        logger.debug("ConfigArtifactProcessor::processArtifact::postData=" + postData);
         try {
-            ProviderOperations providerOperations = new ProviderOperations();
-            if (null != storeUri)
-               logger.debug("ConfigArtifactProcessor::processArtifact::URI is"+storeUri.toString());
-            ProviderResponse result = providerOperations.post(storeUri.toURL(), postData, null);
-            if (result.getStatus() == 200) {
-                logger.debug("ConfigArtifactProcessor::processArtifact::post request success!!");
-                Util.parseResponse(result.getBody());
-            }
-            else {
-                logger.debug("ConfigArtifactProcessor::processArtifact()::post request failed!! Returned :"
-                    +result.getStatus()+"-Result body- "+result.getBody());
-                throw new APPCException("ConfigArtifactProcessor::processArtifact: Invalid status retrurned from post "+result.getStatus());
+            if (null != storeUri) {
+                logger.debug("ConfigArtifactProcessor::processArtifact::URI is" + storeUri.toString());
+                ProviderResponse result = ProviderOperations.post(storeUri.toURL(), postData, null);
+                if (result.getStatus() == 200) {
+                    logger.debug("ConfigArtifactProcessor::processArtifact::post request success!!");
+                    Util.parseResponse(result.getBody());
+                } else {
+                    logger.debug("ConfigArtifactProcessor::processArtifact()::post request failed!! Returned :"
+                            + result.getStatus() + "-Result body- " + result.getBody());
+                    throw new APPCException(
+                            "ConfigArtifactProcessor::processArtifact: Invalid status retrurned from post "
+                                    + result.getStatus());
+                }
+            } else {
+                logger.error("ConfigArtifactProcessor::processArtifact, storeURI is null");
             }
         } catch (MalformedURLException | APPCException e) {
-            logger.error("Error processing artifact : " + this.artifact.toString(),e);
-            throw new APPCException(e.getMessage(),e);
+            logger.error("Error processing artifact : " + this.artifact.toString(), e);
+            throw new APPCException(e.getMessage(), e);
         }
     }
 }
