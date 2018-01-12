958dd154c835c22a13f385b24f336034e7380370
Skip Wonnell <skip@att.com>
2018-01-12 10:17:57 -0600

    Add/update license text part 3
    Convert some file formats from dos to unix
    
    Change-Id: Iab31d84016c84bf03df7fdc8cf0c163c23091a88
    Issue-ID: APPC-398
    Issue-ID: APPC-305
    Signed-off-by: Skip Wonnell <skip@att.com>

--- appc-config/appc-data-services/provider/src/main/java/org/onap/appc/data/services/db/GeneralDataService.java
+++ appc-config/appc-data-services/provider/src/main/java/org/onap/appc/data/services/db/GeneralDataService.java
@@ -1,23 +1,26 @@
 /*-
  * ============LICENSE_START=======================================================
- * ONAP : APP-C
+ * ONAP : APPC
  * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property.  All rights reserved.
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
+ * Copyright (C) 2017 Amdocs
+ * =============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * ECOMP is a trademark and service mark of AT&T Intellectual Property.
  * ============LICENSE_END=========================================================
  */
-
 package org.onap.appc.data.services.db;
 
 import java.util.Map;
@@ -39,42 +42,42 @@ import com.att.eelf.configuration.EELFManager;
 
 public class GeneralDataService {
 
-	private static final EELFLogger log = EELFManager.getInstance().getLogger(GeneralDataService.class);
-	
-	public void saveTransactionLog(Map<String, String> inParams, SvcLogicContext ctx) throws SvcLogicException 
-	{
-		SvcLogicContext logger = new SvcLogicContext();
-		String responsePrefix = inParams.get(AppcDataServiceConstant.INPUT_PARAM_RESPONSE_PREFIX);
-		String messageType = inParams.get(AppcDataServiceConstant.INPUT_PARAM_MESSAGE_TYPE);
-		String message = inParams.get(AppcDataServiceConstant.INPUT_PARAM_MESSAGE);
-		try 
-		{
-			
-			String escapedMessage = EscapeUtils.escapeSql(message);
-			logger.setAttribute("request-id", ctx.getAttribute("request-id"));
-			logger.setAttribute("log-message-type", messageType);
-			logger.setAttribute("log-message", escapedMessage);
+    private static final EELFLogger log = EELFManager.getInstance().getLogger(GeneralDataService.class);
+    
+    public void saveTransactionLog(Map<String, String> inParams, SvcLogicContext ctx) throws SvcLogicException 
+    {
+        SvcLogicContext logger = new SvcLogicContext();
+        String responsePrefix = inParams.get(AppcDataServiceConstant.INPUT_PARAM_RESPONSE_PREFIX);
+        String messageType = inParams.get(AppcDataServiceConstant.INPUT_PARAM_MESSAGE_TYPE);
+        String message = inParams.get(AppcDataServiceConstant.INPUT_PARAM_MESSAGE);
+        try 
+        {
+            
+            String escapedMessage = EscapeUtils.escapeSql(message);
+            logger.setAttribute("request-id", ctx.getAttribute("request-id"));
+            logger.setAttribute("log-message-type", messageType);
+            logger.setAttribute("log-message", escapedMessage);
 
-			responsePrefix = StringUtils.isNotBlank(responsePrefix) ? (responsePrefix+".") : "";
-			DGGeneralDBService db = DGGeneralDBService.initialise();
-			QueryStatus status = db.saveConfigTransactionLog( logger, responsePrefix);
+            responsePrefix = StringUtils.isNotBlank(responsePrefix) ? (responsePrefix+".") : "";
+            DGGeneralDBService db = DGGeneralDBService.initialise();
+            QueryStatus status = db.saveConfigTransactionLog( logger, responsePrefix);
 
-			logger.setAttribute("log-message", null);
-			logger.setAttribute("log-message-type", null);
-			logger.setAttribute("request-id", null);
+            logger.setAttribute("log-message", null);
+            logger.setAttribute("log-message-type", null);
+            logger.setAttribute("request-id", null);
 
-			if (status == QueryStatus.FAILURE)
-				throw new Exception("Unable to insert into config_transaction_log");
+            if (status == QueryStatus.FAILURE)
+                throw new Exception("Unable to insert into config_transaction_log");
 
 
-		} 
-		catch (Exception e) 
-		{
-			ctx.setAttribute(responsePrefix + AppcDataServiceConstant.OUTPUT_PARAM_STATUS,
-			AppcDataServiceConstant.OUTPUT_STATUS_FAILURE);
-			ctx.setAttribute(responsePrefix + AppcDataServiceConstant.OUTPUT_PARAM_ERROR_MESSAGE, e.getMessage());
-			throw new SvcLogicException(e.getMessage());
-		}
-	}
-	
+        } 
+        catch (Exception e) 
+        {
+            ctx.setAttribute(responsePrefix + AppcDataServiceConstant.OUTPUT_PARAM_STATUS,
+            AppcDataServiceConstant.OUTPUT_STATUS_FAILURE);
+            ctx.setAttribute(responsePrefix + AppcDataServiceConstant.OUTPUT_PARAM_ERROR_MESSAGE, e.getMessage());
+            throw new SvcLogicException(e.getMessage());
+        }
+    }
+    
 }
