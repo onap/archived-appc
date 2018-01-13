804d472626b2abeffbca302a4d719f5b76f2ab11
Skip Wonnell <skip@att.com>
2018-01-13 12:11:11 -0600

    Code hardening for Ansible Bundle
    Fix all Sonar issues in AnsibleActivator.java and AnsibleAdapter.java
    
    Format code to ONAP style
    
    Issue-ID: APPC-401
    Change-Id: Ic628684711e0265f599b9409023a98625fcddc81
    Signed-off-by: Skip Wonnell <skip@att.com>

--- appc-adapters/appc-ansible-adapter/appc-ansible-adapter-bundle/src/main/java/org/onap/appc/adapter/ansible/AnsibleAdapter.java
+++ appc-adapters/appc-ansible-adapter/appc-ansible-adapter-bundle/src/main/java/org/onap/appc/adapter/ansible/AnsibleAdapter.java
@@ -9,15 +9,15 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * ECOMP is a trademark and service mark of AT&T Intellectual Property.
  * ============LICENSE_END=========================================================
  */
@@ -25,26 +25,21 @@
 package org.onap.appc.adapter.ansible;
 
 import java.util.Map;
-
-import org.onap.appc.exceptions.APPCException;
 import org.onap.ccsdk.sli.core.sli.SvcLogicContext;
-import org.onap.ccsdk.sli.core.sli.SvcLogicJavaPlugin;
 import org.onap.ccsdk.sli.core.sli.SvcLogicException;
+import org.onap.ccsdk.sli.core.sli.SvcLogicJavaPlugin;
 
 /**
- * This interface defines the operations that the Ansible  adapter exposes.
- * 
+ * This interface defines the operations that the Ansible adapter exposes.
+ *
  */
 public interface AnsibleAdapter extends SvcLogicJavaPlugin {
-
-
     /**
      * Returns the symbolic name of the adapter
-     * 
+     *
      * @return The adapter name
      */
     String getAdapterName();
-
 
     /* Method to post request for execution of Playbook */
     void reqExec(Map<String, String> params, SvcLogicContext ctx) throws SvcLogicException;
@@ -52,9 +47,6 @@ public interface AnsibleAdapter extends SvcLogicJavaPlugin {
     /* Method to get result of a playbook execution request */
     void reqExecResult(Map<String, String> params, SvcLogicContext ctx) throws SvcLogicException;
 
-
-    /* Method to get log  of a playbook execution request */
+    /* Method to get log of a playbook execution request */
     void reqExecLog(Map<String, String> params, SvcLogicContext ctx) throws SvcLogicException;
-
-
 }
