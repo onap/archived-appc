eb0e1dbb38ce5976f3c97b303e655de136283506
Jakub Dudycz <jakub.dudycz@nokia.com>
2018-01-25 10:36:12 +0100

    Fixed exception handling in ArtifactHandlerNode
    Change-Id: I81243ba0704b348995f135d2289135fd9e070928
    Issue-ID: APPC-498
    Signed-off-by: Jakub Dudycz <jakub.dudycz@nokia.com>

---
+++ appc-inbound/appc-artifact-handler/provider/src/main/java/org/onap/appc/artifact/handler/node/ArtifactHandlerInternalException.java
@@ -0,0 +1,12 @@
+package org.onap.appc.artifact.handler.node;
+
+class ArtifactHandlerInternalException extends Exception{
+
+    ArtifactHandlerInternalException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    ArtifactHandlerInternalException(String message) {
+        super(message);
+    }
+}
