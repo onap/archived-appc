5c404dad23777f5311bcb257e228005a44d428c0
Jakub Dudycz <jakub.dudycz@nokia.com>
2018-01-30 17:50:46 +0100

    ServiceCatalogV3 fixes
    Change-Id: I28d22a5d6206b4e4aaed3a017b4db39d0d5f0709
    Issue-ID: APPC-549
    Signed-off-by: Jakub Dudycz <jakub.dudycz@nokia.com>

--- appc-adapters/appc-iaas-adapter/appc-iaas-adapter-bundle/src/main/java/org/onap/appc/adapter/iaas/impl/ServiceCatalogV3.java
+++ appc-adapters/appc-iaas-adapter/appc-iaas-adapter-bundle/src/main/java/org/onap/appc/adapter/iaas/impl/ServiceCatalogV3.java
@@ -9,15 +9,15 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * ECOMP is a trademark and service mark of AT&T Intellectual Property.
  * ============LICENSE_END=========================================================
  */
@@ -30,6 +30,7 @@ import com.att.cdp.openstack.util.ExceptionMapper;
 import com.att.cdp.pal.util.Time;
 import com.att.cdp.zones.ContextFactory;
 import com.att.cdp.zones.spi.RequestState;
+import com.google.common.collect.Lists;
 import com.woorea.openstack.base.client.OpenStackBaseException;
 import com.woorea.openstack.base.client.OpenStackClientConnector;
 import com.woorea.openstack.base.client.OpenStackSimpleTokenProvider;
@@ -57,30 +58,21 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * This class is used to capture and cache the service catalog for a specific OpenStack provider.
- * <p>
- * This is needed because the way the servers are represented in the ECOMP product is as their fully qualified URL's.
- * This is very problematic, because we cant identify their region from the URL, URL's change, and we cant identify the
- * versions of the service implementations. In otherwords, the URL does not provide us enough information.
- * </p>
- * <p>
- * The zone abstraction layer is designed to detect the versions of the services dynamically, and step up or down to
- * match those reported versions. In order to do that, we need to know before hand what region we are accessing (since
- * the supported versions may be different by regions). We will need to authenticate to the identity service in order to
- * do this, plus we have to duplicate the code supporting proxies and trusted hosts that exists in the abstraction
- * layer, but that cant be helped.
- * </p>
- * <p>
- * What we do to circumvent this is connect to the provider using the lowest supported identity api, and read the entire
- * service catalog into this object. Then, we parse the vm URL to extract the host and port and match that to the
- * compute services defined in the catalog. When we find a compute service that has the same host name and port,
- * whatever region that service is supporting is the region for that server.
- * </p>
- * <p>
- * While we really only need to do this for compute nodes, there is no telling what other situations may arise where the
- * full service catalog may be needed. Also, there is very little additional cost (additional RAM) associated with
- * caching the full service catalog since there is no way to list only a portion of it.
- * </p>
+ * This class is used to capture and cache the service catalog for a specific OpenStack provider. <p> This is needed
+ * because the way the servers are represented in the ECOMP product is as their fully qualified URL's. This is very
+ * problematic, because we cant identify their region from the URL, URL's change, and we cant identify the versions of
+ * the service implementations. In otherwords, the URL does not provide us enough information. </p> <p> The zone
+ * abstraction layer is designed to detect the versions of the services dynamically, and step up or down to match those
+ * reported versions. In order to do that, we need to know before hand what region we are accessing (since the supported
+ * versions may be different by regions). We will need to authenticate to the identity service in order to do this, plus
+ * we have to duplicate the code supporting proxies and trusted hosts that exists in the abstraction layer, but that
+ * cant be helped. </p> <p> What we do to circumvent this is connect to the provider using the lowest supported identity
+ * api, and read the entire service catalog into this object. Then, we parse the vm URL to extract the host and port and
+ * match that to the compute services defined in the catalog. When we find a compute service that has the same host name
+ * and port, whatever region that service is supporting is the region for that server. </p> <p> While we really only
+ * need to do this for compute nodes, there is no telling what other situations may arise where the full service catalog
+ * may be needed. Also, there is very little additional cost (additional RAM) associated with caching the full service
+ * catalog since there is no way to list only a portion of it. </p>
  */
 public class ServiceCatalogV3 extends ServiceCatalog {
 
@@ -113,7 +105,7 @@ public class ServiceCatalogV3 extends ServiceCatalog {
      * {@inheritDoc}
      */
     public ServiceCatalogV3(String identityURL, String projectIdentifier, String principal, String credential,
-            String domain, Properties properties) {
+        String domain, Properties properties) {
         super(identityURL, projectIdentifier, principal, credential, domain, properties);
     }
 
@@ -132,7 +124,7 @@ public class ServiceCatalogV3 extends ServiceCatalog {
             connectorClass = Class.forName(CLIENT_CONNECTOR_CLASS);
             connector = (OpenStackClientConnector) connectorClass.newInstance();
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
-            e.printStackTrace();
+            logger.error("An error occurred when initializing ServiceCatalogV3", e);
             return;
         }
         Keystone keystone = new Keystone(identityURL, connector);
@@ -146,21 +138,14 @@ public class ServiceCatalogV3 extends ServiceCatalog {
         }
         if (trustedHosts != null) {
             keystone.getProperties().setProperty(com.woorea.openstack.common.client.Constants.TRUST_HOST_LIST,
-                    trustedHosts);
+                trustedHosts);
         }
 
         // create identity
         Identity identity = Identity.password(domain, principal, credential);
 
         // create scope
-        Scope scope = null;
-        if (projectIdentifier.length() == 32 && projectIdentifier.matches("[0-9a-fA-F]+")) { //$NON-NLS-1$
-            // authenticate = authenticate.withTenantId(projectIdentifier);
-            scope = Scope.project(projectIdentifier);
-        } else {
-            // authenticate = authenticate.withTenantName(projectIdentifier);
-            scope = Scope.project(domain, projectIdentifier);
-        }
+        Scope scope = initScope();
 
         Authentication authentication = new Authentication();
         authentication.setIdentity(identity);
@@ -186,8 +171,16 @@ public class ServiceCatalogV3 extends ServiceCatalog {
             parseServiceCatalog(token.getCatalog());
         } catch (OpenStackBaseException e) {
             ExceptionMapper.mapException(e);
-        } catch (Exception ex) {
-            throw new ContextConnectionException(ex.getMessage());
+        } catch (Exception e) {
+            throw new ContextConnectionException(e);
+        }
+    }
+
+    private Scope initScope() {
+        if (projectIdentifier.length() == 32 && projectIdentifier.matches("[0-9a-fA-F]+")) { //$NON-NLS-1$
+            return Scope.project(projectIdentifier);
+        } else {
+            return Scope.project(domain, projectIdentifier);
         }
     }
 
@@ -275,21 +268,19 @@ public class ServiceCatalogV3 extends ServiceCatalog {
             for (Endpoint endpoint : getEndpoints(ServiceCatalog.COMPUTE_SERVICE)) {
                 String endpointUrl = endpoint.getUrl();
                 Matcher matcher = urlPattern.matcher(endpointUrl);
-                if (matcher.matches()) {
-                    if (url.getHost().equals(matcher.group(1))) {
-                        if (url.getPort() != null) {
-                            if (!url.getPort().equals(matcher.group(2))) {
-                                continue;
-                            }
-                        }
-
-                        region = endpoint.getRegion();
-                        break;
-                    }
+                if (validateUrl(url, matcher)) {
+                    region = endpoint.getRegion();
+                    break;
                 }
             }
         }
         return region;
+    }
+
+    private boolean validateUrl(VMURL url, Matcher matcher) {
+        return matcher.matches()
+            && url.getHost().equals(matcher.group(1))
+            && (url.getPort() == null || url.getPort().equals(matcher.group(2)));
     }
 
     /**
@@ -317,22 +308,28 @@ public class ServiceCatalogV3 extends ServiceCatalog {
         lock.lock();
         try {
             builder.append(String.format("Service Catalog: tenant %s, id[%s]%n", project.getName(), //$NON-NLS-1$
-                    project.getId()));
+                project.getId()));
             if (regions != null && !regions.isEmpty()) {
                 builder.append(String.format("%d regions:%n", regions.size())); //$NON-NLS-1$
                 for (String region : regions) {
-                    builder.append("\t" + region + "%n"); //$NON-NLS-1$ //$NON-NLS-2$
+                    //$NON-NLS-1$ //$NON-NLS-2$
+                    builder
+                        .append("\t")
+                        .append(region)
+                        .append("%n");
                 }
             }
             builder.append(String.format("%d services:%n", serviceEndpoints.size())); //$NON-NLS-1$
-            for (String serviceType : serviceEndpoints.keySet()) {
-                List<Service.Endpoint> endpoints = serviceEndpoints.get(serviceType);
-                Service service = serviceTypes.get(serviceType);
+
+            for (Map.Entry<String, List<Service.Endpoint>> entry : serviceEndpoints.entrySet()) {
+                Service service = serviceTypes.get(entry.getKey());
 
                 builder.append(String.format("\t%s - %d endpoints%n", service.getType(), //$NON-NLS-1$
-                        endpoints.size()));
-                for (Service.Endpoint endpoint : endpoints) {
-                    builder.append(String.format("\t\tRegion [%s], public URL [%s]%n", endpoint.getRegion(), //$NON-NLS-1$
+                    entry.getValue().size()));
+
+                for (Service.Endpoint endpoint : entry.getValue()) {
+                    builder
+                        .append(String.format("\t\tRegion [%s], public URL [%s]%n", endpoint.getRegion(), //$NON-NLS-1$
                             endpoint.getUrl()));
                 }
             }
@@ -345,7 +342,7 @@ public class ServiceCatalogV3 extends ServiceCatalog {
 
     /**
      * Parses the service catalog and caches the results
-     * 
+     *
      * @param services The list of services published by this provider
      */
     private void parseServiceCatalog(List<Service> services) {
@@ -359,31 +356,30 @@ public class ServiceCatalogV3 extends ServiceCatalog {
             for (Service service : services) {
                 String type = service.getType();
                 serviceTypes.put(type, service);
-
-                List<Service.Endpoint> endpoints = service.getEndpoints();
-                for (Service.Endpoint endpoint : endpoints) {
-                    List<Service.Endpoint> endpointList = serviceEndpoints.get(type);
-                    if (endpointList == null) {
-                        endpointList = new ArrayList<>();
-                        serviceEndpoints.put(type, endpointList);
-                    }
-                    endpointList.add(endpoint);
-
-                    String region = endpoint.getRegion();
-                    if (!regions.contains(region)) {
-                        regions.add(region);
-                    }
-                }
+                addRegions(service, type);
             }
         } finally {
             lock.unlock();
         }
     }
 
+    private void addRegions(Service service, String type) {
+        List<Endpoint> endpoints = service.getEndpoints();
+        for (Endpoint endpoint : endpoints) {
+            serviceEndpoints.computeIfAbsent(type, val -> new ArrayList<>());
+            serviceEndpoints.get(type).add(endpoint);
+
+            String region = endpoint.getRegion();
+            if (!regions.contains(region)) {
+                regions.add(region);
+            }
+        }
+    }
+
     /**
      * Computes the local time when the access token will expire, after which we will need to re-login to access the
      * provider.
-     * 
+     *
      * @param accessKey The access key used to access the provider
      * @return The local time the key expires
      */
