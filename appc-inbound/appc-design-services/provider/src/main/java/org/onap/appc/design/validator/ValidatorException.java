75376a9f115758db1a6411113bb82fd55e919a72
Jakub Dudycz <jakub.dudycz@nokia.com>
2018-02-06 16:42:02 +0100

    ValidatorService sonar fixes
    Change-Id: I9b67413922f50051348cb8a6ca3bade6e40bed99
    Issue-ID: APPC-572
    Signed-off-by: Jakub Dudycz <jakub.dudycz@nokia.com>

---
+++ appc-inbound/appc-design-services/provider/src/main/java/org/onap/appc/design/validator/ValidatorException.java
@@ -0,0 +1,8 @@
+package org.onap.appc.design.validator;
+
+public class ValidatorException extends Exception{
+
+    public ValidatorException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
