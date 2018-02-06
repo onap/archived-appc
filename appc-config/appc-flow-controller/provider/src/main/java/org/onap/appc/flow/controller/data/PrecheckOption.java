b1f0aeaf5c5110826a1a904c16d1058d3a4f7b89
Skip Wonnell <skip@att.com>
2018-02-06 11:18:54 -0600

    Update java files with correct license text
    Change-Id: I95dab947ad10ec85ae4234aa3396dd374e313739
    Issue-ID: APPC-575
    Signed-off-by: Skip Wonnell <skip@att.com>

--- appc-config/appc-flow-controller/provider/src/main/java/org/onap/appc/flow/controller/data/PrecheckOption.java
+++ appc-config/appc-flow-controller/provider/src/main/java/org/onap/appc/flow/controller/data/PrecheckOption.java
@@ -1,42 +1,43 @@
 /*-
  * ============LICENSE_START=======================================================
- * ONAP : APP-C
+ * ONAP : APPC
  * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property.  All rights reserved.
- * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * =============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * ECOMP is a trademark and service mark of AT&T Intellectual Property.
  * ============LICENSE_END=========================================================
  */
-
 package org.onap.appc.flow.controller.data;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class PrecheckOption {
 
-    
+
     @JsonProperty("pre-transaction-id")
-    private int  pTransactionID ;
+    private int pTransactionID;
 
     @JsonProperty("param-name")
-    private String  paramName ;
-    
-    
+    private String paramName;
+
+
     @JsonProperty("param-value")
-    private String  paramValue ;
-    
+    private String paramValue;
+
     @JsonProperty("rule")
-    private String  rule ;
+    private String rule;
 
     public int getpTransactionID() {
         return pTransactionID;
@@ -62,15 +63,12 @@ public class PrecheckOption {
         this.paramValue = paramValue;
     }
 
-    
-
     @Override
     public String toString() {
         return "PrecheckOption [pTransactionID=" + pTransactionID + ", paramName=" + paramName + ", paramValue="
                 + paramValue + ", rule=" + rule + "]";
     }
 
-    
     public String getRule() {
         return rule;
     }
@@ -79,17 +77,17 @@ public class PrecheckOption {
         this.rule = rule;
     }
 
-	 @Override
-		public int hashCode() {
-		  final int prime = 31;
-		  int result = 1;
-		  result = prime * result + this.pTransactionID;
-		  result = prime * result + this.paramName.hashCode();
-		  result = prime * result + this.paramValue.hashCode();
-		  result = prime * result + this.rule.hashCode();
-		  return result;
-		}
-		
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + this.pTransactionID;
+        result = prime * result + this.paramName.hashCode();
+        result = prime * result + this.paramValue.hashCode();
+        result = prime * result + this.rule.hashCode();
+        return result;
+    }
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
@@ -115,6 +113,4 @@ public class PrecheckOption {
             return false;
         return true;
     }
-    
-    
 }
--- appc-config/appc-flow-controller/provider/src/main/java/org/onap/appc/flow/controller/node/JsonParsingNode.java
+++ appc-config/appc-flow-controller/provider/src/main/java/org/onap/appc/flow/controller/node/JsonParsingNode.java
@@ -1,23 +1,24 @@
 /*-
  * ============LICENSE_START=======================================================
- * ONAP : APP-C
+ * ONAP : APPC
  * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property.  All rights reserved.
- * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * =============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * ECOMP is a trademark and service mark of AT&T Intellectual Property.
  * ============LICENSE_END=========================================================
  */
-
 package org.onap.appc.flow.controller.node;
 
 import java.io.IOException;
@@ -50,21 +51,21 @@ public class JsonParsingNode implements SvcLogicJavaPlugin{
         String responsePrefix = inParams.get(FlowControllerConstants.INPUT_PARAM_RESPONSE_PRIFIX);
         try
         {
-            responsePrefix = StringUtils.isNotBlank(responsePrefix) ? (responsePrefix+".") : "";        
+            responsePrefix = StringUtils.isNotBlank(responsePrefix) ? (responsePrefix+".") : "";
             //Remove below for Block
             if(isValidJSON(inParams.get("data")) !=null){
                 JsonNode jnode = isValidJSON(inParams.get("data"));
                 ObjectMapper mapper = new ObjectMapper();
                 Map<String, Object> map = new HashMap<String, Object>();
-                map = mapper.readValue(jnode.toString(), new TypeReference<Map<String, String>>(){});                
+                map = mapper.readValue(jnode.toString(), new TypeReference<Map<String, String>>(){});
                 for (Entry<String, Object> entry : map.entrySet())
                 {
                     ctx.setAttribute(responsePrefix + entry.getKey(),(String) entry.getValue());
                 }
-            
+
             }
             ctx.setAttribute(responsePrefix + FlowControllerConstants.OUTPUT_PARAM_STATUS, FlowControllerConstants.OUTPUT_STATUS_SUCCESS);
-            
+
         } catch (Exception e) {
             ctx.setAttribute(responsePrefix + FlowControllerConstants.OUTPUT_PARAM_STATUS, FlowControllerConstants.OUTPUT_STATUS_FAILURE);
             ctx.setAttribute(responsePrefix + FlowControllerConstants.OUTPUT_PARAM_ERROR_MESSAGE, e.getMessage());
@@ -75,18 +76,18 @@ public class JsonParsingNode implements SvcLogicJavaPlugin{
     }
 
     public JsonNode isValidJSON(String json) throws IOException {
-        JsonNode output = null;     
+        JsonNode output = null;
         log.info("Received response from Interface " + json);
         if(json ==null  || json.isEmpty())
             return null;
-        try{ 
+        try{
             ObjectMapper objectMapper = new ObjectMapper();
             output = objectMapper.readTree(json);
         } catch(JsonProcessingException e){
             log.warn("Response received from interface is not a valid JSON block" + json);
             return null;
-        }    
-        
+        }
+
         return output;
     }
 }
