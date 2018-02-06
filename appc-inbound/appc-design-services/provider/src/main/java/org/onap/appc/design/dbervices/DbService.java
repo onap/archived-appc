4c0b17f783787206e2c1f6813859e467e841f262
Jakub Dudycz <jakub.dudycz@nokia.com>
2018-02-06 15:56:15 +0100

    DbService sonar fixes
    Change-Id: I3e9ed180c1c0d6be9182e65afc84dd2947404980
    Issue-ID: APPC-570
    Signed-off-by: Jakub Dudycz <jakub.dudycz@nokia.com>

--- appc-inbound/appc-design-services/provider/src/main/java/org/onap/appc/design/dbervices/DbService.java
+++ appc-inbound/appc-design-services/provider/src/main/java/org/onap/appc/design/dbervices/DbService.java
@@ -24,12 +24,14 @@
 
 package org.onap.appc.design.dbervices;
 
-import java.sql.ResultSet;
-import java.util.ArrayList;
+import static com.google.common.collect.Lists.newArrayList;
 
-import org.onap.ccsdk.sli.core.sli.provider.SvcLogicService;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 import org.onap.ccsdk.sli.core.dblib.DBResourceManager;
 import org.onap.ccsdk.sli.core.dblib.DbLibService;
+import org.onap.ccsdk.sli.core.sli.provider.SvcLogicService;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
@@ -41,20 +43,17 @@ public class DbService {
 
     private static final Logger Log = LoggerFactory.getLogger(DbService.class);
     private static final String DBLIB_SERVICE = "org.onap.ccsdk.sli.core.dblib.DBResourceManager";
-    DbLibService dblibSvc = null;
-    String errorMsg = null;
+    private DbLibService dbLibSvc = null;
 
-    public DbService() throws Exception {
-        DbLibService dblibSvc = null;
+    public DbService() throws DBDesignException {
         Log.info("Initializing DbService service");
         try
         {
-            dblibSvc = getDbLibService();
-            if (dblibSvc == null) {
+            dbLibSvc = getDbLibService();
+            if (dbLibSvc == null) {
                 Log.error("Got Exception While getting DB Connection");
-                throw new Exception("Got Exception While getting DB Connection");
+                throw new DBDesignException("Got Exception While getting DB Connection");
             }
-            this.dblibSvc = dblibSvc;
         }
         catch (Exception e) {
             Log.error(e.getMessage());
@@ -64,50 +63,50 @@ public class DbService {
 
     private static DbLibService getDbLibService() {
         
-        DbLibService dblibSvc = null;
-        BundleContext bctx = null;
-        ServiceReference sref = null;
+        DbLibService dbLibService = null;
+        BundleContext bundleContext = null;
+        ServiceReference serviceRef = null;
 
         Bundle bundle =  FrameworkUtil.getBundle(SvcLogicService.class);
 
         if (bundle != null) {
-            bctx = bundle.getBundleContext();
+            bundleContext = bundle.getBundleContext();
         }
 
-        if (bctx != null) {
+        if (bundleContext != null) {
             Log.debug("Getting bundle Context");
-            sref = bctx.getServiceReference(DBLIB_SERVICE);
+            serviceRef = bundleContext.getServiceReference(DBLIB_SERVICE);
         }
 
-        if (sref == null) {
+        if (serviceRef == null) {
             Log.warn("Could not find service reference for DBLib service");
                     
         } else {
-            dblibSvc = (DbLibService) bctx.getService(sref);
-            if (dblibSvc == null) {
+            dbLibService = (DbLibService)bundleContext.getService(serviceRef);
+            if (dbLibService == null) {
                 Log.warn("DBLIB_SERVICE is null");
             }
         }
-        if (dblibSvc == null) {
+        if (dbLibService == null) {
             try {
-                dblibSvc = new DBResourceManager(System.getProperties());
+                dbLibService = new DBResourceManager(System.getProperties());
             } catch (Exception e) {
                 Log.error("Caught exception trying to create db service", e);
             }
 
-            if (dblibSvc == null) {
+            if (dbLibService == null) {
                 Log.warn("Could not create new DBResourceManager");
             }
         }
-        return (dblibSvc);
+        return dbLibService;
     }
 
-    public ResultSet getDBData(String query) throws Exception {
+    public ResultSet getDBData(String query) throws SQLException {
         ResultSet resultSet;
         StringBuilder sqlBuilder = new StringBuilder(query);
         Log.info("Query: " + sqlBuilder.toString());
         try {
-            resultSet = dblibSvc.getData(sqlBuilder.toString(), null, null);
+            resultSet = dbLibSvc.getData(sqlBuilder.toString(), null, null);
         } catch (Exception e) {
             Log.error("SQL query "+sqlBuilder+" :: " + e.getMessage());
             throw e;
@@ -115,12 +114,12 @@ public class DbService {
         return resultSet;
     }
 
-    public ResultSet getDBData(String query, ArrayList<String> paramList) throws Exception {
+    public ResultSet getDBData(String query, List<String> paramList) throws SQLException {
         ResultSet resultSet;
         StringBuilder sqlBuilder = new StringBuilder(query);
         Log.info("Query :" + sqlBuilder.toString());
         try {
-            resultSet = dblibSvc.getData(sqlBuilder.toString(), paramList, null);
+            resultSet = dbLibSvc.getData(sqlBuilder.toString(), newArrayList(paramList), null);
         } catch (Exception expObj) {
             Log.error("query "+sqlBuilder+" :: " + expObj.getMessage());
             throw expObj;
@@ -128,12 +127,12 @@ public class DbService {
         return resultSet;
     }
     
-    public boolean updateDBData(String query, ArrayList<String> paramList) throws Exception {
+    public boolean updateDBData(String query, List<String> paramList) throws SQLException {
         boolean update;
         StringBuilder sqlBuilder = new StringBuilder(query);
         Log.info("Query :" + sqlBuilder.toString());
         try {
-            update = dblibSvc.writeData(sqlBuilder.toString(), paramList, null);
+            update = dbLibSvc.writeData(sqlBuilder.toString(), newArrayList(paramList), null);
         } catch (Exception expObj) {
             Log.error("query "+sqlBuilder+" :: " + expObj.getMessage());
             throw expObj;
