26444d69bdafd40d4982a895e1dde9df169cbf62
Skip Wonnell <skip@att.com>
2018-02-06 11:18:54 -0600

    Update java files with correct license text
    Change-Id: I95dab947ad10ec85ae4234aa3396dd374e313739
    Issue-ID: APPC-575
    Signed-off-by: Skip Wonnell <skip@att.com>

--- appc-config/appc-encryption-tool/provider/src/main/java/org/onap/appc/encryptiontool/wrapper/DbServiceUtil.java
+++ appc-config/appc-encryption-tool/provider/src/main/java/org/onap/appc/encryptiontool/wrapper/DbServiceUtil.java
@@ -1,20 +1,22 @@
 /*-
  * ============LICENSE_START=======================================================
- * ONAP : APP-C
+ * ONAP : APPC
  * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property.  All rights reserved.
- * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * =============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * ECOMP is a trademark and service mark of AT&T Intellectual Property.
  * ============LICENSE_END=========================================================
  */
 
@@ -41,7 +43,7 @@ public class DbServiceUtil
     private static Properties props;
     private static DBResourceManager jdbcDataSource = null;
 
-    public static boolean updateDB(String tableName, ArrayList inputArgs, 
+    public static boolean updateDB(String tableName, ArrayList inputArgs,
         String scema, String whereClause, String setCluase) throws SQLException
     {
             String updatePasswordString = "update " + tableName + " set " +  setCluase +  " where " + whereClause ;
@@ -49,7 +51,7 @@ public class DbServiceUtil
             return result;
     }
 
-    public static CachedRowSet getData(String tableName, ArrayList argList, String schema, 
+    public static CachedRowSet getData(String tableName, ArrayList argList, String schema,
         String getselectData, String getDataClasue ) throws SQLException
      {
          String selectQuery = "select " + getselectData + "from " + tableName + " where " + getDataClasue ;
--- appc-config/appc-flow-controller/provider/src/test/java/org/onap/appc/flow/executor/node/TestRestServiceNode.java
+++ appc-config/appc-flow-controller/provider/src/test/java/org/onap/appc/flow/executor/node/TestRestServiceNode.java
@@ -1,23 +1,25 @@
+
 /*-
  * ============LICENSE_START=======================================================
- * ONAP : APP-C
+ * ONAP : APPC
  * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property.  All rights reserved.
- * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * =============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * ECOMP is a trademark and service mark of AT&T Intellectual Property.
  * ============LICENSE_END=========================================================
  */
-
 package org.onap.appc.flow.executor.node;
 
 import java.io.InputStream;
@@ -41,10 +43,10 @@ import org.onap.appc.flow.controller.utils.FlowControllerConstants;
 import org.onap.ccsdk.sli.core.sli.SvcLogicContext;
 
 public class TestRestServiceNode {
-    
+
     @Test(expected=Exception.class)
     public void testRestServiceNode() throws Exception {
-        
+
         SvcLogicContext ctx = new SvcLogicContext();
         ctx.setAttribute(FlowControllerConstants.VNF_TYPE, "vUSP - vDBE-IPX HUB");
         ctx.setAttribute(FlowControllerConstants.REQUEST_ACTION, "healthcheck");
@@ -54,22 +56,22 @@ public class TestRestServiceNode {
         ctx.setAttribute("port-number","8888");
         ctx.setAttribute("request-action-type","GET");
         ctx.setAttribute("context", "loader/restconf/operations/appc-provider-lcm:health-check");
-        
+
         HashMap<String, String> inParams = new HashMap<String, String>();
         RestServiceNode rsn = new RestServiceNode();
         inParams.put("output-state", "state");
         inParams.put("responsePrefix", "healthcheck");
         rsn.sendRequest(inParams, ctx);
-        
+
         for (Object key : ctx.getAttributeKeySet()) {
             String parmName = (String) key;
             String parmValue = ctx.getAttribute(parmName);
         }
-        
-        
+
+
     }
-    
-    
+
+
     @Test(expected=Exception.class)
     public void testInputParamsRestServiceNode() throws Exception {
         SvcLogicContext ctx = new SvcLogicContext();
@@ -78,10 +80,10 @@ public class TestRestServiceNode {
         ctx.setAttribute("tmp.vnfInfo.vm[0].vnfc-count", "1");
         RestExecutor restExe = new RestExecutor();
         Transaction transaction = new Transaction();
-        
+
         FlowControlNode node = new FlowControlNode();
         HashMap<String,String>flowSeq= restExe.execute(transaction, ctx);
         String flowSequnce=flowSeq.get("restResponse");
-        
+
     }
 }
