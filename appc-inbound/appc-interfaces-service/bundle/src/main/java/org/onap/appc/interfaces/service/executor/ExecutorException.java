8a00259ecd00c56b5742a04322c8befd2c3b0042
Jakub Dudycz <jakub.dudycz@nokia.com>
2018-02-09 10:18:49 +0100

    ServiceExecutorImpl sonar fixes
    Change-Id: Ic86c01d34324dea9a6c21a7c29dffe647a91f040
    Issue-ID: APPC-586
    Signed-off-by: Jakub Dudycz <jakub.dudycz@nokia.com>

---
+++ appc-inbound/appc-interfaces-service/bundle/src/main/java/org/onap/appc/interfaces/service/executor/ExecutorException.java
@@ -0,0 +1,8 @@
+package org.onap.appc.interfaces.service.executor;
+
+public class ExecutorException extends Exception {
+
+    public ExecutorException(String message) {
+        super(message);
+    }
+}
