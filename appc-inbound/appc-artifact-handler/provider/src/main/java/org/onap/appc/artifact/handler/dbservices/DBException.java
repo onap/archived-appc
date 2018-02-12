e4d7d5eea7a87609692dc523fe110fd3c6ef750f
Jakub Dudycz <jakub.dudycz@nokia.com>
2018-02-12 14:47:37 +0100

    DBService sonar fixes
    Change-Id: I6c58803ab47ff411d7ff29097a7e64a489dfed03
    Issue-ID: APPC-605
    Signed-off-by: Jakub Dudycz <jakub.dudycz@nokia.com>

---
+++ appc-inbound/appc-artifact-handler/provider/src/main/java/org/onap/appc/artifact/handler/dbservices/DBException.java
@@ -0,0 +1,8 @@
+package org.onap.appc.artifact.handler.dbservices;
+
+public class DBException extends Exception {
+
+    public DBException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
