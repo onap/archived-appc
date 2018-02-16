28949b9c5d57e7d28d415473c0a27dd6f2becf00
shubhada <SV00449682@techmahindra.com>
2018-02-16 15:45:24 +0530

    Unit test coverage
    Unit test coverage for
    1.CommonHeader.java
    2.Flags.java
    3.RequestContext.java
    4.ResponseContext.java
    5.RuntimeContext.java
    
    Sonar Link:
    https://sonar.onap.org/code?id=org.onap.appc%3Aappc&selected=org.onap.appc%3Adomain-model-lib%3Asrc%2Fmain%2Fjava%2Forg%2Fonap%2Fappc%2Fdomainmodel%2Flcm
    
    Change-Id: I275495faa8c34f4b2174e93a56071298450c354b
    Issue-ID: APPC-640
    Signed-off-by: shubhada <SV00449682@techmahindra.com>

---
+++ appc-dispatcher/appc-dispatcher-common/domain-model-lib/src/test/java/org/onap/appc/domainmodel/lcm/TestCommonHeader.java
@@ -0,0 +1,91 @@
+/*
+* ============LICENSE_START=======================================================
+* ONAP : APPC
+* ================================================================================
+* Copyright 2018 TechMahindra
+*=================================================================================
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* ============LICENSE_END=========================================================
+*/
+package org.onap.appc.domainmodel.lcm;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.onap.appc.domainmodel.lcm.Flags.Mode;
+
+public class TestCommonHeader {
+    private CommonHeader commonHeader;
+
+    @Before
+    public void setUp() {
+        commonHeader =new  CommonHeader();
+    }
+
+    @Test
+    public void testgetRpcName() {
+        commonHeader.setApiVer("2.0");
+        Assert.assertNotNull(commonHeader.getApiVer());
+        Assert.assertEquals(commonHeader.getApiVer(), "2.0");
+    }
+
+    @Test
+    public void testGetOriginatorId() {
+        commonHeader.setOriginatorId("AAAA");
+        Assert.assertNotNull(commonHeader.getOriginatorId());
+        Assert.assertEquals(commonHeader.getOriginatorId(), "AAAA");
+    }
+
+    @Test
+    public void testGetRequestId() {
+        commonHeader.setRequestId("1111ABCD");
+        Assert.assertNotNull(commonHeader.getRequestId());
+        Assert.assertEquals(commonHeader.getRequestId(), "1111ABCD");
+    }
+
+    @Test
+    public void testGetSubRequestId() {
+        commonHeader.setSubRequestId("1111");
+        Assert.assertNotNull(commonHeader.getSubRequestId());
+        Assert.assertEquals(commonHeader.getSubRequestId(), "1111");
+    }
+
+    @Test
+    public void testGetFlags() {
+        Flags flags=new Flags();
+        flags.setTtl(60);
+        Mode mode=Mode.EXCLUSIVE;
+        flags.setMode(mode);
+        flags.setForce(false);
+        commonHeader.setFlags(flags);
+        Assert.assertNotNull(commonHeader.getFlags());
+        Assert.assertEquals(60,commonHeader.getFlags().getTtl());
+        Assert.assertEquals(Mode.EXCLUSIVE,commonHeader.getFlags().getMode());
+        Assert.assertEquals(false,commonHeader.getFlags().isForce());
+        
+    }
+    @Test
+    public void testToString_ReturnNonEmptyString() {
+        assertNotEquals(commonHeader.toString(), "");
+        assertNotEquals(commonHeader.toString(), null);
+    }
+
+    @Test
+    public void testToString_ContainsString() {
+        assertTrue(commonHeader.toString().contains("CommonHeader{flags"));
+    }
+
+}
---
+++ appc-dispatcher/appc-dispatcher-common/domain-model-lib/src/test/java/org/onap/appc/domainmodel/lcm/TestRequestContext.java
@@ -0,0 +1,75 @@
+/*
+* ============LICENSE_START=======================================================
+* ONAP : APPC
+* ================================================================================
+* Copyright 2018 TechMahindra
+*=================================================================================
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* ============LICENSE_END=========================================================
+*/
+package org.onap.appc.domainmodel.lcm;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestRequestContext {
+    private RequestContext requestContext;
+    private Map<String, String> testadditionalContext;
+
+    @Before
+    public void setUp() {
+        requestContext = new RequestContext();
+    }
+
+    @Test
+    public void testGetActionLevel_ValidEnumConstant() {
+        requestContext.setActionLevel(ActionLevel.VM);
+        Assert.assertNotNull(requestContext. getActionLevel());
+        Assert.assertEquals(requestContext. getActionLevel(),ActionLevel.VM);
+    }
+
+    @Test
+    public void testgetAdditionalContext() {
+        testadditionalContext=new HashMap<String, String>();
+        testadditionalContext.put("A", "a");
+        requestContext.setAdditionalContext(testadditionalContext);
+        Assert.assertNotNull(requestContext.getAdditionalContext());
+        Assert.assertTrue(requestContext.getAdditionalContext().containsKey("A"));
+        Assert.assertTrue(requestContext.getAdditionalContext().containsValue("a"));
+    }
+
+    @Test
+    public void testGetPayload() {
+        requestContext.setPayload("ABC:2000");
+        Assert.assertNotNull(requestContext.getPayload());
+        Assert.assertEquals(requestContext.getPayload(), "ABC:2000");
+    }
+
+    @Test
+    public void testToString_ReturnNonEmptyString() {
+        assertNotEquals(requestContext.toString(), "");
+        assertNotEquals(requestContext.toString(), null);
+    }
+
+    @Test
+    public void testToString_ContainsString() {
+        assertTrue(requestContext.toString().contains("RequestContext{commonHeader"));
+    }
+
+}
