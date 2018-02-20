815e98d2ec4dc933446a208ebcd64d5a8900db8c
Skip Wonnell <skip@att.com>
2018-02-20 14:42:34 -0600

    Update license text part 3
    Issue-ID: APPC-637
    Change-Id: I361c457acd27099294365f8ee3aeb167cd7fcafd
    Signed-off-by: Skip Wonnell <skip@att.com>

--- appc-client/client-simulator/src/main/resources/run.bat
+++ appc-client/client-simulator/src/main/resources/run.bat
@@ -1,2 +1,24 @@
+rem  ============LICENSE_START==========================================
+rem  ONAP : APPC
+rem  ===================================================================
+rem  Copyright (C) 2017-2018 AT&T Intellectual Property. All rights reserved.
+rem  ===================================================================
+rem
+rem  Unless otherwise specified, all software contained herein is licensed
+rem  under the Apache License, Version 2.0 (the License);
+rem  you may not use this software except in compliance with the License.
+rem  You may obtain a copy of the License at
+rem
+rem     http://www.apache.org/licenses/LICENSE-2.0
+rem
+rem  Unless required by applicable law or agreed to in writing, software
+rem  distributed under the License is distributed on an "AS IS" BASIS,
+rem  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem  See the License for the specific language governing permissions and
+rem  limitations under the License.
+rem
+rem  ECOMP is a trademark and service mark of AT&T Intellectual Property.
+rem  ============LICENSE_END============================================
+
 java -jar client-simulator-1.4.07-SNAPSHOT-jar-with-dependencies.jar C:\test\1\ JSON
 pause
