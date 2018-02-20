faae86dcfaab2d8ecc966540ab262bd10849fd01
Skip Wonnell <skip@att.com>
2018-02-20 14:42:34 -0600

    Update license text part 3
    Issue-ID: APPC-637
    Change-Id: I361c457acd27099294365f8ee3aeb167cd7fcafd
    Signed-off-by: Skip Wonnell <skip@att.com>

--- appc-inbound/appc-interfaces-service/model/scripts/python/yang2props.py
+++ appc-inbound/appc-interfaces-service/model/scripts/python/yang2props.py
@@ -1,3 +1,26 @@
+'''
+/*-
+* ============LICENSE_START=======================================================
+* ONAP : APPC
+* ================================================================================
+* Copyright (C) 2017-2018 AT&T Intellectual Property.  All rights reserved.
+* =============================================================================
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+* ECOMP is a trademark and service mark of AT&T Intellectual Property.
+* ============LICENSE_END=========================================================
+*/
+'''
 #!/usr/bin/python
 
 import re
@@ -28,7 +51,7 @@ with open(sys.argv[1], "r") as ins:
 	    match = re.search(r'leaf (\S+)', line)
 	    if match:
                  leaf = match.group(1)
-      
+
         # if we see enum convert the value to enum format and see if it changed
         # if the value is different write a property entry
         if "enum " in line:
@@ -54,4 +77,4 @@ fo.write("\n")
 # Close opend file
 fo.close()
 
-   
+
