2438306e0e4d9f8f9d8586dde7591b931edfb5e1
Jakub Dudycz <jakub.dudycz@nokia.com>
2018-02-21 15:24:46 +0100

    DGXML classes refactoring
    Did some refactor in org.onap.sdnc.dg.loader package in order to reduce code duplication
    
    Change-Id: I78c099e43f117fffbb06bc4834f1b266b34b88f0
    Issue-ID: APPC-654
    Signed-off-by: Jakub Dudycz <jakub.dudycz@nokia.com>

--- appc-directed-graph/dg-loader/provider/src/main/java/org/onap/sdnc/dg/loader/DGXMLLoad.java
+++ appc-directed-graph/dg-loader/provider/src/main/java/org/onap/sdnc/dg/loader/DGXMLLoad.java
@@ -24,81 +24,14 @@
 
 package org.onap.sdnc.dg.loader;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang.StringUtils;
-import org.onap.ccsdk.sli.core.sli.SvcLogicException;
-import org.onap.ccsdk.sli.core.sli.SvcLogicParser;
-import org.onap.ccsdk.sli.core.sli.SvcLogicStore;
-import org.onap.ccsdk.sli.core.sli.SvcLogicStoreFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DGXMLLoad {
 
-    private static final Logger logger = LoggerFactory.getLogger(DGXMLLoad.class);
-    public static final String STRING_ENCODING = "utf-8";
+    private static final Logger logger = LoggerFactory.getLogger(DGXMLLoader.class);
 
-    private final SvcLogicStore store;
-
-    public DGXMLLoad(String propfile) throws DGXMLException, SvcLogicException {
-        if (StringUtils.isBlank(propfile)) {
-            throw new DGXMLException(propfile + " Profile file is not defined");
-        }
-        this.store = SvcLogicStoreFactory.getSvcLogicStore(propfile);
-    }
-
-    protected DGXMLLoad(SvcLogicStore store) {
-        this.store = store;
-    }
-
-    public void loadDGXMLFile(String dgXMLpath) throws SvcLogicException {
-        if (dgXMLpath != null) {
-            SvcLogicParser.load(dgXMLpath, this.store);
-        }
-    }
-
-    private void loadDGXMLDir(String xmlPath)  {
-        try {
-            logger.info(
-                "******************** Loading DG into Database *****************************");
-            List<String> errors = new ArrayList<>();
-            if (this.store != null) {
-                File xmlDir = new File(xmlPath);
-                if (xmlDir.isDirectory()) {
-                    String[] extensions = new String[]{"xml", "XML"};
-                    List<File> files = (List<File>) FileUtils.listFiles(xmlDir, extensions, true);
-                    tryLoadXmls(errors, files);
-                } else {
-                    throw new DGXMLException(xmlPath + " is not a valid XML Directory");
-                }
-            } else {
-                throw new DGXMLException("Failed to initialise SvcLogicStore");
-            }
-
-            if (!errors.isEmpty()) {
-                throw new DGXMLException(errors.toString());
-            }
-        } catch (Exception e) {
-            logger.error("Failed to load DGXML directories", e);
-        }
-    }
-
-    private void tryLoadXmls(List<String> errors, List<File> files) throws IOException {
-        for (File file : files) {
-            logger.info("Loading DG XML file :" + file.getCanonicalPath());
-            try {
-                SvcLogicParser.load(file.getCanonicalPath(), store);
-            } catch (Exception e) {
-                logger.error("Failed to load XML " + file.getCanonicalPath(), e);
-                errors.add("Failed to load XML " + file.getCanonicalPath()
-                    + ", Exception : " + e.getMessage());
-            }
-        }
-    }
+    private DGXMLLoad(){}
 
     public static void main(String[] args) {
         try {
@@ -112,10 +45,10 @@ public class DGXMLLoad {
                 throw new DGXMLException(
                     "Sufficient inputs for DGXMLLoadNActivate are missing <xmlpath> <dbPropertyfile>");
             }
-            DGXMLLoad dgXMLLoadDB = new DGXMLLoad(propertyPath);
-            dgXMLLoadDB.loadDGXMLDir(xmlPath);
+            DGXMLLoader loader = new DGXMLLoader(propertyPath);
+            loader.loadDGXMLDir(xmlPath);
         } catch (Exception e) {
-           logger.error("Arguments missing", e);
+            logger.error("Arguments missing", e);
         } finally {
             System.exit(1);
         }
