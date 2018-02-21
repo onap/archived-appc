42eb3dcc0b9b3ce2cc1f28ef490e7438a096fbfb
vidhyasree <sn141y@att.com>
2018-02-21 11:19:33 -0500

    Task to add ConfigScaleOut to LCM API, Yang Model.
    A new action ConfigScaleOut is added to LCM API and Yang model.
    Removed old action item configscaleout supporting VM action level
    which is no longer needed.
    
    Issue-ID: APPC-479
    Change-Id: Ie5452f4d5625fc3be6b66c0f403d12baa64fa096
    Signed-off-by: vidhyasree <sn141y@att.com>

---
+++ appc-provider/appc-provider-bundle/src/main/java/org/onap/appc/provider/lcm/service/ConfigScaleOutService.java
@@ -0,0 +1,108 @@
+
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : APPC
+ * ================================================================================
+ * Copyright (C) 2017-2018 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Copyright (C) 2017 Amdocs
+ * =============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * ECOMP is a trademark and service mark of AT&T Intellectual Property.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.appc.provider.lcm.service;
+
+import org.opendaylight.yang.gen.v1.org.onap.appc.lcm.rev160108.Action;
+import org.opendaylight.yang.gen.v1.org.onap.appc.lcm.rev160108.ConfigScaleOutInput;
+import org.opendaylight.yang.gen.v1.org.onap.appc.lcm.rev160108.ConfigScaleOutOutputBuilder;
+import org.opendaylight.yang.gen.v1.org.onap.appc.lcm.rev160108.Payload;
+import org.opendaylight.yang.gen.v1.org.onap.appc.lcm.rev160108.action.identifiers.ActionIdentifiers;
+import org.opendaylight.yang.gen.v1.org.onap.appc.lcm.rev160108.common.header.CommonHeader;
+import org.onap.appc.executor.objects.LCMCommandStatus;
+import org.onap.appc.requesthandler.objects.RequestHandlerInput;
+import org.onap.appc.util.JsonUtil;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class ConfigScaleOutService extends AbstractBaseService {
+	/**
+     * Constructor
+     *
+     */  
+	public ConfigScaleOutService() {
+        super(Action.ConfigScaleOut);
+        logger.debug("ConfigScaleOutService starts");
+    }
+	public ConfigScaleOutOutputBuilder process(ConfigScaleOutInput input) {
+    	CommonHeader commonHeader = input.getCommonHeader();
+        ActionIdentifiers actionIdentifiers = input.getActionIdentifiers();
+        Payload payload = input.getPayload();
+
+        validate(commonHeader, input.getAction(), actionIdentifiers, payload);
+        if (status == null) {
+            proceedAction(commonHeader,actionIdentifiers,payload);
+        }
+
+        ConfigScaleOutOutputBuilder outputBuilder = new ConfigScaleOutOutputBuilder();
+        outputBuilder.setStatus(status);
+        outputBuilder.setCommonHeader(input.getCommonHeader());
+        return outputBuilder;
+    }
+
+
+    void validate(CommonHeader commonHeader,
+                  Action action,
+                  ActionIdentifiers actionIdentifiers,
+                  Payload payload) {
+        status = validateVnfId(commonHeader, action, actionIdentifiers);
+        if (status != null) {
+            return;
+        }
+
+        // validate payload
+        String keyName = "payload";
+        if (payload == null) {
+            status = buildStatusForParamName(LCMCommandStatus.MISSING_MANDATORY_PARAMETER, keyName);
+            return;
+        }
+        String payloadString = payload.getValue();
+        status = validateMustHaveParamValue(
+            payloadString == null ? payloadString : payloadString.trim(), "payload");
+        if (status != null) {
+            return;
+        }
+
+        try {
+            Map<String, String> payloadMap = JsonUtil.convertJsonStringToFlatMap(payloadString);
+            validateMustHaveParamValue(payloadMap.get(keyName), keyName);
+        } catch (IOException e) {
+            logger.error(String.format("ConfigScaleOutService (%s) got IOException when converting payload", rpcName), e);
+            status = buildStatusForErrorMsg(LCMCommandStatus.UNEXPECTED_ERROR, e.getMessage());
+        }
+    }
+
+    void proceedAction(CommonHeader commonHeader,
+                       ActionIdentifiers actionIdentifiers,
+                       Payload payload) {
+        RequestHandlerInput requestHandlerInput =
+            getRequestHandlerInput(commonHeader, actionIdentifiers, payload, this.getClass().getName());
+        if (requestHandlerInput != null) {
+            executeAction(requestHandlerInput);
+        }
+    }
+}
+
