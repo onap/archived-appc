f43ad427d92dc0a70641046eda27d97a9c6dc155
shubhada <SV00449682@techmahindra.com>
2018-03-01 19:09:38 +0530

    Unit test Coverage
    Unit Test Coverage for:
    1. Snapshot.java
    2. Volume.java
    3. Volume_.java
    4. Template.java
    5. SnapshotRestoreResponse.java
    
    Sonar Link:
    https://sonar.onap.org/code?id=org.onap.appc%3Aappc&selected=org.onap.appc%3Aappc-iaas-adapter-bundle%3Asrc%2Fmain%2Fjava%2Forg%2Fonap%2Fappc%2Fadapter%2Fopenstack%2Fheat%2Fmodel
    
    Change-Id: Ib5b0d80bd523f07a8255b34c6305e2de787d754b
    Issue-ID: APPC-686
    Signed-off-by: shubhada <SV00449682@techmahindra.com>

---
+++ appc-adapters/appc-iaas-adapter/appc-iaas-adapter-bundle/src/test/java/org/onap/appc/adapter/openstack/heat/model/TestSnapshot.java
@@ -0,0 +1,75 @@
+/*
+* ============LICENSE_START=======================================================
+* ONAP : APPC
+* ================================================================================
+* Copyright 2018 TechMahindra
+*=================================================================================
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* ============LICENSE_END=========================================================
+*/
+package org.onap.appc.adapter.openstack.heat.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestSnapshot {
+    private Snapshot snapshot;
+
+    @Before
+    public void setUp() {
+        snapshot=new Snapshot();
+    }
+
+    @Test
+    public void testGetBackupId() {
+        snapshot.setId("222");
+        assertNotNull(snapshot.getId());
+        assertEquals(snapshot.getId(),"222");
+    }
+
+    @Test
+    public void testGetName() {
+        snapshot.setName("ABC");
+        assertNotNull(snapshot.getName());
+        assertEquals(snapshot.getName(),"ABC");
+    }
+
+    @Test
+    public void testGetStatus() {
+        snapshot.setStatus("status");
+        assertNotNull(snapshot.getStatus());
+        assertEquals(snapshot.getStatus(),"status");
+    }
+
+    @Test
+    public void testGetStatusReason() {
+        snapshot.setStatusReason("statusReason");
+        assertNotNull(snapshot.getStatusReason());
+        assertEquals(snapshot.getStatusReason(),"statusReason");
+    }
+
+    @Test
+    public void testGetCreationTime() {
+        snapshot.setCreationTime("01-March-2018");
+        assertNotNull(snapshot.getCreationTime());
+        assertEquals(snapshot.getCreationTime(),"01-March-2018");
+    }
+
+    @Test
+    public void testToString_ReturnNonEmptyString() {
+        assertNotEquals(snapshot.toString(), "");
+        assertNotEquals(snapshot.toString(), null);
+    }
+}
---
+++ appc-adapters/appc-iaas-adapter/appc-iaas-adapter-bundle/src/test/java/org/onap/appc/adapter/openstack/heat/model/TestTemplate.java
@@ -0,0 +1,47 @@
+/*
+* ============LICENSE_START=======================================================
+* ONAP : APPC
+* ================================================================================
+* Copyright 2018 TechMahindra
+*=================================================================================
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* ============LICENSE_END=========================================================
+*/
+package org.onap.appc.adapter.openstack.heat.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestTemplate {
+    private Template template;
+
+    @Before
+    public void setUp() {
+        template=new Template();
+    }
+    @Test
+    public void testGetHeatTemplateVersion() {
+        template.setHeatTemplateVersion("1.0");
+        assertNotNull(template.getHeatTemplateVersion());
+        assertEquals(template.getHeatTemplateVersion(),"1.0");
+    }
+
+    @Test
+    public void testToString_ReturnNonEmptyString() {
+        assertNotEquals(template.toString(), "");
+        assertNotEquals(template.toString(), null);
+    }
+
+}
