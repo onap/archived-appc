89a36a48001e19f57a09bb432e7baf7e1034599d
amshegokar <AS00500801@techmahindra.com>
2018-03-05 14:52:28 +0530

    Unit Test Coverage Part:2
    Unit Test Coverage for:
    1. Snapshot.java
    2. Volume.java
    3. Volume_.java
    4. Template.java
    5. SnapshotRestoreResponse.java
    
    Sonar Link:
    https://sonar.onap.org/code?id=org.onap.appc%3Aappc&selected=org.onap.appc%3Aappc-iaas-adapter-bundle%3Asrc%2Fmain%2Fjava%2Forg%2Fonap%2Fappc%2Fadapter%2Fopenstack%2Fheat%2Fmodel
    
    Change-Id: I18ecc259973bbd3934f9ccd4cc20d897444d9d76
    Issue-ID: APPC-686
    Signed-off-by: amshegokar <AS00500801@techmahindra.com>

---
+++ appc-adapters/appc-iaas-adapter/appc-iaas-adapter-bundle/src/test/java/org/onap/appc/adapter/openstack/heat/model/TestVolume_.java
@@ -0,0 +1,75 @@
+/*
+* ============LICENSE_START=======================================================
+* ONAP : APPC
+* ================================================================================
+* Copyright 2018 TechMahindra
+*=================================================================================
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* ============LICENSE_END=========================================================
+*/
+package org.onap.appc.adapter.openstack.heat.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestVolume_ {
+    private Volume_ volume_;
+
+    @Before
+    public void setUp() {
+        volume_ = new Volume_();
+    }
+
+    @Test
+    public void testGetStatus() {
+        volume_.setStatus("Success");
+        assertNotNull(volume_.getStatus());
+        assertEquals(volume_.getStatus(), "Success");
+    }
+
+    @Test
+    public void testGetName() {
+        volume_.setName("XYZ");
+        assertNotNull(volume_.getName());
+        assertEquals(volume_.getName(), "XYZ");
+    }
+
+    @Test
+    public void testGetResourceId() {
+        volume_.setResourceId("333");
+        assertNotNull(volume_.getResourceId());
+        assertEquals(volume_.getResourceId(), "333");
+    }
+
+    @Test
+    public void testGetAction() {
+        volume_.setAction("action");
+        assertNotNull(volume_.getAction());
+        assertEquals(volume_.getAction(), "action");
+    }
+
+    @Test
+    public void testGetType() {
+        volume_.setType("A");
+        assertNotNull(volume_.getType());
+        assertEquals(volume_.getType(), "A");
+    }
+
+    @Test
+    public void testToString_ReturnNonEmptyString() {
+        assertNotEquals(volume_.toString(), "");
+        assertNotEquals(volume_.toString(), null);
+    }
+}
