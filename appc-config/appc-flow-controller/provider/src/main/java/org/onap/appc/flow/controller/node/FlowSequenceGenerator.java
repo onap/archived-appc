de293c77ed1dba73a956e9385875e717ccebbd2a
kurczews <krzysztof.kurczewski@nokia.com>
2018-03-05 15:39:47 +0100

    Split sequence generation to classess
    * Delegate original logic to separate clasess
    * Introduce InputParamsCollector
    * Introduce CapabilitiesDataExtractor
    * Add coverage for changes
    
    Change-Id: Ibb35dfb67306f789950c3b77362c5d79a3b6da63
    Issue-ID: APPC-440
    Signed-off-by: kurczews <krzysztof.kurczewski@nokia.com>

---
+++ appc-config/appc-flow-controller/provider/src/main/java/org/onap/appc/flow/controller/node/FlowSequenceGenerator.java
@@ -0,0 +1,126 @@
+package org.onap.appc.flow.controller.node;
+
+import static org.onap.appc.flow.controller.utils.FlowControllerConstants.DESINGTIME;
+import static org.onap.appc.flow.controller.utils.FlowControllerConstants.EXTERNAL;
+import static org.onap.appc.flow.controller.utils.FlowControllerConstants.FLOW_SEQUENCE;
+import static org.onap.appc.flow.controller.utils.FlowControllerConstants.GENERATION_NODE;
+import static org.onap.appc.flow.controller.utils.FlowControllerConstants.RUNTIME;
+import static org.onap.appc.flow.controller.utils.FlowControllerConstants.SEQUENCE_TYPE;
+import static org.onap.appc.flow.controller.utils.FlowControllerConstants.VNFC_TYPE;
+import static org.onap.appc.flow.controller.utils.FlowControllerConstants.VNF_TYPE;
+
+import com.att.eelf.configuration.EELFLogger;
+import com.att.eelf.configuration.EELFManager;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
+import org.json.JSONObject;
+import org.onap.appc.flow.controller.data.Transaction;
+import org.onap.appc.flow.controller.data.Transactions;
+import org.onap.appc.flow.controller.dbervices.FlowControlDBService;
+import org.onap.appc.flow.controller.executorImpl.GraphExecutor;
+import org.onap.appc.flow.controller.executorImpl.RestExecutor;
+import org.onap.ccsdk.sli.core.sli.SvcLogicContext;
+
+/**
+ * Helper class for FlowControlNode
+ */
+class FlowSequenceGenerator {
+
+  private static final EELFLogger log = EELFManager.getInstance().getLogger(FlowSequenceGenerator.class);
+
+  static final String MODULE = "APPC_COMMOM";
+
+  private final FlowControlDBService dbService;
+  private final FlowGenerator flowGenerator;
+  private final GraphExecutor transactionExecutor;
+  private final RestExecutor restExecutor;
+  private final InputParamsCollector inputParamsCollector;
+
+  FlowSequenceGenerator() {
+    this.dbService = FlowControlDBService.initialise();
+    this.flowGenerator = new FlowGenerator();
+    this.transactionExecutor = new GraphExecutor();
+    this.restExecutor = new RestExecutor();
+    this.inputParamsCollector = new InputParamsCollector();
+  }
+
+  /**
+   * Constructor for tests, prefer to use default one
+   */
+  FlowSequenceGenerator(FlowControlDBService dbService, FlowGenerator flowGenerator,
+      GraphExecutor graphExecutor, RestExecutor restExecutor, EnvVariables envVariables) {
+    this.dbService = dbService;
+    this.flowGenerator = flowGenerator;
+    this.transactionExecutor = graphExecutor;
+    this.restExecutor = restExecutor;
+    this.inputParamsCollector = new InputParamsCollector(envVariables, dbService);
+  }
+
+  String getFlowSequence(Map<String, String> inParams, SvcLogicContext ctx, SvcLogicContext localContext)
+      throws Exception {
+
+    String generationNode = localContext.getAttribute(GENERATION_NODE);
+    if (generationNode != null) {
+      if (!transactionExecutor.hasGraph(MODULE, generationNode, null, "sync")) {
+        throw new Exception("Can not find Custom defined Flow Generator for " + generationNode);
+      }
+      return transactionExecutor
+          .executeGraph(MODULE, generationNode, null, "sync", null)
+          .getProperty(FLOW_SEQUENCE);
+    }
+
+    String sequenceType = localContext.getAttribute(SEQUENCE_TYPE);
+    if (sequenceType == null) {
+      Transactions trans = flowGenerator.createSingleStepModel(inParams, ctx);
+      ObjectMapper mapper = new ObjectMapper();
+      String flowSequence = mapper.writeValueAsString(trans);
+      log.debug("Single step Flow Sequence : " + flowSequence);
+
+      return flowSequence;
+    }
+
+    return getFlowSequenceFromType(ctx, localContext, sequenceType);
+  }
+
+  private String getFlowSequenceFromType(SvcLogicContext ctx, SvcLogicContext localContext,
+      String sequenceType) throws Exception {
+    String flowSequence;
+    String vnfType = ctx.getAttribute(VNF_TYPE);
+    if (sequenceType.equalsIgnoreCase(DESINGTIME)) {
+
+      localContext.setAttribute(VNFC_TYPE, ctx.getAttribute(VNFC_TYPE));
+      flowSequence = dbService.getDesignTimeFlowModel(localContext);
+
+      if (flowSequence == null) {
+        throw new Exception("Flow Sequence is not found User Designed VNF " + vnfType);
+      }
+    } else if (sequenceType.equalsIgnoreCase(RUNTIME)) {
+
+      Transaction transaction = inputParamsCollector.collectInputParams(ctx);
+      log.info("CollectInputParamsData-Input: " + transaction.getPayload());
+
+      Map<String, String> flowSeq = restExecutor.execute(transaction, localContext);
+
+      JSONObject output = new JSONObject(flowSeq.get("restResponse")).optJSONObject("output");
+      if (output == null) {
+        throw new Exception("Failed to get the Flow Sequence runtime for VNF type " + vnfType);
+      }
+      flowSequence = output.toString();
+      log.info("MultistepSequenceGenerator-Output: " + flowSequence);
+
+    } else if (sequenceType.equalsIgnoreCase(EXTERNAL)) {
+      //String input = collectInputParams(localContext);
+      //    flowSequnce = ""; //get it from the External interface calling the Rest End point - TBD
+      //if(flowSequnce == null)
+
+      throw new Exception("Flow Sequence not found for " + vnfType);
+
+    } else {
+      //No other type of model supported...
+      //in Future can get flowModel from other generators which will be included here
+      throw new Exception("No information found for sequence Owner Design-Time Vs Run-Time");
+    }
+    return flowSequence;
+  }
+
+}
