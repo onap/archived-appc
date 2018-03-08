e286ae9beab6f022e006e03e67395c17904e2d40
amshegokar <AS00500801@techmahindra.com>
2018-03-08 18:28:44 +0530

    Unit test for SshConnectionDetails.java
    Unit test for SshConnectionDetails.java
    
    Sonar Link:
    https://sonar.onap.org/code?id=org.onap.appc%3Aappc&selected=org.onap.appc%3Aappc-ssh-adapter-api%3Asrc%2Fmain%2Fjava%2Forg%2Fonap%2Fappc%2Fadapter%2Fssh%2FSshConnectionDetails.java
    
    Change-Id: I91c50daae2fd1e04e74de3b6c64c0519b13c0c56
    Issue-ID: APPC-715
    Signed-off-by: amshegokar <AS00500801@techmahindra.com>

---
+++ appc-adapters/appc-ssh-adapter/appc-ssh-adapter-api/src/test/java/org/onap/appc/adapter/ssh/TestSshConnectionDetails.java
@@ -0,0 +1,63 @@
+/*
+* ============LICENSE_START=======================================================
+* ONAP : APPC
+* ================================================================================
+* Copyright 2018 TechMahindra
+*=================================================================================
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* ============LICENSE_END=========================================================
+*/
+package org.onap.appc.adapter.ssh;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestSshConnectionDetails {
+
+    private SshConnectionDetails sshConnectionDetails;
+
+    @Before
+    public void setUp() {
+        sshConnectionDetails = new SshConnectionDetails();
+    }
+
+    @Test
+    public void testGetHost() {
+        sshConnectionDetails.setHost("localhost");
+        assertNotNull(sshConnectionDetails.getHost());
+        assertEquals(sshConnectionDetails.getHost(), "localhost");
+    }
+
+    @Test
+    public void testGetPort() {
+        sshConnectionDetails.setPort(8080);
+        assertNotNull(sshConnectionDetails.getPort());
+        assertEquals(sshConnectionDetails.getPort(), 8080);
+    }
+
+    @Test
+    public void testGetUsername() {
+        sshConnectionDetails.setUsername("username");
+        assertNotNull(sshConnectionDetails.getUsername());
+        assertEquals(sshConnectionDetails.getUsername(), "username");
+    }
+
+    @Test
+    public void testGetPassword() {
+        sshConnectionDetails.setPassword("password");
+        assertNotNull(sshConnectionDetails.getPassword());
+        assertEquals(sshConnectionDetails.getPassword(), "password");
+    }
+
+}
