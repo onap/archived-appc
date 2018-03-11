bb446bb30604c107be83b5cd10bee6b0a00eb8ad
amshegokar <AS00500801@techmahindra.com>
2018-03-11 19:17:42 +0530

    Unit Test Coverage for ConnectionDetails.java
    Unit Test Coverage for ConnectionDetails.java
    Sonar-Link:
    https://sonar.onap.org/code?id=org.onap.appc%3Aappc&selected=org.onap.appc%3Aappc-dg-common%3Asrc%2Fmain%2Fjava%2Forg%2Fonap%2Fappc%2Fdg%2Fcommon%2Fobjects%2FConnectionDetails.java
    
    Change-Id: Ia253f7cea39d980f966f0afc5ab1b64d1e3f527e
    Issue-ID: APPC-718
    Signed-off-by: amshegokar <AS00500801@techmahindra.com>

---
+++ appc-dg/appc-dg-shared/appc-dg-common/src/test/java/org/onap/appc/dg/common/objects/TestConnectionDetails.java
@@ -0,0 +1,63 @@
+/*
+* ============LICENSE_START=======================================================
+* ONAP : APPC
+* ================================================================================
+* Copyright 2018 TechMahindra
+*=================================================================================
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* ============LICENSE_END=========================================================
+*/
+package org.onap.appc.dg.common.objects;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestConnectionDetails {
+
+    private ConnectionDetails connectionDetails;
+
+    @Before
+    public void setUp() {
+        connectionDetails = new ConnectionDetails();
+    }
+
+    @Test
+    public void testGetHost() {
+        connectionDetails.setHost("host");
+        assertNotNull(connectionDetails.getHost());
+        assertEquals(connectionDetails.getHost(), "host");
+    }
+
+    @Test
+    public void testGetPort() {
+        connectionDetails.setPort(8080);
+        assertNotNull(connectionDetails.getPort());
+        assertEquals(connectionDetails.getPort(), 8080);
+    }
+
+    @Test
+    public void testGetUsername() {
+        connectionDetails.setUsername("username");
+        assertNotNull(connectionDetails.getUsername());
+        assertEquals(connectionDetails.getUsername(), "username");
+    }
+
+    @Test
+    public void testGetPassword() {
+        connectionDetails.setPassword("password");
+        assertNotNull(connectionDetails.getPassword());
+        assertEquals(connectionDetails.getPassword(), "password");
+    }
+}
