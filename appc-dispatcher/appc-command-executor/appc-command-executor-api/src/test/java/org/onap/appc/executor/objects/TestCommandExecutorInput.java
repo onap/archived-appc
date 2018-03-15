f4c5c6b6080422ea836b11fd61c7d5998fc7ea6e
amshegokar <AS00500801@techmahindra.com>
2018-03-15 19:29:52 +0530

    Unit Test Coverage
    Unit test Coverage for:
    org/onap/appc/executor/objects classes
    Sonar-Link:
    https://sonar.onap.org/code?id=org.onap.appc%3Aappc&selected=org.onap.appc%3Aappc-command-executor-api%3Asrc%2Fmain%2Fjava%2Forg%2Fonap%2Fappc%2Fexecutor%2Fobjects
    
    Change-Id: I692ed8e4fc6d3789fdcff24a4fc231320f5a23e3
    Issue-ID: APPC-746
    Signed-off-by: amshegokar <AS00500801@techmahindra.com>

---
+++ appc-dispatcher/appc-command-executor/appc-command-executor-api/src/test/java/org/onap/appc/executor/objects/TestCommandExecutorInput.java
@@ -0,0 +1,52 @@
+/*
+* ============LICENSE_START=======================================================
+* ONAP : APPC
+* ================================================================================
+* Copyright 2018 TechMahindra
+*=================================================================================
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package org.onap.appc.executor.objects;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestCommandExecutorInput {
+
+    private CommandExecutorInput commandExecutorInput;
+    @Before
+    public void setUp() {
+        commandExecutorInput=new CommandExecutorInput();
+        
+    }
+    
+    @Test
+    public void testGetTtl() {
+        commandExecutorInput.setTtl(180);
+        assertNotNull(commandExecutorInput.getTtl());
+        assertEquals(commandExecutorInput.getTtl(), 180);
+    }
+    @Test
+    public void testTostring() {
+        assertTrue(commandExecutorInput.toString().contains("CommandExecutorInput"));
+    }
+    @Test
+    public void testToString_ReturnNonEmptyString() {
+        assertNotEquals(commandExecutorInput.toString(), "");
+        assertNotEquals(commandExecutorInput.toString(), null);
+
+    }
+
+}
