38d43da7d605d475901071f6256ca90cc09cd420
Lori Keighron <lk2924@att.com>
2018-03-19 12:24:47 -0400

    Added junit coverage to CoreException class
    Introduce junit-tests for CoreException class
    
    Issue-ID: APPC-752
    Change-Id: Ic5c8c2c83ce99d488e9e79a8fb72e7ad5fcf2ad3
    Signed-off-by: Lori Keighron <lk2924@att.com>

---
+++ appc-client/client-lib/src/test/java/org/onap/appc/client/impl/core/CoreExceptionTest.java
@@ -0,0 +1,55 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : APPC
+ * ================================================================================
+ * Copyright (C) 2017-2018 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Copyright (C) 2017 Amdocs
+ * =============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ECOMP is a trademark and service mark of AT&T Intellectual Property.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.appc.client.impl.core;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CoreExceptionTest {
+    @Test
+    public void testCoreException() {
+        CoreException ce = new CoreException();
+        assertNotNull(ce);
+    }
+
+    @Test
+    public void testCoreExceptionString() {
+        CoreException ce = new CoreException(new String("testCoreExceptionString"));
+        assertNotNull(ce.getMessage());
+    }
+
+    @Test
+    public void testCoreExceptionStringThrowable() {
+        Throwable tr = new Throwable();
+        CoreException ce = new CoreException("abc", tr);
+        assertNotNull(ce.getMessage());
+    }
+
+    @Test
+    public void testCoreExceptionThrowable() {
+        CoreException ce = new CoreException(new Throwable());
+        assertNotNull(ce.getMessage());
+    }
+}
