cd296af560a10b84dd943ff6b2a360f7b59f5f4d
shubhada <SV00449682@techmahindra.com>
2018-03-21 16:41:07 +0530

    Unit Test Coverage
    Unit Test Coverage for:
    1.RequestHandlerInput.java
    2.RequestHandlerOutput.java
    
    Sonar-Link:
    https://sonar.onap.org/code?id=org.onap.appc%3Aappc&selected=org.onap.appc%3Aappc-request-handler-api
    
    Change-Id: I07e6be04fbe4958cd98cb0313e672e2a0098856e
    Issue-ID: APPC-771
    Signed-off-by: shubhada <SV00449682@techmahindra.com>

---
+++ appc-dispatcher/appc-request-handler/appc-request-handler-api/src/test/java/org/onap/appc/requesthandler/objects/TestRequestHandlerInput.java
@@ -0,0 +1,51 @@
+/*
+* ============LICENSE_START=======================================================
+* ONAP : APPC
+* ================================================================================
+* Copyright 2018 TechMahindra
+*=================================================================================
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* ============LICENSE_END=========================================================
+*/
+package org.onap.appc.requesthandler.objects;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestRequestHandlerInput {
+    private RequestHandlerInput requestHandlerInput;
+
+    @Before
+    public void SetUp() {
+        requestHandlerInput= new RequestHandlerInput();
+    }
+
+    @Test
+    public void testGetRpcName() {
+        requestHandlerInput.setRpcName("rpcName");
+        Assert.assertNotNull(requestHandlerInput.getRpcName());
+        Assert.assertEquals("rpcName",requestHandlerInput.getRpcName());
+    }
+
+    @Test
+    public void testToString_ReturnNonEmptyString() {
+        Assert.assertNotEquals(requestHandlerInput.toString(), "");
+        Assert.assertNotEquals(requestHandlerInput.toString(), null);
+    }
+
+    @Test
+    public void testToString_ContainsString() {
+        Assert.assertTrue(requestHandlerInput.toString().contains("requestContext"));
+    }
+}
---
+++ appc-dispatcher/appc-request-handler/appc-request-handler-api/src/test/java/org/onap/appc/requesthandler/objects/TestRequestHandlerOutput.java
@@ -0,0 +1,44 @@
+/*
+* ============LICENSE_START=======================================================
+* ONAP : APPC
+* ================================================================================
+* Copyright 2018 TechMahindra
+*=================================================================================
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* ============LICENSE_END=========================================================
+*/
+package org.onap.appc.requesthandler.objects;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestRequestHandlerOutput {
+    private RequestHandlerOutput requestHandlerOutput;
+
+    @Before
+    public void SetUp() {
+        requestHandlerOutput= new RequestHandlerOutput();
+    }
+
+    @Test
+    public void testToString_ReturnNonEmptyString() {
+        Assert.assertNotEquals(requestHandlerOutput.toString(), "");
+        Assert.assertNotEquals(requestHandlerOutput.toString(), null);
+    }
+
+    @Test
+    public void testToString_ContainsString() {
+        Assert.assertTrue(requestHandlerOutput.toString().contains("responseContext"));
+    }
+}
