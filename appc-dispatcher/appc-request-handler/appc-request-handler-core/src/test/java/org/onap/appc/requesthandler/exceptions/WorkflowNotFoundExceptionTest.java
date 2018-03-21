9b9d9cd2fc1bca099680dbccc64479669c0c14cb
Lori Keighron <lk2924@att.com>
2018-03-21 14:37:21 -0400

    Add junit coverage to request handler exceptions
    Introduce 7 more junit-test files:
    DGWorkflowNotFoundExceptionTest.java
    LCMOperationsDisabledExceptionTest.java
    MultipleRecordsRetrievedExceptionTest.java
    NoRecordsRetrievedExceptionTest.java
    RequestValidationExceptionTest.java
    VNFNotFoundExceptionTest.java
    WorkflowNotFoundExceptionTest.java
    
    Issue-ID: APPC-773
    Change-Id: I2e962a9614e3b17d77d2685e1055322531d832a1
    Signed-off-by: Lori Keighron <lk2924@att.com>

---
+++ appc-dispatcher/appc-request-handler/appc-request-handler-core/src/test/java/org/onap/appc/requesthandler/exceptions/WorkflowNotFoundExceptionTest.java
@@ -0,0 +1,40 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : APPC
+ * ================================================================================
+ * Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ECOMP is a trademark and service mark of AT&T Intellectual Property.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.appc.requesthandler.exceptions;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.onap.appc.executor.objects.LCMCommandStatus;
+
+public class WorkflowNotFoundExceptionTest {
+
+   @Test
+    public final void test() {
+       String sampleMsg = "sample testing message";
+       String vnfId = "sample vnfId";
+       String cmd = "sample command";
+       WorkflowNotFoundException wfnfe = new WorkflowNotFoundException(sampleMsg, vnfId, cmd);
+       assertNotNull(wfnfe.getMessage());
+       assertEquals(sampleMsg, wfnfe.getMessage());
+       assertEquals(wfnfe.getLcmCommandStatus(), LCMCommandStatus.WORKFLOW_NOT_FOUND);   }
+}
