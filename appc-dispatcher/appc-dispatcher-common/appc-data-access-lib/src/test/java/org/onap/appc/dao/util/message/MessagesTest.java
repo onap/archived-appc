1f540701dd0f8e34f38bed3168192039af32fcc8
amshegokar <AS00500801@techmahindra.com>
2018-03-23 19:07:03 +0530

    Unit test for Messages.java
    Unit test for Messages.java
    Sonar-Link:
    https://sonar.onap.org/code?id=org.onap.appc%3Aappc-netconf-adapter-bundle&selected=org.onap.appc%3Aappc-data-access-lib%3Asrc%2Fmain%2Fjava%2Forg%2Fonap%2Fappc%2Fdao%2Futil%2Fmessage
    
    Change-Id: I2204f296d3d2200eb4aa4f809e0c0dd841b0b93e
    Issue-ID: APPC-782
    Signed-off-by: amshegokar <AS00500801@techmahindra.com>

---
+++ appc-dispatcher/appc-dispatcher-common/appc-data-access-lib/src/test/java/org/onap/appc/dao/util/message/MessagesTest.java
@@ -0,0 +1,48 @@
+/*
+* ============LICENSE_START=======================================================
+* ONAP : APPC
+* ================================================================================
+* Copyright 2018 TechMahindra
+*=================================================================================
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* ============LICENSE_END=========================================================
+*/
+package org.onap.appc.dao.util.message;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestMessages {
+    private Messages messages=Messages.EXP_APPC_JDBC_CONNECT;
+
+    @Test
+    public void testName() {
+        Assert.assertEquals("EXP_APPC_JDBC_CONNECT", messages.name());
+    }
+
+    @Test
+    public void testEqual() {
+        Assert.assertTrue(messages.equals(Messages.EXP_APPC_JDBC_CONNECT));
+        Assert.assertFalse(messages.equals(null));
+    }
+
+    @Test
+    public void testgetMessage() {
+        Assert.assertEquals("Error connecting to JDBC using properties for schema [%s]", Messages.EXP_APPC_JDBC_CONNECT.getMessage());
+    }
+
+    @Test
+    public void testformat() {
+        Assert.assertEquals(("Error connecting to JDBC using properties for schema [%s]"), Messages.EXP_APPC_JDBC_CONNECT.format("%s"));
+    }
+}
