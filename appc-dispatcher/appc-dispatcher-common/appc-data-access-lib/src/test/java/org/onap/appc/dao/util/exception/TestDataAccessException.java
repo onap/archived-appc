d1aa80f74a6b2897c6ad6689b81feeb88b259d5b
amshegokar <AS00500801@techmahindra.com>
2018-03-23 19:16:27 +0530

    Coverage for:org/onap/appc/dao/util/exception
    Unit Tests for:
    1) DataAccessException.java
    2)DBConnectionPoolException.java
    3) JdbcRuntimeException.java
    
    Sonar-Link:
    https://sonar.onap.org/code?id=org.onap.appc%3Aappc-netconf-adapter-bundle&selected=org.onap.appc%3Aappc-data-access-lib%3Asrc%2Fmain%2Fjava%2Forg%2Fonap%2Fappc%2Fdao%2Futil%2Fexception
    
    Change-Id: Ie5c44417ca33c73de5577052a01ca5f019adeb1b
    Issue-ID: APPC-781
    Signed-off-by: amshegokar <AS00500801@techmahindra.com>

---
+++ appc-dispatcher/appc-dispatcher-common/appc-data-access-lib/src/test/java/org/onap/appc/dao/util/exception/TestDataAccessException.java
@@ -0,0 +1,64 @@
+/*
+* ============LICENSE_START=======================================================
+* ONAP : APPC
+* ================================================================================
+* Copyright 2018 TechMahindra
+*=================================================================================
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* ============LICENSE_END=========================================================
+*/
+package org.onap.appc.dao.util.exception;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestDataAccessException {
+
+    @Test
+    public void testConstructorNoArgument() throws Exception {
+        DataAccessException dataAccessException = new DataAccessException();
+        Assert.assertTrue(dataAccessException.getCause() == null);
+        Assert.assertTrue(dataAccessException.getLocalizedMessage() == null);
+        Assert.assertTrue(dataAccessException.getMessage() == null);
+    }
+
+    @Test
+    public void testConstructorWithMessage() throws Exception {
+        String message = "testing message";
+        DataAccessException dataAccessException = new DataAccessException(message);
+        Assert.assertTrue(dataAccessException.getCause() == null);
+        Assert.assertEquals(message, dataAccessException.getLocalizedMessage());
+        Assert.assertEquals(message, dataAccessException.getMessage());
+    }
+
+    @Test
+    public void testConstructorWithMessageAndThrowable() throws Exception {
+        String message = "testing message";
+        String tMessage = "throwable message";
+        Throwable throwable = new Throwable(tMessage);
+        DataAccessException dataAccessException = new DataAccessException(message, throwable);
+        Assert.assertEquals(throwable, dataAccessException.getCause());
+        Assert.assertTrue(dataAccessException.getLocalizedMessage().contains(message));
+        Assert.assertTrue(dataAccessException.getMessage().contains(message));
+    }
+
+    @Test
+    public void testConstructorWithThrowable() throws Exception {
+        String message = "testing message";
+        Throwable throwable = new Throwable(message);
+        DataAccessException dataAccessException = new DataAccessException(throwable);
+        Assert.assertEquals(throwable, dataAccessException.getCause());
+        Assert.assertTrue(dataAccessException.getLocalizedMessage().contains(message));
+        Assert.assertTrue(dataAccessException.getMessage().contains(message));
+    }
+}
