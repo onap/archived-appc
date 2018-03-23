10c723a51705719eb072d18c8a44a4f488e39d2b
amshegokar <AS00500801@techmahindra.com>
2018-03-23 18:37:01 +0530

    Unit test coverage
    Unit test coverage for Constants.java
    Sonar-Link:
    https://sonar.onap.org/code?id=org.onap.appc%3Aappc&selected=org.onap.appc%3Aappc-iaas-adapter-bundle%3Asrc%2Fmain%2Fjava%2Forg%2Fonap%2Fappc%2Fadapter%2Futils%2FConstants.java
    
    Change-Id: Ied122a1838111897079243f0e444a9eef1b2f162
    Issue-ID: APPC-778
    Signed-off-by: amshegokar <AS00500801@techmahindra.com>

---
+++ appc-adapters/appc-iaas-adapter/appc-iaas-adapter-bundle/src/test/java/org/onap/appc/adapter/utils/TestConstants.java
@@ -0,0 +1,34 @@
+/*
+* ============LICENSE_START=======================================================
+* ONAP : APPC
+* ================================================================================
+* Copyright 2018 TechMahindra
+*=================================================================================
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* ============LICENSE_END=========================================================
+*/
+package org.onap.appc.adapter.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestConstants {
+    private Constants constants=new Constants();
+
+    @Test
+    public void testConstants() {
+        Assert.assertNotNull(constants.ADAPTER_NAME);
+        Assert.assertEquals("Appc IaaS Adapter",constants.ADAPTER_NAME);
+    }
+
+}
