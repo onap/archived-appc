d9d58734c13eed68411ac4651460228ffef24650
amshegokar <AS00500801@techmahindra.com>
2018-03-26 17:04:40 +0530

    Unit test coverage for DBException.java
    Unit test coverage for DBException.java
    Sonar-Link:
    https://sonar.onap.org/code?id=org.onap.appc%3Aappc&selected=org.onap.appc%3Aappc-artifact-handler-provider%3Asrc%2Fmain%2Fjava%2Forg%2Fonap%2Fappc%2Fartifact%2Fhandler%2Fdbservices%2FDBException.java
    
    Change-Id: Iae0c5a90358f61560c8e164fa506a0e3e75857f5
    Issue-ID: APPC-791
    Signed-off-by: amshegokar <AS00500801@techmahindra.com>

---
+++ appc-inbound/appc-artifact-handler/provider/src/test/java/org/onap/appc/artifact/handler/dbservices/DBExceptionTest.java
@@ -0,0 +1,37 @@
+/*
+* ============LICENSE_START=======================================================
+* ONAP : APPC
+* ================================================================================
+* Copyright 2018 TechMahindra
+*=================================================================================
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* ============LICENSE_END=========================================================
+*/
+package org.onap.appc.artifact.handler.dbservices;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DBExceptionTest {
+
+    @Test
+    public void testConstructorWithMessageAndThrowable() throws Exception {
+        String message = "testing message";
+        String tMessage = "throwable message";
+        Throwable throwable = new Throwable(tMessage);
+        DBException dBException =new DBException(message, throwable);
+        Assert.assertEquals(throwable, dBException.getCause());
+        Assert.assertTrue(dBException.getLocalizedMessage().contains(message));
+        Assert.assertTrue(dBException.getMessage().contains(message));
+    }
+}
