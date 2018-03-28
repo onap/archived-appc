058bdd998f87f072d4a6d2272e92c20d3312b138
Neha Sood <ns189k@att.com>
2018-03-28 15:36:02 -0400

    Add junit coverage for three classes
    Issue-ID: APPC-806
    
    Change-Id: I5dc4da5fcd2325c6915b86a7f1c26d3a0f69cf9b
    Signed-off-by: Neha Sood <ns189k@att.com>

---
+++ appc-config/appc-flow-controller/provider/src/test/java/org/onap/appc/flow/controller/data/ParametersTest.java
@@ -0,0 +1,79 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : APPC
+ * ================================================================================
+ * Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+ * =============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.appc.flow.controller.data;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ParametersTest {
+    private Parameters param;
+    private Parameters param1;
+    private Parameters param2;
+
+    @Before
+    public void SetUp() {
+        param = new Parameters();
+        param1 = new Parameters();
+        param2 = new Parameters();
+    }
+
+    @Test
+    public void testSetParamValue() {
+        param.setParamValue("1");
+        assertNotNull(param.getParamValue());
+        assertEquals(param.getParamValue(),"1");
+    }
+
+    @Test
+    public void testSetParamName() {
+        param.setParamName("abc");
+        assertNotNull(param.getParamName());
+        assertEquals(param.getParamName(),"abc");
+    }
+
+    @Test
+    public void testHashCode_Print() {
+        param.setParamName("2");
+        param.setParamValue("def");
+        System.out.println("param hashcode is " + param.hashCode());
+    }
+
+    @Test
+    public void testToString() {
+        param.setParamName("3");
+        param.setParamValue("ghi");
+        String ret = param.toString();
+        assertFalse("toString is not empty", ret.isEmpty());
+    }
+
+    @Test
+    public void testEqualsObject() {
+        assertTrue(param1.equals(param2) && param2.equals(param1));
+        assertTrue(param1.equals(param1));
+        assertFalse(param1.equals(null));
+    }
+}
---
+++ appc-config/appc-flow-controller/provider/src/test/java/org/onap/appc/flow/controller/data/PreCheckTest.java
@@ -0,0 +1,72 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : APPC
+ * ================================================================================
+ * Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+ * =============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.appc.flow.controller.data;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PreCheckTest {
+    
+	private PreCheck preCheck;
+
+    @Before
+    public void SetUp() {
+        preCheck = new PreCheck();
+    }
+
+    @Test
+    public void testSetPrecheckOperator() {
+        preCheck.setPrecheckOperator("op");
+        assertNotNull(preCheck.getPrecheckOperator());
+        assertEquals(preCheck.getPrecheckOperator(),"op");
+    }
+
+    @Test
+    public void testSetPrecheckOptions() {
+        List<PrecheckOption> precheckOptionList = new LinkedList<>();
+        preCheck.setPrecheckOptions(precheckOptionList);
+        assertNotNull(preCheck.getPrecheckOptions());
+        assertEquals(preCheck.getPrecheckOptions(), precheckOptionList);
+    }
+
+    @Test
+    public void testHashCode() {
+        preCheck.setPrecheckOperator("1");
+        System.out.println(" precheck hashcode is "  + preCheck.hashCode());
+    }
+
+    @Test
+    public void testToString() {
+        preCheck.setPrecheckOperator("A");
+        List<PrecheckOption> precheckOptionList = new LinkedList<>();
+        preCheck.setPrecheckOptions(precheckOptionList);
+        String ret = preCheck.toString();
+        System.out.println("ret is " + ret);
+
+    }
+
+}
