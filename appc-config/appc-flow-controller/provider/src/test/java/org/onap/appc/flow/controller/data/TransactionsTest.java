64a38e6ec2c0d9796b621105d1c7e118e8a3fd20
kurczews <krzysztof.kurczewski@nokia.com>
2018-03-30 11:31:03 +0200

    Added tests for appc.flow.Transactions
    Change-Id: I00a683484f8a6e8a97e661a421382dafeb0e8237
    Issue-ID: APPC-442
    Signed-off-by: kurczews <krzysztof.kurczewski@nokia.com>

---
+++ appc-config/appc-flow-controller/provider/src/test/java/org/onap/appc/flow/controller/data/TransactionsTest.java
@@ -0,0 +1,66 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : APPC
+ * ================================================================================
+ * Copyright (C) 2017-18 Nokia Intellectual Property. All rights reserved.
+ * =============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.appc.flow.controller.data;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TransactionsTest {
+
+    private Transactions transactions;
+
+    @Before
+    public void setUp() {
+        transactions = new Transactions();
+    }
+
+    @Test
+    public void get_set_transactions() {
+        List<Transaction> transactionsList = new ArrayList<>();
+        Transaction transaction = mock(Transaction.class);
+        transactionsList.add(transaction);
+
+        transactions.setTransactions(transactionsList);
+
+        Assert.assertEquals(transactionsList, this.transactions.getTransactions());
+    }
+
+    @Test
+    public void to_string() {
+        Transaction mock = mock(Transaction.class);
+        when(mock.toString()).thenReturn("some_transactions");
+
+        List<Transaction> transactionsList = new ArrayList<>();
+        transactionsList.add(mock);
+
+        transactions.setTransactions(transactionsList);
+
+        Assert.assertEquals("Transactions [transactions=[some_transactions]]",
+            transactions.toString());
+    }
+
+}
