826a1e4b16d7b16491650e8718ec48e7a0cc4f5a
Arnel Pajarillo <ap1541@att.com>
2018-04-02 08:59:46 -0400

    Add junit for more APPC client-lib classes
    Test coverage for CoreRegisty, TaskQueue, TaskQueueManager classes in client-lib.
    
    Change-Id: Id601c4736b51ea256de392fb8fcd91c3a49a55cc
    Issue-ID: APPC-819
    Signed-off-by: Arnel Pajarillo <ap1541@att.com>

---
+++ appc-client/client-lib/src/test/java/org/onap/appc/client/impl/core/CoreRegistryTest.java
@@ -0,0 +1,77 @@
+/*
+ * ============LICENSE_START=======================================================
+ * ONAP : APPC
+ * ================================================================================
+ * Copyright 2018 AT&T
+ * =================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.appc.client.impl.core;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.onap.appc.client.impl.core.CoreRegistry.EmptyRegistryCallback;
+
+public class CoreRegistryTest {
+    
+    private boolean erCalledBack;
+    private CoreRegistry<String> registry;
+    
+    @Before
+    public void beforeTest() throws Exception {
+        erCalledBack = false;
+        registry = new CoreRegistry<>(
+                new EmptyRegistryCallback() {
+
+                    @Override
+                    public void emptyCallback() {
+                        erCalledBack = true;
+                    }
+                });
+    }
+
+    @Test
+    public void testRegister() {
+        registry.register("1", "a");
+        assertEquals("a", registry.get("1"));
+        registry.register("1", "b");
+        assertEquals("b", registry.get("1"));
+    }
+
+    @Test
+    public void testUnregister() {
+        registry.register("3", "c");
+        registry.unregister("3");
+        assertFalse(registry.isExist("3"));
+    }
+
+    @Test
+    public void testEmptyCallbackNotCalledOnNewRegistry() {
+        assertFalse(erCalledBack);
+    }
+
+    @Test
+    public void testEmptyCallbackAndIsEmpty() {
+        registry.register("3", "c");
+        registry.unregister("3");
+        assertTrue(registry.isEmpty());
+        assertTrue(erCalledBack);
+    }
+}
---
+++ appc-client/client-lib/src/test/java/org/onap/appc/client/impl/core/TaskQueueManagerTest.java
@@ -0,0 +1,75 @@
+/*
+ * ============LICENSE_START=======================================================
+ * ONAP : APPC
+ * ================================================================================
+ * Copyright 2018 AT&T
+ * =================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.appc.client.impl.core;
+
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TaskQueueManagerTest {
+
+    private TaskQueueManager qm;
+    private CountDownLatch latch;
+    private static Properties props = new Properties();
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        props.setProperty("client.pool.size", "3");
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        qm = new TaskQueueManager(props);
+    }
+
+    @Test
+    public void testSubmit() throws InterruptedException {
+        int count = 10;
+        latch = new CountDownLatch(count);
+        for (int i = 0; i < count; i++) {
+            qm.submit(i + "", new RunTask());
+        }
+        assertTrue(latch.await(3, TimeUnit.SECONDS));
+    }
+
+    @Test
+    public void testStopQueueManager() throws InterruptedException {
+        latch = new CountDownLatch(1);
+        qm.stopQueueManager();
+        qm.submit("1", new RunTask());
+        assertFalse(latch.await(1, TimeUnit.SECONDS));
+    }
+
+    private class RunTask implements Runnable {
+        @Override
+        public void run() {
+            latch.countDown();
+        }
+    }
+}
