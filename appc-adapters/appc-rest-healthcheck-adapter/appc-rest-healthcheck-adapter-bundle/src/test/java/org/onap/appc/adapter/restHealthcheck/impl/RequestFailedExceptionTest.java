02fe1492b25618890cc898b1e84457d4b7c62399
amshegokar <AS00500801@techmahindra.com>
2018-04-02 17:22:37 +0530

    Unit test for RequestFailedException.java
    Unit test for RequestFailedException.java
    Sonar-Link:
    https://sonar.onap.org/code?id=org.onap.appc%3Aappc&selected=org.onap.appc%3Aappc-rest-healthcheck-adapter-bundle%3Asrc%2Fmain%2Fjava%2Forg%2Fonap%2Fappc%2Fadapter%2FrestHealthcheck%2Fimpl%2FRequestFailedException.java
    
    Change-Id: I7626b968a757d2dd6c99500fbae7a6150a7a613e
    Issue-ID: APPC-818
    Signed-off-by: amshegokar <AS00500801@techmahindra.com>

---
+++ appc-adapters/appc-rest-healthcheck-adapter/appc-rest-healthcheck-adapter-bundle/src/test/java/org/onap/appc/adapter/restHealthcheck/impl/RequestFailedExceptionTest.java
@@ -0,0 +1,133 @@
+/*
+* ============LICENSE_START=======================================================
+* ONAP : APPC
+* ================================================================================
+* Copyright 2018 TechMahindra
+*=================================================================================
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* ============LICENSE_END=========================================================
+*/
+package org.onap.appc.adapter.restHealthcheck.impl;
+
+import org.glassfish.grizzly.http.util.HttpStatus;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.att.cdp.zones.model.Server;
+
+public class RequestFailedExceptionTest {
+    @Test
+    public void testConstructorNoArgument() throws Exception {
+        RequestFailedException requestFailedException = new RequestFailedException();
+        Assert.assertTrue(requestFailedException.getCause() == null);
+        Assert.assertTrue(requestFailedException.getLocalizedMessage() == null);
+        Assert.assertTrue(requestFailedException.getMessage() == null);
+    }
+
+    @Test
+    public void testConstructorWithMessage() throws Exception {
+        String message = "testing message";
+        RequestFailedException requestFailedException = new RequestFailedException(message);
+        Assert.assertTrue(requestFailedException.getCause() == null);
+        Assert.assertEquals(message, requestFailedException.getLocalizedMessage());
+        Assert.assertEquals(message, requestFailedException.getMessage());
+    }
+
+    @Test
+    public void testConstructor_And_GetterSetters() throws Exception {
+        Server server = new Server();
+        HttpStatus status = HttpStatus.ACCEPTED_202;
+        String reason = "Success";
+        String operation = "POST";
+        RequestFailedException requestFailedException = new RequestFailedException(operation, reason, status, server);
+        requestFailedException.setOperation(operation);
+        requestFailedException.setReason(reason);
+        requestFailedException.setServerId("A");
+        requestFailedException.setStatus(status);
+        Assert.assertEquals("POST", requestFailedException.getOperation());
+        Assert.assertEquals("Success", requestFailedException.getReason());
+        Assert.assertEquals("A", requestFailedException.getServerId());
+        Assert.assertEquals(HttpStatus.ACCEPTED_202, requestFailedException.getStatus());
+        Assert.assertEquals("A", requestFailedException.getServerId());
+    }
+
+    @Test
+    public void testConstructorWithFiveArguements() throws Exception {
+        String tMessage = "throwable message";
+        Server server = new Server();
+        HttpStatus status = HttpStatus.ACCEPTED_202;
+        String reason = "Success";
+        String operation = "POST";
+        Throwable throwable = new Throwable(tMessage);
+        RequestFailedException requestFailedException = new RequestFailedException(throwable, operation, reason, status,
+                server);
+        Assert.assertEquals(throwable, requestFailedException.getCause());
+
+    }
+
+    @Test
+    public void testConstructorWithFiveArguements_server_Null() throws Exception {
+        String tMessage = "throwable message";
+        Server server = null;
+        HttpStatus status = HttpStatus.ACCEPTED_202;
+        String reason = "Success";
+        String operation = "POST";
+        Throwable throwable = new Throwable(tMessage);
+        RequestFailedException requestFailedException = new RequestFailedException(throwable, operation, reason, status,
+                server);
+        Assert.assertEquals(throwable, requestFailedException.getCause());
+    }
+
+    @Test
+    public void testConstructorWith_Server_Null() throws Exception {
+        Server server = null;
+        HttpStatus status = HttpStatus.ACCEPTED_202;
+        String reason = "Success";
+        String operation = "POST";
+        RequestFailedException requestFailedException = new RequestFailedException(operation, reason, status, server);
+        requestFailedException.setServer(server);
+        Assert.assertEquals(null, requestFailedException.getServer());
+    }
+
+    @Test
+    public void testConstructorWithMessageAndThrowable() throws Exception {
+        String message = "testing message";
+        String tMessage = "throwable message";
+        Throwable throwable = new Throwable(tMessage);
+        RequestFailedException requestFailedException = new RequestFailedException(message, throwable);
+        Assert.assertEquals(throwable, requestFailedException.getCause());
+        Assert.assertTrue(requestFailedException.getLocalizedMessage().contains(message));
+        Assert.assertTrue(requestFailedException.getMessage().contains(message));
+    }
+
+    @Test
+    public void testConstructorWithFourArguements() throws Exception {
+        String message = "testing message";
+        String tMessage = "throwable message";
+        Throwable throwable = new Throwable(tMessage);
+        RequestFailedException requestFailedException = new RequestFailedException(message, throwable, true, true);
+        Assert.assertEquals(throwable, requestFailedException.getCause());
+        Assert.assertTrue(requestFailedException.getLocalizedMessage().contains(message));
+        Assert.assertTrue(requestFailedException.getMessage().contains(message));
+    }
+
+    @Test
+    public void testConstructorWithThrowable() throws Exception {
+        String message = "testing message";
+        Throwable throwable = new Throwable(message);
+        RequestFailedException requestFailedException = new RequestFailedException(throwable);
+        Assert.assertEquals(throwable, requestFailedException.getCause());
+        Assert.assertTrue(requestFailedException.getLocalizedMessage().contains(message));
+        Assert.assertTrue(requestFailedException.getMessage().contains(message));
+    }
+}
