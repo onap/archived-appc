52003e742c268416ab75830b67a22420f7d77c0b
Mei Su <ms6523@att.com>
2018-04-13 15:23:31 -0400

    Add junit coverage to Request class
    Introduce junit-tests for Request class
    
    Issue-ID: APPC-852
    Change-Id: I6ee0919565e0de9fba79604dac653e7634277f39
    Signed-off-by: Mei Su <ms6523@att.com>

---
+++ appc-dispatcher/appc-request-handler/appc-request-handler-core/src/test/java/org/onap/appc/requesthandler/model/RequestTest.java
@@ -0,0 +1,80 @@
+/*-
+* ============LICENSE_START=======================================================
+* ONAP : APPC
+* ================================================================================
+* Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+* =============================================================================
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* ============LICENSE_END=========================================================
+*/
+package org.onap.appc.requesthandler.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RequestTest {
+    private Request request;
+
+    @Before
+    public void setUp() throws Exception {
+        request = new Request();
+    }
+
+    @Test
+    public void testGetRequestData() {
+        assertNull(request.getRequestData());
+        request.setRequestData("myRequestData");
+        assertEquals(request.getRequestData(), "myRequestData");
+    }
+
+    @Test
+    public void testSetRequestData() {
+        assertNull(request.getRequestData());
+        request.setRequestData("testRequestData");
+        assertNotNull(request.getRequestData());
+        assertEquals(request.getRequestData(), "testRequestData");
+    }
+
+    @Test
+    public void testGetRequestID() {
+        assertNull(request.getRequestID());
+        request.setRequestID("myRequestID");
+        assertEquals(request.getRequestID(), "myRequestID");
+    }
+
+    @Test
+    public void testSetRequestID() {
+        assertNull(request.getRequestID());
+        request.setRequestID("testRequestID");
+        assertEquals(request.getRequestID(), "testRequestID");
+    }
+
+    @Test
+    public void testGetAction() {
+        assertNull(request.getAction());
+        request.setAction("myAction");
+        assertEquals(request.getAction(), "myAction");
+    }
+
+    @Test
+    public void testSetAction() {
+        assertNull(request.getAction());
+        request.setAction("testAction");
+        assertEquals(request.getAction(), "testAction");
+    }
+
+}
