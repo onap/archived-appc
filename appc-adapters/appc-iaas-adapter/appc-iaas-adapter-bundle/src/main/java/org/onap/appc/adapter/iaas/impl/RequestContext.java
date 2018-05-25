8b1b9328ad3609dc93c3a18be1dec4b45051fdb6
John McClung <jm1364@att.com>
2018-05-25 10:02:29 -0400

    Continue removing ECOMP trademark from lic header.
    Remove ECOMP trademark license header in miscellaneous files.
    
    Change-Id: I76e386462402cb959de377b56997ecf7986ffd6b
    Issue-ID: APPC-931
    Signed-off-by: John McClung <jm1364@att.com>

--- appc-adapters/appc-iaas-adapter/appc-iaas-adapter-bundle/src/main/java/org/onap/appc/adapter/iaas/impl/RequestContext.java
+++ appc-adapters/appc-iaas-adapter/appc-iaas-adapter-bundle/src/main/java/org/onap/appc/adapter/iaas/impl/RequestContext.java
@@ -1,8 +1,8 @@
-/*-
+/*
  * ============LICENSE_START=======================================================
  * ONAP : APPC
  * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2017-2018 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Copyright (C) 2017 Amdocs
  * =============================================================================
@@ -18,10 +18,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  * 
- * ECOMP is a trademark and service mark of AT&T Intellectual Property.
  * ============LICENSE_END=========================================================
  */
-
 
 package org.onap.appc.adapter.iaas.impl;
 
