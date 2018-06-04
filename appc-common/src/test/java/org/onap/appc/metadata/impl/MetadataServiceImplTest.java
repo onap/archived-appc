6d2802525290eaa7f5ed53d11933b0c17db28e7e
John McClung <jm1364@att.com>
2018-06-04 14:26:21 -0400

    More license header updates to appc-common files
    Modify license header in appc-common files to reflect 2018 changes.
    
    Change-Id: Id4183e9f4f8a95263a01f04d41505ce25d81ce07
    Issue-ID: APPC-966
    Signed-off-by: John McClung <jm1364@att.com>

--- appc-common/src/test/java/org/onap/appc/metadata/impl/MetadataServiceImplTest.java
+++ appc-common/src/test/java/org/onap/appc/metadata/impl/MetadataServiceImplTest.java
@@ -2,7 +2,7 @@
  * ============LICENSE_START=======================================================
  * ONAP : APPC
  * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2017-2018 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Copyright (C) 2018 Nokia Solutions and Networks
  * =============================================================================
@@ -18,7 +18,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  * 
- * ECOMP is a trademark and service mark of AT&T Intellectual Property.
  * ============LICENSE_END=========================================================
  */
 package org.onap.appc.metadata.impl;
