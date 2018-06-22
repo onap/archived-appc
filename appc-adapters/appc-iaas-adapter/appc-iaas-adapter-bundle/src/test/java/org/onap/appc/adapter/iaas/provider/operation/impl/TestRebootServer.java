ef6d9c762700f44f0daf5d1e1fdc4a98dff4181d
Modaboina, Kusumakumari (km583p) <km583p@us.att.com>
2018-06-22 13:20:12 -0400

    add support for new openstack rebootaction VM levl
    Issue-ID: APPC-922
    Change-Id: I84941d8a5882b422d4e8365c82b8aaaf4618fa57
    Signed-off-by: Modaboina, Kusumakumari (km583p) <km583p@us.att.com>

---
+++ appc-adapters/appc-iaas-adapter/appc-iaas-adapter-bundle/src/test/java/org/onap/appc/adapter/iaas/provider/operation/impl/TestRebootServer.java
@@ -0,0 +1,51 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : APPC
+ * ================================================================================
+ * Copyright (C) 2017-2018 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Copyright (C) 2017 Amdocs
+ * =============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.appc.adapter.iaas.provider.operation.impl;
+
+import static org.mockito.Mockito.inOrder;
+import org.onap.appc.adapter.iaas.ProviderAdapter;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.InOrder;
+import org.onap.appc.exceptions.APPCException;
+import com.att.cdp.exceptions.ZoneException;
+import com.att.cdp.zones.model.Server;
+import com.att.cdp.zones.model.Server.Status;
+import static org.junit.Assert.assertEquals;
+
+public class TestRebootServer {
+
+    @Test
+        public void should_returnNullAsServer() throws ZoneException,APPCException {
+        MockGenerator mg = new MockGenerator(Status.SUSPENDED);
+        Server server = mg.getServer();
+        RebootServer rbs = new RebootServer();
+        mg.getParams().put(ProviderAdapter.PROPERTY_INSTANCE_URL, "url1");
+        mg.getParams().put(ProviderAdapter.REBOOT_TYPE, "");
+        mg.getParams().put("REBOOT_STATUS", "SUCCESS");
+        rbs.setProviderCache(mg.getProviderCacheMap());
+        rbs.executeProviderOperation(mg.getParams(), mg.getSvcLogicContext());
+        assertEquals("SUCCESS", mg.getParams().get("REBOOT_STATUS"));
+    }
+
+}
