9e0d62532b7326bda465407e072f60525f2ec471
Sandeep J <sandeejh@in.ibm.com>
2018-07-13 14:45:24 +0530

    added file to test constants in sdnc config pkg
    added TestConstants.java in sdnc config generator tool package to test
    the constants
    
    Issue-ID: APPC-1062
    Change-Id: I57cabbfce00e4fc90a3e992795cc18042344a9fa
    Signed-off-by: Sandeep J <sandeejh@in.ibm.com>

---
+++ appc-config/appc-config-generator/provider/src/test/java/org/onap/sdnc/config/generator/tool/TestConstants.java
@@ -0,0 +1,37 @@
+package org.onap.sdnc.config.generator.tool;
+
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : APPC
+ * ================================================================================
+ * Copyright (C) 2018 IBM Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR ConstantsDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ============LICENSE_END=========================================================
+ */
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestConstants {
+
+    @Test
+    public void TestConstantsstants() {
+
+        Assert.assertEquals("org.onap.ccsdk.sli.core.dblib.DBResourceManager", Constants.DBLIB_SERVICE);
+        Assert.assertEquals("DEVICE_AUTHENTICATION", Constants.DEVICE_AUTHENTICATION);
+        Assert.assertEquals("SDNCTL", Constants.SCHEMA_SDNCTL);
+        Assert.assertEquals("/opt/onap/appc/data/properties", Constants.APPC_CONFIG_DIR);
+    }
+}
