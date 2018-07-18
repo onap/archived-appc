0fd375f2b5bc446beb78d8b842e7be9e610bfe88
Dilip kumar Pampana <dilip.kumar.pampana@ibm.com>
2018-07-18 11:23:24 -0400

    Junit tests for config/flowController
    Junit Tests for code coverage in config/flow-controller, added a
    constructor in FlowControlDBService.java file to get the code coverage.
    
    Issue-ID: APPC-1090
    Change-Id: If1c33d1bfde58fc5f22ef2faa50380e71f2fdac4
    Signed-off-by: Dilip kumar Pampana <dilip.kumar.pampana@ibm.com>

--- appc-config/appc-flow-controller/provider/src/main/java/org/onap/appc/flow/controller/dbervices/FlowControlDBService.java
+++ appc-config/appc-flow-controller/provider/src/main/java/org/onap/appc/flow/controller/dbervices/FlowControlDBService.java
@@ -3,7 +3,9 @@
  * ONAP : APPC
  * ================================================================================
  * Copyright (C) 2017-2018 AT&T Intellectual Property. All rights reserved.
- * =============================================================================
+ * ================================================================================
+ * Copyright (C) 2018 IBM
+ * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -57,7 +59,13 @@ public class FlowControlDBService {
             serviceLogic = new SqlResource();
         }
     }
-    
+
+    protected FlowControlDBService(SqlResource svcLogic) {
+        if (serviceLogic == null) {
+            serviceLogic = svcLogic;
+        }
+    }
+
     public static FlowControlDBService initialise() {
         if (dgGeneralDBService == null) {
             dgGeneralDBService = new FlowControlDBService();
