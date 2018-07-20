c66a616860708058ce8dc532c66410336a57a4c4
Sandeep J <sandeejh@in.ibm.com>
2018-07-21 00:41:45 +0530

    replaced @ignore with @test to include the test
    replaced the @ignore with @test to include the test as it is a
    legitimate test and has wrongly been ignored. After removing ignore, the
    test runs successfully and also increases the code-coverage.
    
    Issue-ID: APPC-1086
    Change-Id: I60066a1f225f9a0afe5d4150f9f9e8f053258deb
    Signed-off-by: Sandeep J <sandeejh@in.ibm.com>

--- appc-config/appc-config-params/provider/src/test/java/org/onap/sdnc/config/params/parser/TestPropertyDefinitionNode.java
+++ appc-config/appc-config-params/provider/src/test/java/org/onap/sdnc/config/params/parser/TestPropertyDefinitionNode.java
@@ -6,6 +6,8 @@
  * ================================================================================
  * Copyright (C) 2017 Amdocs
  * =============================================================================
+ * Modifications Copyright (C) 2018 IBM
+ * =============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -41,7 +43,7 @@ import org.onap.sdnc.config.params.transformer.ArtificatTransformer;
 
 public class TestPropertyDefinitionNode {
 
-    @Ignore
+    @Test
     public void testProcessMissingParamKeys() throws Exception {
         PropertyDefinitionNode propertyDefinitionNode = new PropertyDefinitionNode();
         Map<String, String> inParams = new HashMap<String, String>();
