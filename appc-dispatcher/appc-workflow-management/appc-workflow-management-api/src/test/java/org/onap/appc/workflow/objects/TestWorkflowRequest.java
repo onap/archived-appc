f13b552850b111291a18e7b3a31002cd3fef225a
Sandeep J <sandeejh@in.ibm.com>
2018-07-31 16:49:51 +0530

    added test cases to TestWorkFlowRequest.java
    to increase code coverage
    
    Issue-ID: APPC-1086
    Change-Id: Ic13af8abd0b984111cec2b8b4c822ea0f85c6286
    Signed-off-by: Sandeep J <sandeejh@in.ibm.com>

--- appc-dispatcher/appc-workflow-management/appc-workflow-management-api/src/test/java/org/onap/appc/workflow/objects/TestWorkflowRequest.java
+++ appc-dispatcher/appc-workflow-management/appc-workflow-management-api/src/test/java/org/onap/appc/workflow/objects/TestWorkflowRequest.java
@@ -4,6 +4,8 @@
 * ================================================================================
 * Copyright 2018 TechMahindra
 *=================================================================================
+* Modifications Copyright 2018 IBM.
+*=================================================================================
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
@@ -22,6 +24,9 @@ package org.onap.appc.workflow.objects;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.onap.appc.domainmodel.lcm.RequestContext;
+import org.onap.appc.domainmodel.lcm.ResponseContext;
+import org.onap.appc.domainmodel.lcm.VNFContext;
 
 public class TestWorkflowRequest {
     private WorkflowRequest workflowRequest=new WorkflowRequest(); 
@@ -34,5 +39,26 @@ public class TestWorkflowRequest {
     @Test
     public void testToString_ContainsString() {
         assertTrue(workflowRequest.toString().contains("requestContext"));
+    }
+    
+    @Test
+    public void testGetRequestContext() {
+        RequestContext requestContext= new RequestContext();
+        workflowRequest.setRequestContext(requestContext);
+        assertEquals(requestContext, workflowRequest.getRequestContext());
+    }
+    
+    @Test
+    public void testGetResponseContext() {
+        ResponseContext responseContext= new ResponseContext();
+        workflowRequest.setResponseContext(responseContext);
+        assertEquals(responseContext, workflowRequest.getResponseContext());
+    }
+    
+    @Test
+    public void testGetVnfContext() {
+        VNFContext vnfContext= new VNFContext();
+        workflowRequest.setVnfContext(vnfContext);
+        assertEquals(vnfContext, workflowRequest.getVnfContext());
     }
 }
