343b1b67b04e41547963faa72134cb8970168051
Dilip kumar Pampana <dilip.kumar.pampana@ibm.com>
2018-08-06 13:44:56 -0400

    Junits for inbound/InterfaceServices
    Code Coverage for appc-Inbound/InterfaceServices
    
    Issue-ID: APPC-1132
    Change-Id: Idd36429115f2d324fded4ab4106226cfad33bb65
    Signed-off-by: Dilip kumar Pampana <dilip.kumar.pampana@ibm.com>

--- appc-inbound/appc-interfaces-service/bundle/src/test/java/org/onap/appc/interfaceService/serviceExecutor/TestServiceExecutor.java
+++ appc-inbound/appc-interfaces-service/bundle/src/test/java/org/onap/appc/interfaceService/serviceExecutor/TestServiceExecutor.java
@@ -6,7 +6,7 @@
  * ================================================================================
  * Copyright (C) 2017 Amdocs
  * ================================================================================
- * Copyright (C) 2018 IBM
+ * Modification Copyright (C) 2018 IBM
  * =============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,6 +31,8 @@ import org.junit.Test;
 
 import org.onap.appc.interfaces.service.executorImpl.ServiceExecutorImpl;
 import org.onap.appc.interfaces.service.data.ScopeOverlap;
+import org.onap.appc.interfaces.service.executor.ServiceExecutor;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.powermock.reflect.Whitebox;
@@ -53,5 +55,87 @@ public class TestServiceExecutor {
         scopeOverlap = mapper.readValue(requestData, ScopeOverlap.class);
         boolean result = Whitebox.invokeMethod(sei, "isVserverOrVnfcIdOverLap",scopeOverlap);
         assertEquals(true, result);
+    }
+
+    @Test
+    public void serviceExecutor() throws Exception {
+        ServiceExecutor sei = new ServiceExecutor();
+        String action ="getdatabymodel";
+        String requestDataType = "";
+        String requestData = "{\"vnf-id\":\"ibcx8888v\",\"current-request\" :{\"action\" : \"Audit\",\"action-identifiers\" : {\"service-instance-id\" : \"service-instance-id\",\"vnf-id\" : \"vnf-id\",\"vnfc-name\" : \"vnfc-name\",\"vf-module-id\" : \"vf-module-id\",\"vserver-id\": \"vserver-id\"}},\"in-progress-requests\" :[{\"action\" : \"HealthCheck\",\"action-identifiers\" : {\"service-instance-id\" : \"service-instance-id1\",\"vnf-id\" : \"vnf-id1\",\"vnfc-name\" : \"vnfc-name1\",\"vf-module-id\" : \"vf-module-id\",\"vserver-id\": \"vserver-id1\"}},{\"action\" : \"CheckLock\",\"action-identifiers\" : {\"service-instance-id\" : \"service-instance-id2\",\"vnf-id\" : \"vnf-id2\",\"vnfc-name\" : \"vnfc-name2\",\"vf-module-id\" : \"vf-module-id2\",\"vserver-id\": \"vserver-id2\"}}]}";
+       String result = sei.execute(action, requestData, requestDataType);
+       assertEquals(null, result);
+    }
+
+    @Test(expected = Exception.class)
+    public void serviceExecutorException() throws Exception {
+        ServiceExecutor sei = new ServiceExecutor();
+        String action ="";
+        String requestDataType = "";
+        String requestData = "{\"vnf-id\":\"ibcx8888v\",\"current-request\" :{\"action\" : \"Audit\",\"action-identifiers\" : {\"service-instance-id\" : \"service-instance-id\",\"vnf-id\" : \"vnf-id\",\"vnfc-name\" : \"vnfc-name\",\"vf-module-id\" : \"vf-module-id\",\"vserver-id\": \"vserver-id\"}},\"in-progress-requests\" :[{\"action\" : \"HealthCheck\",\"action-identifiers\" : {\"service-instance-id\" : \"service-instance-id1\",\"vnf-id\" : \"vnf-id1\",\"vnfc-name\" : \"vnfc-name1\",\"vf-module-id\" : \"vf-module-id\",\"vserver-id\": \"vserver-id1\"}},{\"action\" : \"CheckLock\",\"action-identifiers\" : {\"service-instance-id\" : \"service-instance-id2\",\"vnf-id\" : \"vnf-id2\",\"vnfc-name\" : \"vnfc-name2\",\"vf-module-id\" : \"vf-module-id2\",\"vserver-id\": \"vserver-id2\"}}]}";
+        sei.execute(action, requestData, requestDataType);
+    }
+
+    @Test
+    public void serviceExecutorRequest() throws Exception {
+        ServiceExecutor sei = new ServiceExecutor();
+        String action ="isScopeOverlap";
+        String requestDataType = "";
+        String requestData = "{\"vnf-id\":\"ibcx8888v\",\"current-request\" :{\"action\" : \"Audit\",\"action-identifiers\" : {\"service-instance-id\" : \"service-instance-id\",\"vnf-id\" : \"vnf-id\",\"vnfc-name\" : \"vnfc-name\",\"vf-module-id\" : \"vf-module-id\",\"vserver-id\": \"vserver-id\"}},\"in-progress-requests\" :[{\"action\" : \"HealthCheck\",\"action-identifiers\" : {\"service-instance-id\" : \"service-instance-id1\",\"vnf-id\" : \"vnf-id1\",\"vnfc-name\" : \"vnfc-name1\",\"vf-module-id\" : \"vf-module-id\",\"vserver-id\": \"vserver-id1\"}},{\"action\" : \"CheckLock\",\"action-identifiers\" : {\"service-instance-id\" : \"service-instance-id2\",\"vnf-id\" : \"vnf-id2\",\"vnfc-name\" : \"vnfc-name2\",\"vf-module-id\" : \"vf-module-id2\",\"vserver-id\": \"vserver-id2\"}}]}";
+        String actual ="\"requestOverlap\"  : true";
+        String result = sei.execute(action, requestData, requestDataType);
+        assertEquals(actual,result);
+    }
+
+    @Test
+    public void serviceExecutorRqsFal() throws Exception {
+        ServiceExecutor sei = new ServiceExecutor();
+        String action ="isScopeOverlap";
+        String requestDataType = "";
+        String actual ="\"requestOverlap\"  : false";
+        String requestData = "{\"vnf-id\":\"ibcx8888v\",\"current-request\" :{\"action\" : \"Audit\",\"action-identifiers\" : {\"service-instance-id\" : \"service-instance-id\",\"vnf-id\" : \"vnf-id\",\"vnfc-name\" : \"vnfc-name\",\"vf-module-id\" : \"vf-module-id\",\"vserver-id\": \"vserver-id\"}}}";        
+        String result = sei.execute(action, requestData, requestDataType);
+        assertEquals(actual,result);
+    }
+
+    @Test(expected = Exception.class)
+    public void serviceExecutorRqstEx() throws Exception {
+        ServiceExecutor sei = new ServiceExecutor();
+        String action ="isScopeOverlap";
+        String requestDataType = "";
+        String requestData = "{\"vnf-id\":\"ibcx8888v\",\"current-request\" :{\"action\" : \"Audit\",\"action-identifiers\" : {\"service-instance-id\" : \"service-instance-id\"}},\"in-progress-requests\" :[{\"action\" : \"HealthCheck\",\"action-identifiers\" : {\"service-instance-id\" : \"service-instance-id1\",\"vnf-id\" : \"vnf-id1\",\"vnfc-name\" : \"vnfc-name1\",\"vf-module-id\" : \"vf-module-id\",\"vserver-id\": \"vserver-id1\"}},{\"action\" : \"CheckLock\",\"action-identifiers\" : {\"service-instance-id\" : \"service-instance-id2\",\"vnf-id\" : \"vnf-id2\",\"vnfc-name\" : \"vnfc-name2\",\"vf-module-id\" : \"vf-module-id2\",\"vserver-id\": \"vserver-id2\"}}]}";        
+        sei.execute(action, requestData, requestDataType);
+    }
+
+    @Test
+    public void isVserverOrVnfcIdOverlap() throws Exception{
+        String requestData = "{\"vnf-id\":\"ibcx8888v\",\"current-request\" :{\"action\" : \"Audit\",\"action-identifiers\" : {\"service-instance-id\" : \"service-instance-id\"}},\"in-progress-requests\" :[{\"action\" : \"HealthCheck\",\"action-identifiers\" : {\"service-instance-id\" : \"service-instance-id1\",\"vf-module-id\" : \"vf-module-id\",\"vserver-id\": \"vserver-id1\"}},{\"action\" : \"CheckLock\",\"action-identifiers\" : {\"service-instance-id\" : \"service-instance-id2\",\"vf-module-id\" : \"vf-module-id2\",\"vserver-id\": \"vserver-id2\"}}]}";
+        ServiceExecutorImpl sei = new ServiceExecutorImpl();
+        ScopeOverlap scopeOverlap = new ScopeOverlap();
+        ObjectMapper mapper = new ObjectMapper();
+        scopeOverlap = mapper.readValue(requestData, ScopeOverlap.class);
+        boolean result = Whitebox.invokeMethod(sei, "isVserverOrVnfcIdOverLap",scopeOverlap);
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void isVserverOrVnfcIdO() throws Exception{
+        String requestData = "{\"vnf-id\":\"ibcx8888v\",\"current-request\" :{\"action\" : \"Audit\",\"action-identifiers\" : {\"service-instance-id\" : \"service-instance-id\",\"vserver-id\": \"vserver-id1\"}},\"in-progress-requests\" :[{\"action\" : \"HealthCheck\",\"action-identifiers\" : {\"service-instance-id\" : \"service-instance-id1\",\"vserver-id\": \"vserver-id1\"}},{\"action\" : \"CheckLock\",\"action-identifiers\" : {\"service-instance-id\" : \"service-instance-id2\",\"vserver-id\": \"vserver-id2\"}}]}";
+        ServiceExecutorImpl sei = new ServiceExecutorImpl();
+        ScopeOverlap scopeOverlap = new ScopeOverlap();
+        ObjectMapper mapper = new ObjectMapper();
+        scopeOverlap = mapper.readValue(requestData, ScopeOverlap.class);
+        boolean result = Whitebox.invokeMethod(sei, "isVserverOrVnfcIdOverLap",scopeOverlap);
+        assertEquals(true, result);
+    }
+
+    @Test(expected = Exception.class)
+    public void isVserverOrVnfcId() throws Exception{
+        String requestData =  "{\"vnf-id\":\"ibcx8888v\",\"current-request\" :{\"action\" : \"Audit\",\"action-identifiers\" : {\"service-instance-id\" : \"service-instance-id\",\"vnfc-name\" : \"vnfc-name2\"}},\"in-progress-requests\" :[{\"action\" : \"HealthCheck\",\"action-identifiers\" : {\"service-instance-id\" : \"service-instance-id1\"}},{\"action\" : \"CheckLock\",\"action-identifiers\" : {\"service-instance-id\" : \"service-instance-id2\",\"vnfc-name\" : \"vnfc-name2\"}}]}";
+        ServiceExecutorImpl sei = new ServiceExecutorImpl();
+        ScopeOverlap scopeOverlap = new ScopeOverlap();
+        ObjectMapper mapper = new ObjectMapper();
+        scopeOverlap = mapper.readValue(requestData, ScopeOverlap.class);
+        Whitebox.invokeMethod(sei, "isVserverOrVnfcIdOverLap",scopeOverlap);
     }
 }
