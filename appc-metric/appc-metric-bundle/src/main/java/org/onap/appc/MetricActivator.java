f4f2bc5fe104866b83647a5e46944d76f6d82fab
Sandeep J <sandeejh@in.ibm.com>
2018-08-20 17:29:27 +0530

    resolved SONAR issue in MetricActivator.java
    resolved SONAR issue
    
    Issue-ID: APPC-1086
    Change-Id: I1d91fbebb938480f5470055768d9cff9e5902a57
    Signed-off-by: Sandeep J <sandeejh@in.ibm.com>

--- appc-metric/appc-metric-bundle/src/main/java/org/onap/appc/MetricActivator.java
+++ appc-metric/appc-metric-bundle/src/main/java/org/onap/appc/MetricActivator.java
@@ -6,6 +6,8 @@
  * ================================================================================
  * Copyright (C) 2017 Amdocs
  * =============================================================================
+ * Modifications Copyright (C) 2018 IBM.
+ * =============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -50,6 +52,6 @@ public class MetricActivator implements BundleActivator {
 
     @Override
     public void stop(BundleContext bundleContext) throws Exception {
-
+        throw new UnsupportedOperationException();
     }
 }
