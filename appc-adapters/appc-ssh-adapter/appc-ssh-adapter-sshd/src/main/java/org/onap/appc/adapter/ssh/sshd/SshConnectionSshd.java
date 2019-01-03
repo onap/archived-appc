a816582c8915b0da514a99932924dd6bff50607e
ezhil <ezhrajam@in.ibm.com>
2019-01-03 21:43:44 +0530

    Fixed sonar issue  in SshConnectionSshd.java
    Change-Id: Id22ec7bc6191cb59c106b886fbcfcc7c1b4a7219
    Issue-ID: APPC-1300
    Signed-off-by: ezhil <ezhrajam@in.ibm.com>

--- appc-adapters/appc-ssh-adapter/appc-ssh-adapter-sshd/src/main/java/org/onap/appc/adapter/ssh/sshd/SshConnectionSshd.java
+++ appc-adapters/appc-ssh-adapter/appc-ssh-adapter-sshd/src/main/java/org/onap/appc/adapter/ssh/sshd/SshConnectionSshd.java
@@ -6,6 +6,8 @@
  * ================================================================================
  * Copyright (C) 2017 Amdocs
  * =============================================================================
+ * Modifications Copyright (C) 2019 IBM.
+ * =============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -173,11 +175,7 @@ class SshConnectionSshd implements SshConnection {
     }
 
     private int execCommand(String cmd, OutputStream out, OutputStream err, boolean usePty) {
-        try {
-            if (logger.isDebugEnabled()) {
-                logger.debug("SSH: executing command");
-            }
-            ChannelExec client = clientSession.createExecChannel(cmd);
+        try(ChannelExec client = clientSession.createExecChannel(cmd)) {
             client.setUsePty(usePty); // use pseudo-tty?
             client.setOut(out);
             client.setErr(err);
@@ -192,15 +190,15 @@ class SshConnectionSshd implements SshConnection {
                         + ":" + port + "]. Operation timed out.");
                 }
                 exitStatus = exitStatusI;
-            } finally {
-                client.close(false);
-            }
+            } 
             return exitStatus;
         } catch (RuntimeException e) {
             throw e;
         } catch (Exception e1) {
             throw new SshException(
                 "Error executing command [" + cmd + "] over SSH [" + username + "@" + host + ":" + port + "]", e1);
+        } finally{
+            client.close();
         }
     }
 
@@ -218,6 +216,7 @@ class SshConnectionSshd implements SshConnection {
                      * amount of delay time needed and reenter the sleep until we get to the future time.
                      */
                     time = future - System.currentTimeMillis();
+                    Thread.currentThread().interrupt();
                 }
             }
         }
