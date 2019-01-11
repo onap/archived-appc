0a5e4d14bd96a8c2a77a45d221c9dd4b79dfc8bd
Joss Armstrong <joss.armstrong@ericsson.com>
2019-01-11 22:33:49 +0000

    Fix unit tests in provider-lcm-service
    Increased line coverage from 69% to 87%
    Removed PrepareForTest annotations for classes under test
    
    Issue-ID: APPC-1325
    Change-Id: I75be7235f4c13b0f395843907958d7391a333a32
    Signed-off-by: Joss Armstrong <joss.armstrong@ericsson.com>

--- appc-provider/appc-provider-bundle/src/test/java/org/onap/appc/provider/lcm/service/ResumeTrafficServiceTest.java
+++ appc-provider/appc-provider-bundle/src/test/java/org/onap/appc/provider/lcm/service/ResumeTrafficServiceTest.java
@@ -5,6 +5,8 @@
  * Copyright (C) 2017-2018 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Copyright (C) 2017 Amdocs
+ * ================================================================================
+ * Modifications (C) 2019 Ericsson
  * =============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,34 +26,23 @@
 package org.onap.appc.provider.lcm.service;
 
 import org.junit.Assert;
-import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.mockito.internal.util.reflection.Whitebox;
 import org.opendaylight.yang.gen.v1.org.onap.appc.lcm.rev160108.*;
 import org.opendaylight.yang.gen.v1.org.onap.appc.lcm.rev160108.action.identifiers.ActionIdentifiers;
 import org.opendaylight.yang.gen.v1.org.onap.appc.lcm.rev160108.common.header.CommonHeader;
 import org.opendaylight.yang.gen.v1.org.onap.appc.lcm.rev160108.status.Status;
-import org.onap.appc.domainmodel.lcm.ResponseContext;
 import org.onap.appc.executor.objects.LCMCommandStatus;
-import org.onap.appc.requesthandler.objects.RequestHandlerOutput;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
-import static org.powermock.api.mockito.PowerMockito.whenNew;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({ResumeTrafficService.class, RequestExecutor.class})
+
 public class ResumeTrafficServiceTest {
-    private final Action myAction = Action.ResumeTraffic;
-    private final String PAYLOAD_STRING = "{\"A\":\"A-value\",\"B\":{\"C\":\"B.C-value\",\"D\":\"B.D-value\"}}";
-    private ResumeTrafficInput mockInput = mock(ResumeTrafficInput.class);
     private CommonHeader mockCommonHeader = mock(CommonHeader.class);
     private ActionIdentifiers mockAI = mock(ActionIdentifiers.class);
     private Payload mockPayload = mock(Payload.class);
@@ -62,7 +53,6 @@ public class ResumeTrafficServiceTest {
         resumeServiceAction = spy(new ResumeTrafficService());
     }
 
-
     @Test
     public void testConstructor() throws Exception {
         Action expectedAction = Action.ResumeTraffic;
@@ -71,58 +61,6 @@ public class ResumeTrafficServiceTest {
         Assert.assertEquals("Should have resume-traffic RPC name", "resume-traffic",
                 (org.powermock.reflect.Whitebox.getInternalState(resumeServiceAction, "rpcName")).toString());
     }
-
-//    @Test
-//    public void testProcess() throws Exception {
-//        // test error occurs in validation
-//        ResumeTrafficOutputBuilder outputBuilder = resumeServiceAction.process(mockInput);
-//        Mockito.verify(resumeServiceAction, times(0)).proceedAction(any());
-//        Assert.assertTrue("Should not have commonHeader as we did not mock it",outputBuilder.getCommonHeader() == null);
-//        Assert.assertEquals("should return missing parameter status",
-//            Integer.valueOf(LCMCommandStatus.MISSING_MANDATORY_PARAMETER.getResponseCode()),
-//            outputBuilder.getStatus().getCode());
-//
-//        // make validation pass
-//        Mockito.doReturn(mockCommonHeader).when(mockInput).getCommonHeader();
-//
-//        // to make validation pass
-//        ZULU zuluTimeStamp = new ZULU("2017-06-29T21:44:00.35Z");
-//        Mockito.doReturn(zuluTimeStamp).when(mockCommonHeader).getTimestamp();
-//        Mockito.doReturn("api ver").when(mockCommonHeader).getApiVer();
-//        Mockito.doReturn("orignator Id").when(mockCommonHeader).getOriginatorId();
-//        Mockito.doReturn("request Id").when(mockCommonHeader).getRequestId();
-//
-//        Mockito.doReturn(myAction).when(mockInput).getAction();
-//        Mockito.doReturn(mockAI).when(mockInput).getActionIdentifiers();
-//        Mockito.doReturn("vnfId").when(mockAI).getVnfId();
-//
-//        // test processAction return with error
-//        outputBuilder = resumeServiceAction.process(mockInput);
-//        Mockito.verify(resumeServiceAction, times(1)).proceedAction(any());
-//        Assert.assertTrue("Should have commonHeader",outputBuilder.getCommonHeader() != null);
-//        Assert.assertEquals("should return rejected status",
-//            Integer.valueOf(LCMCommandStatus.REJECTED.getResponseCode()),
-//            outputBuilder.getStatus().getCode());
-//
-//        // test processAction return without error
-//        RequestExecutor mockExecutor = mock(RequestExecutor.class);
-//        whenNew(RequestExecutor.class).withNoArguments().thenReturn(mockExecutor);
-//
-//        RequestHandlerOutput mockOutput = mock(RequestHandlerOutput.class);
-//        Mockito.doReturn(mockOutput).when(mockExecutor).executeRequest(any());
-//
-//        ResponseContext mockResponseContext = mock(ResponseContext.class);
-//        Mockito.doReturn(mockResponseContext).when(mockOutput).getResponseContext();
-//
-//        org.onap.appc.domainmodel.lcm.Status mockStatus = mock(org.onap.appc.domainmodel.lcm.Status.class);
-//        Integer successCode = Integer.valueOf(LCMCommandStatus.SUCCESS.getResponseCode());
-//        Mockito.doReturn(successCode).when(mockStatus).getCode();
-//        Mockito.doReturn(mockStatus).when(mockResponseContext).getStatus();
-//
-//        outputBuilder = resumeServiceAction.process(mockInput);
-//        Assert.assertTrue("Should have commonHeader",outputBuilder.getCommonHeader() != null);
-//        Assert.assertEquals("should return success status", successCode, outputBuilder.getStatus().getCode());
-//    }
 
     @Test
     public void testValidate() throws Exception {
@@ -157,7 +95,7 @@ public class ResumeTrafficServiceTest {
         status = (Status) Whitebox.getInternalState(resumeServiceAction, "status");
         Assert.assertEquals("should return invalid parameter",
                 Integer.valueOf(LCMCommandStatus.INVALID_INPUT_PARAMETER.getResponseCode()), status.getCode());
-    // test null payload
+        // test null payload
         Mockito.doReturn("vnfId").when(mockAI).getVnfId();
         resumeServiceAction.validate(mockCommonHeader, Action.ResumeTraffic, mockAI, null);
         Mockito.verify(resumeServiceAction, times(1)).validateExcludedActIds(any(), any());
@@ -166,7 +104,6 @@ public class ResumeTrafficServiceTest {
                 Integer.valueOf(LCMCommandStatus.MISSING_MANDATORY_PARAMETER.getResponseCode()), status.getCode());
 
         // test empty payload
-
         Mockito.doReturn("").when(mockPayload).getValue();
         resumeServiceAction.validate(mockCommonHeader, Action.ResumeTraffic, mockAI, mockPayload);
         status = (Status) Whitebox.getInternalState(resumeServiceAction, "status");
@@ -174,12 +111,12 @@ public class ResumeTrafficServiceTest {
                 Integer.valueOf(LCMCommandStatus.INVALID_INPUT_PARAMETER.getResponseCode()), status.getCode());
 
         // test space payload
+        Mockito.doReturn(null).when(resumeServiceAction).validateMustHaveParamValue(Mockito.anyString(),
+                Mockito.anyString());
         Mockito.doReturn(" ").when(mockPayload).getValue();
         resumeServiceAction.validate(mockCommonHeader, Action.ResumeTraffic, mockAI, mockPayload);
         status = (Status) Whitebox.getInternalState(resumeServiceAction, "status");
         Assert.assertEquals("should return invalid parameter",
-                Integer.valueOf(LCMCommandStatus.INVALID_INPUT_PARAMETER.getResponseCode()), status.getCode());
-    
+                Integer.valueOf(LCMCommandStatus.UNEXPECTED_ERROR.getResponseCode()), status.getCode());
     }
-
 }
--- appc-provider/appc-provider-bundle/src/test/java/org/onap/appc/provider/lcm/service/VolumeServiceTest.java
+++ appc-provider/appc-provider-bundle/src/test/java/org/onap/appc/provider/lcm/service/VolumeServiceTest.java
@@ -5,6 +5,8 @@
  * Copyright (C) 2017-2018 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Copyright (C) 2017 Amdocs
+ * ================================================================================
+ * Modifications (C) 2019 Ericsson
  * =============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,37 +28,23 @@ package org.onap.appc.provider.lcm.service;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.mockito.internal.util.reflection.Whitebox;
 import org.opendaylight.yang.gen.v1.org.onap.appc.lcm.rev160108.Action;
-import org.opendaylight.yang.gen.v1.org.onap.appc.lcm.rev160108.AttachVolumeInput;
-import org.opendaylight.yang.gen.v1.org.onap.appc.lcm.rev160108.AttachVolumeOutputBuilder;
-import org.opendaylight.yang.gen.v1.org.onap.appc.lcm.rev160108.DetachVolumeInput;
-import org.opendaylight.yang.gen.v1.org.onap.appc.lcm.rev160108.DetachVolumeOutputBuilder;
 import org.opendaylight.yang.gen.v1.org.onap.appc.lcm.rev160108.Payload;
 import org.opendaylight.yang.gen.v1.org.onap.appc.lcm.rev160108.ZULU;
 import org.opendaylight.yang.gen.v1.org.onap.appc.lcm.rev160108.action.identifiers.ActionIdentifiers;
 import org.opendaylight.yang.gen.v1.org.onap.appc.lcm.rev160108.common.header.CommonHeader;
 import org.opendaylight.yang.gen.v1.org.onap.appc.lcm.rev160108.status.Status;
-import org.onap.appc.domainmodel.lcm.ResponseContext;
 import org.onap.appc.executor.objects.LCMCommandStatus;
-import org.onap.appc.requesthandler.objects.RequestHandlerOutput;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
-import static org.powermock.api.mockito.PowerMockito.whenNew;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({VolumeService.class, RequestExecutor.class})
+
 public class VolumeServiceTest {
-    private final String PAYLOAD_STRING = "{\"A\":\"A-value\",\"B\":{\"C\":\"B.C-value\",\"D\":\"B.D-value\"}}";
-    private AttachVolumeInput mockAttachInput = mock(AttachVolumeInput.class);
-    private DetachVolumeInput mockDetachInput = mock(DetachVolumeInput.class);
     private CommonHeader mockCommonHeader = mock(CommonHeader.class);
     private ActionIdentifiers mockAI = mock(ActionIdentifiers.class);
     private Payload mockPayload = mock(Payload.class);
@@ -81,123 +69,9 @@ public class VolumeServiceTest {
         expectedAction = Action.DetachVolume;
         Assert.assertEquals("Should have proper ACTION", expectedAction,
             (Action) org.powermock.reflect.Whitebox.getInternalState(volumeServiceForDetachAction, "expectedAction"));
-        Assert.assertEquals("Should have detach-volume RPC name","detach-volume",
+        Assert.assertEquals("Should have detach-volume RPC name", "detach-volume",
             (org.powermock.reflect.Whitebox.getInternalState(volumeServiceForDetachAction, "rpcName")).toString());
     }
-
-//    @Test
-//    public void testAttachVolume() throws Exception {
-//        // test error occurs in validation
-//        AttachVolumeOutputBuilder outputBuilder = volumeServiceForAttachAction.attachVolume(mockAttachInput);
-//        Mockito.verify(volumeServiceForAttachAction, times(0)).proceedAction(any(), any(), any());
-//        Assert.assertTrue("Should not have commonHeader as we did not mock it",
-//            outputBuilder.getCommonHeader() == null);
-//        Assert.assertEquals("should return missing parameter status",
-//            Integer.valueOf(LCMCommandStatus.MISSING_MANDATORY_PARAMETER.getResponseCode()),
-//            outputBuilder.getStatus().getCode());
-//
-//        // make validation pass
-//        Mockito.doReturn(mockCommonHeader).when(mockAttachInput).getCommonHeader();
-//        Mockito.doReturn(mockPayload).when(mockAttachInput).getPayload();
-//        Mockito.doReturn(PAYLOAD_STRING).when(mockPayload).getValue();
-//
-//        ZULU zuluTimeStamp = new ZULU("2017-06-29T21:44:00.35Z");
-//        Mockito.doReturn(zuluTimeStamp).when(mockCommonHeader).getTimestamp();
-//        Mockito.doReturn("api ver").when(mockCommonHeader).getApiVer();
-//        Mockito.doReturn("orignator Id").when(mockCommonHeader).getOriginatorId();
-//        Mockito.doReturn("request Id").when(mockCommonHeader).getRequestId();
-//
-//        Mockito.doReturn(Action.AttachVolume).when(mockAttachInput).getAction();
-//        Mockito.doReturn(mockAI).when(mockAttachInput).getActionIdentifiers();
-//        Mockito.doReturn("vserverId").when(mockAI).getVserverId();
-
-        // test proceedAction return with error
-//        outputBuilder = volumeServiceForAttachAction.attachVolume(mockAttachInput);
-//        Mockito.verify(volumeServiceForAttachAction, times(1)).proceedAction(any(), any(), any());
-//        Assert.assertTrue("Should have commonHeader",
-//            outputBuilder.getCommonHeader() != null);
-//        Assert.assertEquals("should return rejected status",
-//            Integer.valueOf(LCMCommandStatus.REJECTED.getResponseCode()),
-//            outputBuilder.getStatus().getCode());
-
-        // test proceedAction return without error
-//        RequestExecutor mockExecutor = mock(RequestExecutor.class);
-//        whenNew(RequestExecutor.class).withNoArguments().thenReturn(mockExecutor);
-//
-//        RequestHandlerOutput mockOutput = mock(RequestHandlerOutput.class);
-//        Mockito.doReturn(mockOutput).when(mockExecutor).executeRequest(any());
-//
-//        ResponseContext mockResponseContext = mock(ResponseContext.class);
-//        Mockito.doReturn(mockResponseContext).when(mockOutput).getResponseContext();
-//
-//        org.onap.appc.domainmodel.lcm.Status mockStatus = mock(org.onap.appc.domainmodel.lcm.Status.class);
-//        Integer successCode = Integer.valueOf(LCMCommandStatus.SUCCESS.getResponseCode());
-//        Mockito.doReturn(successCode).when(mockStatus).getCode();
-//        Mockito.doReturn(mockStatus).when(mockResponseContext).getStatus();
-//
-//        outputBuilder = volumeServiceForAttachAction.attachVolume(mockAttachInput);
-//        Assert.assertTrue("Should have commonHeader",
-//            outputBuilder.getCommonHeader() != null);
-//        Assert.assertEquals("should return success status",
-//            successCode, outputBuilder.getStatus().getCode());
-//    }
-
-//    @Test
-//    public void testDetachVolumn() throws Exception {
-//        // test error occurs in validation
-//        DetachVolumeOutputBuilder outputBuilder = volumeServiceForDetachAction.detachVolume(mockDetachInput);
-//        Mockito.verify(volumeServiceForDetachAction, times(0)).proceedAction(any(), any(), any());
-//        Assert.assertTrue("Should not have commonHeader as we did not mock it",
-//            outputBuilder.getCommonHeader() == null);
-//        Assert.assertEquals("should return missing parameter status",
-//            Integer.valueOf(LCMCommandStatus.MISSING_MANDATORY_PARAMETER.getResponseCode()),
-//            outputBuilder.getStatus().getCode());
-//
-//        // make validation pass
-//        Mockito.doReturn(mockCommonHeader).when(mockDetachInput).getCommonHeader();
-//        Mockito.doReturn(mockPayload).when(mockDetachInput).getPayload();
-//        Mockito.doReturn(PAYLOAD_STRING).when(mockPayload).getValue();
-//
-//        ZULU zuluTimeStamp = new ZULU("2017-06-29T21:44:00.35Z");
-//        Mockito.doReturn(zuluTimeStamp).when(mockCommonHeader).getTimestamp();
-//        Mockito.doReturn("api ver").when(mockCommonHeader).getApiVer();
-//        Mockito.doReturn("orignator Id").when(mockCommonHeader).getOriginatorId();
-//        Mockito.doReturn("request Id").when(mockCommonHeader).getRequestId();
-//
-//        Mockito.doReturn(Action.DetachVolume).when(mockDetachInput).getAction();
-//        Mockito.doReturn(mockAI).when(mockDetachInput).getActionIdentifiers();
-//        Mockito.doReturn("vserverId").when(mockAI).getVserverId();
-
-        // test proceedAction return with error
-//        outputBuilder = volumeServiceForDetachAction.detachVolume(mockDetachInput);
-//        Mockito.verify(volumeServiceForDetachAction, times(1)).proceedAction(any(), any(), any());
-//        Assert.assertTrue("Should have commonHeader",
-//            outputBuilder.getCommonHeader() != null);
-//        Assert.assertEquals("should return rejected status",
-//            Integer.valueOf(LCMCommandStatus.REJECTED.getResponseCode()),
-//            outputBuilder.getStatus().getCode());
-
-        // test proceedAction return without error
-//        RequestExecutor mockExecutor = mock(RequestExecutor.class);
-//        whenNew(RequestExecutor.class).withNoArguments().thenReturn(mockExecutor);
-//
-//        RequestHandlerOutput mockOutput = mock(RequestHandlerOutput.class);
-//        Mockito.doReturn(mockOutput).when(mockExecutor).executeRequest(any());
-//
-//        ResponseContext mockResponseContext = mock(ResponseContext.class);
-//        Mockito.doReturn(mockResponseContext).when(mockOutput).getResponseContext();
-//
-//        org.onap.appc.domainmodel.lcm.Status mockStatus = mock(org.onap.appc.domainmodel.lcm.Status.class);
-//        Integer successCode = Integer.valueOf(LCMCommandStatus.SUCCESS.getResponseCode());
-//        Mockito.doReturn(successCode).when(mockStatus).getCode();
-//        Mockito.doReturn(mockStatus).when(mockResponseContext).getStatus();;
-//
-//        outputBuilder = volumeServiceForDetachAction.detachVolume(mockDetachInput);
-//        Assert.assertTrue("Should have commonHeader",
-//            outputBuilder.getCommonHeader() != null);
-//        Assert.assertEquals("should return success status",
-//            successCode, outputBuilder.getStatus().getCode());
-//    }
 
     @Test
     public void testValidateForAttachAction() throws Exception {
