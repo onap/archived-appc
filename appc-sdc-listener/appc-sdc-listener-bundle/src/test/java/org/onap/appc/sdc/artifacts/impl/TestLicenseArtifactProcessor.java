c134b8112560ac165ee513c6250da12b7b2e0a29
Lathishbabu Ganesan <lathishbabu.ganesan@ericsson.com>
2019-01-23 06:37:41 -0500

    Test coverage for sdc-listener-bundle
    Increased the Test coverage from 41.9% to 71%
    
    Issue-ID: APPC-1325
    Change-Id: I33c336a2fff30422e436c26226519332a0a8b55c
    Signed-off-by: Lathishbabu Ganesan <lathishbabu.ganesan@ericsson.com>

--- appc-sdc-listener/appc-sdc-listener-bundle/src/test/java/org/onap/appc/sdc/artifacts/impl/TestLicenseArtifactProcessor.java
+++ appc-sdc-listener/appc-sdc-listener-bundle/src/test/java/org/onap/appc/sdc/artifacts/impl/TestLicenseArtifactProcessor.java
@@ -6,6 +6,8 @@
  * ================================================================================
  * Copyright (C) 2017 Amdocs
  * =============================================================================
+ * Modifications (C) 2019 Ericsson
+ * =============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -21,6 +23,10 @@
  */
 package org.onap.appc.sdc.artifacts.impl;
 
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -31,123 +37,76 @@ import org.onap.appc.exceptions.APPCException;
 import org.onap.appc.sdc.artifacts.helper.ArtifactStorageService;
 import org.onap.appc.sdc.artifacts.object.SDCArtifact;
 import org.onap.sdc.api.IDistributionClient;
-import org.onap.sdc.api.notification.IArtifactInfo;
-import org.onap.sdc.api.notification.INotificationData;
-import org.onap.sdc.api.notification.IResourceInstance;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.reflect.Whitebox;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static org.mockito.Matchers.anyObject;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.verify;
-
 @RunWith(PowerMockRunner.class)
 public class TestLicenseArtifactProcessor {
 
-    private LicenseArtifactProcessor artifactProcessor;
-    private ArtifactStorageService storageService;
+  private LicenseArtifactProcessor artifactProcessor;
+  private ArtifactStorageService storageService;
 
-    @Before
-    public void setup() throws Exception{
-        IDistributionClient client =  PowerMockito.mock(IDistributionClient.class);
-        EventSender eventSender = PowerMockito.mock(EventSender.class);
-        storageService = PowerMockito.mock(ArtifactStorageService.class);
-        artifactProcessor = Mockito.spy(new LicenseArtifactProcessor(client,eventSender,getNotificationData(),getResources().get(0)
-                ,getServiceArtifacts().get(0),null));
-        Whitebox.setInternalState(artifactProcessor,"artifactStorageService", storageService);
-        PowerMockito.doCallRealMethod().when(artifactProcessor).processArtifact((SDCArtifact)Matchers.anyObject());
-        PowerMockito.doNothing().when(storageService).storeSDCArtifact(Matchers.anyObject());
-    }
+  @Before
+  public void setup() throws Exception {
+    IDistributionClient client = PowerMockito.mock(IDistributionClient.class);
+    EventSender eventSender = PowerMockito.mock(EventSender.class);
+    storageService = PowerMockito.mock(ArtifactStorageService.class);
+    artifactProcessor = Mockito.spy(new LicenseArtifactProcessor(client, eventSender,
+        ArtifactProcessorUtility.getNotificationData(),
+        ArtifactProcessorUtility.getResources().get(0),
+        ArtifactProcessorUtility.getServiceArtifacts().get(0), null));
+    Whitebox.setInternalState(artifactProcessor, "artifactStorageService", storageService);
+    PowerMockito.doCallRealMethod().when(artifactProcessor)
+        .processArtifact((SDCArtifact) Matchers.anyObject());
+    PowerMockito.doNothing().when(storageService).storeSDCArtifact(Matchers.anyObject());
+  }
 
-    @Test(expected = org.onap.appc.exceptions.APPCException.class)
-    public void testProcessArtifactWithMissingData() throws APPCException {
-        SDCArtifact artifact=new SDCArtifact();
-        artifact.setResourceVersion("RESOURCE VERSION");
-        artifact.setArtifactUUID("123-456-789");
-        artifactProcessor.processArtifact(artifact);
-    }
-    @Test
-    public void testProcessArtifact() throws APPCException {
-        PowerMockito.when(storageService.retrieveSDCArtifact(anyString(),anyString(),anyString())).thenReturn(null);
-        SDCArtifact artifact=new SDCArtifact();
-        artifact.setResourceVersion("RESOURCE VERSION");
-        artifact.setArtifactUUID("123-456-789");
-        artifact.setResourceName("Resource Name");
-        artifactProcessor.processArtifact(artifact);
-        verify(storageService,Mockito.times(1)).storeSDCArtifact(anyObject());
-    }
-    @Test
-    public void testProcessArtifactWithDuplicateArtifact() throws APPCException {
-        SDCArtifact artifact=new SDCArtifact();
-        artifact.setResourceVersion("RESOURCE VERSION");
-        artifact.setArtifactUUID("123-456-789");
-        artifact.setResourceName("Resource Name");
-        PowerMockito.when(storageService.retrieveSDCArtifact(anyString(),anyString(),anyString())).thenReturn(artifact);
-        artifactProcessor.processArtifact(artifact);
-        verify(storageService,Mockito.times(0)).storeSDCArtifact(anyObject());
-    }
+  @Test(expected = org.onap.appc.exceptions.APPCException.class)
+  public void testProcessArtifactWithMissingData() throws APPCException {
+    SDCArtifact artifact = new SDCArtifact();
+    artifact.setResourceVersion("RESOURCE VERSION");
+    artifact.setArtifactUUID("123-456-789");
+    artifactProcessor.processArtifact(artifact);
+  }
 
-    private INotificationData getNotificationData() throws ClassNotFoundException, IllegalAccessException,
-            InstantiationException, InvocationTargetException {
+  @Test
+  public void testProcessArtifact() throws APPCException {
+    PowerMockito.when(storageService.retrieveSDCArtifact(anyString(), anyString(), anyString()))
+        .thenReturn(null);
+    SDCArtifact artifact = new SDCArtifact();
+    artifact.setResourceVersion("RESOURCE VERSION");
+    artifact.setArtifactUUID("123-456-789");
+    artifact.setResourceName("Resource Name");
+    artifactProcessor.processArtifact(artifact);
+    verify(storageService, Mockito.times(1)).storeSDCArtifact(anyObject());
+  }
 
-        org.onap.sdc.api.notification.INotificationData notificationData = (INotificationData)getObject("org.onap.sdc.impl.NotificationDataImpl");
+  @Test
+  public void testProcessArtifactWithDuplicateArtifact() throws APPCException {
+    SDCArtifact artifact = new SDCArtifact();
+    artifact.setResourceVersion("RESOURCE VERSION");
+    artifact.setArtifactUUID("123-456-789");
+    artifact.setResourceName("Resource Name");
+    PowerMockito.when(storageService.retrieveSDCArtifact(anyString(), anyString(), anyString()))
+        .thenReturn(artifact);
+    artifactProcessor.processArtifact(artifact);
+    verify(storageService, Mockito.times(0)).storeSDCArtifact(anyObject());
+  }
 
-        List<IArtifactInfo> serviceArtifacts = getServiceArtifacts();
-
-        invokeMethod(notificationData, "setServiceArtifacts", serviceArtifacts);
-        return notificationData;
-    }
-
-    private List<IResourceInstance> getResources() throws ClassNotFoundException, InvocationTargetException,
-            InstantiationException, IllegalAccessException {
-        List<IResourceInstance> resources = new ArrayList<>();
-        IResourceInstance resource = (IResourceInstance)getObject("org.onap.sdc.impl.JsonContainerResourceInstance");
-
-        List<IArtifactInfo> serviceArtifacts = getServiceArtifacts();
-        invokeMethod(resource,"setArtifacts",serviceArtifacts);
-        invokeMethod(resource,"setResourceName","Vnf");
-        invokeMethod(resource,"setResourceVersion","1.0");
-
-        resources.add(resource);
-        return resources;
-    }
-
-    private void invokeMethod(Object object, String methodName,Object... arguments) throws IllegalAccessException, InvocationTargetException {
-        Method[] methods = object.getClass().getDeclaredMethods();
-        for(Method method:methods){
-            if(methodName.equalsIgnoreCase(method.getName())){
-                method.setAccessible(true);
-                method.invoke(object,arguments);
-            }
-        }
-    }
-
-    private Object getObject(String fqcn) throws ClassNotFoundException, InstantiationException, IllegalAccessException, InvocationTargetException {
-        Constructor constructor = Arrays.asList(Class.forName(fqcn).getDeclaredConstructors())
-                .stream()
-                .filter(constructor1 -> constructor1.getParameterCount()==0)
-                .collect(Collectors.toList())
-                .get(0);
-        constructor.setAccessible(true);
-        return constructor.newInstance();
-    }
-
-    private List<IArtifactInfo> getServiceArtifacts() throws ClassNotFoundException, InvocationTargetException,
-            InstantiationException, IllegalAccessException {
-        List<IArtifactInfo> serviceArtifacts = new ArrayList<>();
-        IArtifactInfo artifactInfo = (IArtifactInfo)getObject("org.onap.sdc.impl.ArtifactInfoImpl");
-        invokeMethod(artifactInfo,"setArtifactType","TOSCA_CSAR");
-        invokeMethod(artifactInfo,"setArtifactUUID","abcd-efgh-ijkl");
-        serviceArtifacts.add(artifactInfo);
-        return serviceArtifacts;
-    }
+  /**
+   * This method tests the process Artifacts method failure scenario.
+   * 
+   * @throws APPCException if the there are any exception in sdc artifacts
+   */
+  @Test(expected = APPCException.class)
+  public void testProcessArtifactNullSDCArtifact() throws APPCException {
+    SDCArtifact artifact = new SDCArtifact();
+    artifact.setResourceVersion("RESOURCE VERSION");
+    artifact.setArtifactUUID("123-456-789");
+    artifact.setResourceName("Resource Name");
+    when(storageService.retrieveSDCArtifact(anyString(), anyString(), anyString()))
+        .thenThrow(new APPCException());
+    artifactProcessor.processArtifact(artifact);
+  }
 }
