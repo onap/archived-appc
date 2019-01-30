1ab4dfe9b1c30fc92e5ce0d822e2f97ae69f6167
Sudarshan Kumar <sudars19@in.ibm.com>
2019-01-30 13:15:51 +0530

    Data-Junit Test Case Added
    Data-Junit Test Case Added
    
    Issue-ID: APPC-1372
    Change-Id: Ib0a8c0c66d23bedd213df17d517c10add0ee18a9
    Signed-off-by: Sudarshan Kumar <sudars19@in.ibm.com>

--- appc-adapters/appc-iaas-adapter/appc-iaas-adapter-bundle/src/test/java/org/onap/appc/adapter/openstack/heat/model/TestData.java
+++ appc-adapters/appc-iaas-adapter/appc-iaas-adapter-bundle/src/test/java/org/onap/appc/adapter/openstack/heat/model/TestData.java
@@ -4,6 +4,8 @@
 * ================================================================================
 * Copyright 2018 TechMahindra
 *=================================================================================
+* Modifications Copyright (c) 2018-2019 IBM
+*=================================================================================
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
@@ -26,52 +28,58 @@ import org.junit.Test;
 
 public class TestData {
     private Data data;
+    private Environment environment;
+    private Resources__ resources__;
+    private Template template;
 
     @Before
     public void setUp() {
-        data=new Data();
+        data = new Data();
+        environment = new Environment();
+        resources__ = new Resources__();
+        template = new Template();
     }
 
     @Test
     public void testGetStatus() {
         data.setStatus("status");
         assertNotNull(data.getStatus());
-        assertEquals(data.getStatus(),"status");
+        assertEquals(data.getStatus(), "status");
     }
 
     @Test
     public void testGetName() {
         data.setName("XYZ");
         assertNotNull(data.getName());
-        assertEquals(data.getName(),"XYZ");
+        assertEquals(data.getName(), "XYZ");
     }
 
     @Test
     public void testGetStackUserProjectId() {
         data.setStackUserProjectId("stackUserProjectId");
         assertNotNull(data.getStackUserProjectId());
-        assertEquals(data.getStackUserProjectId(),"stackUserProjectId");
+        assertEquals(data.getStackUserProjectId(), "stackUserProjectId");
     }
 
     @Test
     public void testGetAction() {
         data.setAction("action");
         assertNotNull(data.getAction());
-        assertEquals(data.getAction(),"action");
+        assertEquals(data.getAction(), "action");
     }
 
     @Test
     public void testGetProjectId() {
         data.setProjectId("projectId");
         assertNotNull(data.getProjectId());
-        assertEquals(data.getProjectId(),"projectId");
+        assertEquals(data.getProjectId(), "projectId");
     }
 
     @Test
     public void testGetId() {
         data.setId("Id");
         assertNotNull(data.getId());
-        assertEquals(data.getId(),"Id");
+        assertEquals(data.getId(), "Id");
     }
 
     @Test
@@ -80,4 +88,40 @@ public class TestData {
         assertNotEquals(data.toString(), null);
     }
 
+    @Test
+    public void testSetEnvironment() {
+        data.setEnvironment(environment);
+        assertNotNull(data.getEnvironment());
+    }
+
+    @Test
+    public void testGetEnvironment() {
+        data.setEnvironment(environment);
+        assertEquals(data.getEnvironment(), environment);
+    }
+
+    @Test
+    public void testSetTemplate() {
+        data.setTemplate(template);
+        assertNotNull(data.getTemplate());
+    }
+
+    @Test
+    public void testGetTemplate() {
+        data.setTemplate(template);
+        assertEquals(data.getTemplate(), template);
+    }
+
+    @Test
+    public void testSetResources__() {
+        data.setResources(resources__);
+        ;
+        assertNotNull(data.getResources());
+    }
+
+    @Test
+    public void testGetResources__() {
+        data.setResources(resources__);
+        assertEquals(data.getResources(), resources__);
+    }
 }
