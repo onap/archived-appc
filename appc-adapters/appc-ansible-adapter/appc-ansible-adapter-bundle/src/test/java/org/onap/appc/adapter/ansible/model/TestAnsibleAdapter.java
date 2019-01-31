6c7498e9e1baaffadba537258a17c5af4be2da36
Modaboina, Kusumakumari (km583p) <km583p@us.att.com>
2019-01-30 20:23:22 -0500

    ansible adapter changes for multiple ansible servs
    Issue-ID: APPC-1365
    Change-Id: Ie3d102d9efcef7ba98b1fb5920a73a6c64b01897
    Signed-off-by: Modaboina, Kusumakumari (km583p) <km583p@us.att.com>

--- appc-adapters/appc-ansible-adapter/appc-ansible-adapter-bundle/src/test/java/org/onap/appc/adapter/ansible/model/TestAnsibleAdapter.java
+++ appc-adapters/appc-ansible-adapter/appc-ansible-adapter-bundle/src/test/java/org/onap/appc/adapter/ansible/model/TestAnsibleAdapter.java
@@ -25,48 +25,41 @@ package org.onap.appc.adapter.ansible.model;
 import static org.junit.Assert.assertNotNull;
 import java.lang.reflect.*;
 import org.junit.Test;
+import org.json.JSONObject;
 
 public class TestAnsibleAdapter {
 
-    private Class[] parameterTypes;
     private AnsibleMessageParser ansibleMessageParser;
     private Method m;
     private String name;
+    private Method m2;
 
     @Test
-    public void callPrivateConstructorsMethodsForCodeCoverage() throws SecurityException, NoSuchMethodException, IllegalArgumentException, InstantiationException, IllegalAccessException, InvocationTargetException {
+    public void callPrivateConstructorsMethodsForCodeCoverage() throws SecurityException, NoSuchMethodException,
+            IllegalArgumentException, InstantiationException, IllegalAccessException, InvocationTargetException {
 
-          /* test constructors */
-          Class<?>[] classesOne = {AnsibleMessageParser.class};
-          for(Class<?> clazz : classesOne) {
-                Constructor<?> constructor = clazz.getDeclaredConstructor();
-                name = constructor.getName();
-                constructor.setAccessible(true);
-                assertNotNull(constructor.newInstance());
-          }
-          Class<?>[] classesTwo = {AnsibleServerEmulator.class};
-          for(Class<?> clazz : classesTwo) {
-                Constructor<?> constructor = clazz.getDeclaredConstructor();
-                name = constructor.getName();
-                constructor.setAccessible(true);
-                assertNotNull(constructor.newInstance());
-          }
-          Class<?>[] classesThree = {AnsibleResult.class};
-          for(Class<?> clazz : classesThree) {
-                Constructor<?> constructor = clazz.getDeclaredConstructor();
-                name = constructor.getName();
-                constructor.setAccessible(true);
-                assertNotNull(constructor.newInstance());
-          }
+        /* test constructors */
+        Class<?>[] classes = { AnsibleMessageParser.class, AnsibleServerEmulator.class, AnsibleResult.class };
+        for (Class<?> currentClass : classes) {
+            Constructor<?> constructor = currentClass.getDeclaredConstructor();
+            name = constructor.getName();
+            constructor.setAccessible(true);
+            assertNotNull("Constructor " + name + " returned null.", constructor.newInstance());
+        }
 
-          /* test methods */
-          ansibleMessageParser = new AnsibleMessageParser();
-          parameterTypes = new Class[1];
-          parameterTypes[0] = java.lang.String.class;
+        /* test methods */
+        ansibleMessageParser = new AnsibleMessageParser();
+        m = ansibleMessageParser.getClass().getDeclaredMethod("getFilePayload", String.class);
+        m.setAccessible(true);
+        assertNotNull(m.invoke(ansibleMessageParser, "{\"test\": test}"));
+        // test logging-suppression for an invalid host value (Fortify Log Forging fix)
+        String input = "{\"Results\":{\"192.168.1.10\":{\"Id\":\"101\",\"StatusCode\":200,\"StatusMessage\":\"SUCCESS\"},\"192%168%1%10\":{\"Id\":\"102\",\"StatusCode\":200,\"StatusMessage\":\"SUCCESS\"},\"server-dev.att.com\":{\"Id\":\"103\",\"StatusCode\":200,\"StatusMessage\":\"SUCCESS\"}},\"StatusCode\":200,\"StatusMessage\":\"FINISHED\"}";
+        JSONObject postResponse = new JSONObject(input);
+        m2 = ansibleMessageParser.getClass().getDeclaredMethod("parseGetResponseNested", AnsibleResult.class, JSONObject.class);
+        m2.setAccessible(true);
+        AnsibleResult ansibleResult = new AnsibleResult();
 
-          m = ansibleMessageParser.getClass().getDeclaredMethod("getFilePayload", parameterTypes);
-          m.setAccessible(true);
-          assertNotNull(m.invoke(ansibleMessageParser,"{\"test\": test}"));
-
+        // assertNotNull(m2.invoke(ansibleMessageParser, ansibleResult, postResponse));
+        m2.invoke(ansibleMessageParser, ansibleResult, postResponse);
     }
 }
