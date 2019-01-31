0cab0f3f40ae7d23d3eece2665ee8a20a881a527
Sandeep J <sandeejh@in.ibm.com>
2019-01-31 13:20:24 +0530

    added test file to cover Resources__.java
    to increase code coverage
    
    Issue-ID: APPC-1086
    Change-Id: I3fedafa767942bb6427407d65b2eaf2cff4fd88f
    Signed-off-by: Sandeep J <sandeejh@in.ibm.com>

---
+++ appc-adapters/appc-iaas-adapter/appc-iaas-adapter-bundle/src/test/java/org/onap/appc/adapter/openstack/heat/model/TestResources__.java
@@ -0,0 +1,52 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : APPC
+ * ================================================================================
+ * Copyright (C) 2019 IBM.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.appc.adapter.openstack.heat.model;
+
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestResources__ {
+
+    public Resources__ resources;
+
+    @Before
+    public void setUp() {
+        resources = new Resources__();
+    }
+
+    @Test
+    public void testVolume() {
+        Volume_ volume = new Volume_();
+        resources.setVolume(volume);
+        assertSame(volume, resources.getVolume());
+
+    }
+
+    @Test
+    public void testtoString() {
+        assertTrue(resources.toString() instanceof String);
+    }
+
+}
