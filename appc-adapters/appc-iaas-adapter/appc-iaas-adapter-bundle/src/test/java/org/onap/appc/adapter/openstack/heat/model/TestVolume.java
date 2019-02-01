5372bd300728ef21ffc67dcedabbfb2dc4856db1
Sudarshan Kumar <sudars19@in.ibm.com>
2019-02-01 13:13:05 +0530

    Volume - Junit Test case added
    Volume - Junit Test case added
    
    Issue-ID: APPC-1372
    Change-Id: I44a262819ddc3f0f3319e320ef3bf01ef8ca1e13
    Signed-off-by: Sudarshan Kumar <sudars19@in.ibm.com>

--- appc-adapters/appc-iaas-adapter/appc-iaas-adapter-bundle/src/test/java/org/onap/appc/adapter/openstack/heat/model/TestVolume.java
+++ appc-adapters/appc-iaas-adapter/appc-iaas-adapter-bundle/src/test/java/org/onap/appc/adapter/openstack/heat/model/TestVolume.java
@@ -3,7 +3,9 @@
 * ONAP : APPC
 * ================================================================================
 * Copyright 2018 TechMahindra
-*=================================================================================
+* ================================================================================
+* Modifications Copyright (c) 2019 IBM
+* ================================================================================
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
@@ -26,22 +28,31 @@ import org.junit.Test;
 
 public class TestVolume {
     private Volume volume;
+    private Properties properties;
 
     @Before
     public void setUp() {
         volume = new Volume();
+        properties = new Properties();
     }
 
     @Test
     public void testGetType() {
         volume.setType("A");
-        assertNotNull(volume.getType());
-        assertEquals(volume.getType(), "A");
+        assertEquals("A",volume.getType());
     }
 
     @Test
     public void testToString_ReturnNonEmptyString() {
-        assertNotEquals(volume.toString(), "");
-        assertNotEquals(volume.toString(), null);
+        assertNotEquals("",volume.toString());
+        assertNotEquals(null,volume.toString());
+    }
+
+    @Test
+    public void testGetProperties() {
+        properties.setSize(2);
+        volume.setProperties(properties);
+        assertEquals(2, volume.getProperties().getSize());
+        assertSame(properties, volume.getProperties());
     }
 }
