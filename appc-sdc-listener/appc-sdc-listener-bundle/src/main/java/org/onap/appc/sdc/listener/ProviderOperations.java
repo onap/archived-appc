d3a5a154ed46491e06ba8e47a7dfffb8833d7d0c
Balaji, Ramya (rb111y) <rb111y@att.com>
2019-02-04 07:57:11 -0500

    Sdc Listener Platform hardening changes
    Issue-ID: APPC-1382
    Change-Id: Ib4928843a8f169d856aaa1afa49fcfa127ba4990
    Signed-off-by: Balaji, Ramya (rb111y) <rb111y@att.com>

--- appc-sdc-listener/appc-sdc-listener-bundle/src/main/java/org/onap/appc/sdc/listener/ProviderOperations.java
+++ appc-sdc-listener/appc-sdc-listener-bundle/src/main/java/org/onap/appc/sdc/listener/ProviderOperations.java
@@ -2,22 +2,22 @@
  * ============LICENSE_START=======================================================
  * ONAP : APPC
  * ================================================================================
- * Copyright (C) 2017-2018 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2017-2019 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Copyright (C) 2017 Amdocs
  * =============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * ============LICENSE_END=========================================================
  */
 
@@ -69,6 +69,7 @@ public class ProviderOperations {
     private static final EELFLogger LOG = EELFManager.getInstance().getLogger(ProviderOperations.class);
 
     private static String basic_auth;
+    private static URL defaultUrl;
 
     public static ProviderResponse post(URL url, String json, Map<String, String> adtl_headers) throws APPCException {
         if (json == null) {
@@ -133,6 +134,18 @@ public class ProviderOperations {
         return basic_auth;
     }
 
+    /**
+     * Sets the default Provider URL to the provided URL. If the entry is null then sets to null.
+     *
+     * @param URL The URL
+     */
+    public static void setDefaultUrl(URL URL) {
+        if (URL != null) {
+            defaultUrl = URL;
+        } else {
+            defaultUrl = null;
+        }
+    }
     @SuppressWarnings("deprecation")
     private static HttpClient getHttpClient(URL url) throws APPCException {
         HttpClient client;
