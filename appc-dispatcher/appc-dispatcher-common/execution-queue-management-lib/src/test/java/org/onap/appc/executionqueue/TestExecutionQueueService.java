cc48824b82792d240fb62862f167a0d991fa5eab
Joss Armstrong <joss.armstrong@ericsson.com>
2019-02-06 18:31:58 +0000

    Improve testing in ExecutionQueueServiceImpl
    Increased coverage to 100%
    
    Issue-ID: APPC-1396
    Change-Id: Ie9c2bd14d83f43e4fc41dacfd45a596ec46b1d95
    Signed-off-by: Joss Armstrong <joss.armstrong@ericsson.com>

--- appc-dispatcher/appc-dispatcher-common/execution-queue-management-lib/src/test/java/org/onap/appc/executionqueue/TestExecutionQueueService.java
+++ appc-dispatcher/appc-dispatcher-common/execution-queue-management-lib/src/test/java/org/onap/appc/executionqueue/TestExecutionQueueService.java
@@ -5,6 +5,8 @@
  * Copyright (C) 2017-2018 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Copyright (C) 2017 Amdocs
+ * ================================================================================
+ * Modifications Copyright (C) 2019 Ericsson
  * =============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,9 +25,14 @@
 
 package org.onap.appc.executionqueue;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.times;
+import java.util.concurrent.TimeUnit;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mockito;
@@ -36,13 +43,11 @@ import org.onap.appc.executionqueue.impl.ExecutionQueueServiceImpl;
 import org.onap.appc.executionqueue.impl.QueueManager;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import java.util.concurrent.TimeUnit;
-
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.times;
-
 @RunWith(PowerMockRunner.class)
 public class TestExecutionQueueService {
+
+    @Rule
+    public ExpectedException expectedEx = ExpectedException.none();
 
     @InjectMocks
     ExecutionQueueServiceImpl service;
@@ -65,5 +70,14 @@ public class TestExecutionQueueService {
         } catch (APPCException e) {
             Assert.fail(e.toString());
         }
+    }
+
+    @Test
+    public void testPutMessageExceptionFlow() throws APPCException {
+        QueueManager queueManager = Mockito.mock(QueueManager.class);
+        Mockito.when(queueManager.enqueueTask(Mockito.any())).thenReturn(false);
+        service.setQueueManager(queueManager);
+        expectedEx.expect(APPCException.class);
+        service.putMessage(new Thread(), 1, TimeUnit.MILLISECONDS);
     }
 }
