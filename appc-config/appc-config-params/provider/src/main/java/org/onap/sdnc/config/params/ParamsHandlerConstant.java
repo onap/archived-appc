676fde0f66223f04dec44a1512f7aaef5e9d71a5
Sudarshan Kumar <sudars19@in.ibm.com>
2019-02-07 15:39:53 +0530

    Vulnerabilities Fix - ParamsHandlerConstant.java
    Vulnerabilities Fix - Added final to all the constant variables
    
    Issue-ID: APPC-1399
    Change-Id: I20ce67627f3d1752c4896cfa42e0f4cb88bf8059
    Signed-off-by: Sudarshan Kumar <sudars19@in.ibm.com>

--- appc-config/appc-config-params/provider/src/main/java/org/onap/sdnc/config/params/ParamsHandlerConstant.java
+++ appc-config/appc-config-params/provider/src/main/java/org/onap/sdnc/config/params/ParamsHandlerConstant.java
@@ -5,7 +5,9 @@
  * Copyright (C) 2017-2018 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Copyright (C) 2017 Amdocs
- * =============================================================================
+ * ================================================================================
+ * Modifications Copyright (c) 2019 IBM
+ * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -25,30 +27,30 @@ package org.onap.sdnc.config.params;
 
 public class ParamsHandlerConstant {
 
-    public static String STRING_ENCODING = "utf-8";
-    public static String Y = "Y";
-    public static String N = "N";
-    public static String DATA_TYPE_TEXT = "TEXT";
-    public static String DATA_TYPE_JSON = "JSON";
-    public static String DATA_TYPE_XML = "XML";
-    public static String DATA_TYPE_SQL = "SQL";
+    public static final String STRING_ENCODING = "utf-8";
+    public static final String Y = "Y";
+    public static final String N = "N";
+    public static final String DATA_TYPE_TEXT = "TEXT";
+    public static final String DATA_TYPE_JSON = "JSON";
+    public static final String DATA_TYPE_XML = "XML";
+    public static final String DATA_TYPE_SQL = "SQL";
 
 
-    public static String INPUT_PARAM_PD_CONTENT = "pdContent";
-    public static String INPUT_PARAM_SYSTEM_NAME = "systemName";
-    public static String INPUT_PARAM_JSON_DATA = "jsonData";
-    public static String INPUT_PARAM_MERGE__JSON_DATA = "mergeJsonData";
+    public static final String INPUT_PARAM_PD_CONTENT = "pdContent";
+    public static final String INPUT_PARAM_SYSTEM_NAME = "systemName";
+    public static final String INPUT_PARAM_JSON_DATA = "jsonData";
+    public static final String INPUT_PARAM_MERGE__JSON_DATA = "mergeJsonData";
 
 
-    public static String INPUT_PARAM_REQUEST_DATA = "requestData";
-    public static String INPUT_PARAM_RESPONSE_PRIFIX = "responsePrefix";
-    public static String OUTPUT_PARAM_CONFIGURATION_PARAMETER = "configuration-parameters";
+    public static final String INPUT_PARAM_REQUEST_DATA = "requestData";
+    public static final String INPUT_PARAM_RESPONSE_PRIFIX = "responsePrefix";
+    public static final String OUTPUT_PARAM_CONFIGURATION_PARAMETER = "configuration-parameters";
 
 
 
-    public static String OUTPUT_PARAM_STATUS = "status";
-    public static String OUTPUT_PARAM_ERROR_MESSAGE = "error-message";
-    public static String OUTPUT_STATUS_SUCCESS = "success";
-    public static String OUTPUT_STATUS_FAILURE = "failure";
+    public static final String OUTPUT_PARAM_STATUS = "status";
+    public static final String OUTPUT_PARAM_ERROR_MESSAGE = "error-message";
+    public static final String OUTPUT_STATUS_SUCCESS = "success";
+    public static final String OUTPUT_STATUS_FAILURE = "failure";
 
 }
