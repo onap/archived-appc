621baa308b6ad0d3bb7b0f99c8c6e620301d111b
Driptaroop Das <driptaroop.das@in.ibm.com>
2019-02-10 21:34:31 +0530

    Junit for AppcDataServiceActivator
    Junit for AppcDataServiceActivator
    
    Issue-ID: APPC-1411
    Change-Id: Iddccff7afd8960d82e2dec8217d385810a4b80c3
    Signed-off-by: Driptaroop Das <driptaroop.das@in.ibm.com>

---
+++ appc-config/appc-data-services/provider/src/test/java/org/onap/appc/data/services/AppcDataServiceActivatorTest.java
@@ -0,0 +1,79 @@
+/*
+ * ============LICENSE_START=======================================================
+ * ONAP : APPC
+ * ================================================================================
+ * Copyright 2019 IBM
+ *=================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.appc.data.services;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.onap.appc.data.services.node.ConfigResourceNode;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+import java.lang.reflect.Field;
+import java.util.Dictionary;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AppcDataServiceActivatorTest {
+
+    AppcDataServiceActivator appcDataServiceActivator;
+    Field registrationsField;
+    @Mock
+    BundleContext ctx;
+    @Mock
+    ServiceRegistration serviceRegistration;
+
+    @Before
+    public void setUp() throws Exception {
+        this.appcDataServiceActivator = new AppcDataServiceActivator();
+        this.registrationsField = AppcDataServiceActivator.class.getDeclaredField("registrations");
+        registrationsField.setAccessible(true);
+        MockitoAnnotations.initMocks(this);
+        when(ctx.registerService(anyString(), any(ConfigResourceNode.class), any(Dictionary.class)))
+                .thenReturn(serviceRegistration);
+    }
+
+    @Test
+    public void start() throws Exception {
+        appcDataServiceActivator.start(ctx);
+        List<ServiceRegistration> registrations = (List<ServiceRegistration>) registrationsField.get(appcDataServiceActivator);
+        assertEquals(1, registrations.size());
+        assertThat(registrations.get(0), is(serviceRegistration));
+    }
+
+    @Test
+    public void stop() throws Exception {
+        appcDataServiceActivator.start(ctx);
+        appcDataServiceActivator.stop(ctx);
+        verify(serviceRegistration).unregister();
+    }
+}
