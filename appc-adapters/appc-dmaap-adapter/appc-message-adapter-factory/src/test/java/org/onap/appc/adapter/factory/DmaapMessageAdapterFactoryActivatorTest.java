28ebd9cb512f41600eed2beeb0f6df31fedf19d7
Joss Armstrong <joss.armstrong@ericsson.com>
2019-02-12 20:07:10 +0000

    Test coverage DmaapMessageAdapterFactoryActivator
    Increased coverage from 0% to 100%
    
    Issue-ID: APPC-1422
    Change-Id: I246ddffe3d84682ddb37ca5e94abd0c7f5c7c312
    Signed-off-by: Joss Armstrong <joss.armstrong@ericsson.com>

--- appc-adapters/appc-dmaap-adapter/appc-message-adapter-factory/src/test/java/org/onap/appc/adapter/factory/DmaapMessageAdapterFactoryActivatorTest.java
+++ appc-adapters/appc-dmaap-adapter/appc-message-adapter-factory/src/test/java/org/onap/appc/adapter/factory/DmaapMessageAdapterFactoryActivatorTest.java
@@ -1,48 +1,49 @@
-/*-
+/*
  * ============LICENSE_START=======================================================
  * ONAP : APPC
  * ================================================================================
- * Copyright (C) 2017-2018 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2019 Ericsson
  * ================================================================================
- * Copyright (C) 2017 Amdocs
- * ================================================================================
- * Modifications Copyright (C) 2019 Ericsson
- * =============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * ============LICENSE_END=========================================================
  */
+
 package org.onap.appc.adapter.factory;
 
-import org.onap.appc.adapter.message.MessageAdapterFactory;
-import org.osgi.framework.BundleActivator;
+import org.junit.Test;
+import org.mockito.Mockito;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
+import org.powermock.reflect.Whitebox;
 
-public class DmaapMessageAdapterFactoryActivator implements BundleActivator {
-    private ServiceRegistration registration;
 
-    @Override
-    public void start(BundleContext context) throws Exception {
-        registration = context.registerService(
-                MessageAdapterFactory.class.getName(),
-                new DmaapMessageAdapterFactoryImpl(),
-                null);
+public class DmaapMessageAdapterFactoryActivatorTest {
+
+    @Test
+    public void testStart() throws Exception {
+        DmaapMessageAdapterFactoryActivator activator = new DmaapMessageAdapterFactoryActivator();
+        BundleContext context = Mockito.mock(BundleContext.class);
+        activator.start(context);
+        Mockito.verify(context).registerService(Mockito.anyString(), Mockito.any(), Mockito.any());
     }
 
-    @Override
-    public void stop(BundleContext context) throws Exception {
-        registration.unregister();
+    @Test
+    public void testStop() throws Exception {
+        DmaapMessageAdapterFactoryActivator activator = new DmaapMessageAdapterFactoryActivator();
+        ServiceRegistration registration = Mockito.mock(ServiceRegistration.class);
+        Whitebox.setInternalState(activator, "registration", registration);
+        activator.stop(Mockito.mock(BundleContext.class));
+        Mockito.verify(registration).unregister();
     }
-
 }
