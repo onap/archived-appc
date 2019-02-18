d6f7479531783842acab0ce020b747a6e82bc005
Patrick Brady <patrick.brady@att.com>
2019-02-18 01:39:30 -0800

    Correct line indent spacing
    The indent spacing was incorrectly changed to 2 spaces
    instead of 4 by https://gerrit.onap.org/r/#/c/78396/
    The incorrect spacing change was causing merge conflicts
    in another commit.
    
    Change-Id: I6693b3bfaa5685318c8ada5d7984235c788b9504
    Signed-off-by: Patrick Brady <patrick.brady@att.com>
    Issue-ID: APPC-1420

--- appc-inbound/appc-design-services/provider/src/test/java/org/onap/appc/design/services/impl/TestDesignServicesImpl.java
+++ appc-inbound/appc-design-services/provider/src/test/java/org/onap/appc/design/services/impl/TestDesignServicesImpl.java
@@ -42,108 +42,108 @@ import org.powermock.modules.junit4.PowerMockRunner;
 @PrepareForTest({DesignDBService.class,XInterfaceService.class,XResponseProcessor.class})
 public class TestDesignServicesImpl {
 
-  private DesignServicesImpl designServicesImpl;
-  private DbserviceInput dbserviceInput;
-  private XinterfaceserviceInput xinterfaceserviceInput;
-  private ValidatorInput validatorInput;
-  private DesignRequest designRequest;
-  private DesignDBService designDBService;
-  private XInterfaceService xInterfaceService;
-  private XResponseProcessor xResponseProcessor;
+    private DesignServicesImpl designServicesImpl;
+    private DbserviceInput dbserviceInput;
+    private XinterfaceserviceInput xinterfaceserviceInput;
+    private ValidatorInput validatorInput;
+    private DesignRequest designRequest;
+    private DesignDBService designDBService;
+    private XInterfaceService xInterfaceService;
+    private XResponseProcessor xResponseProcessor;
 
 
-  @Before
-  public void setUp() throws Exception {
-    designServicesImpl = new DesignServicesImpl();
-    dbserviceInput = Mockito.mock(DbserviceInput.class);
-    designRequest = Mockito.mock(DesignRequest.class);
-    designDBService = Mockito.mock(DesignDBService.class);
-    xInterfaceService = Mockito.mock(XInterfaceService.class);
-    PowerMockito.mockStatic(DesignDBService.class);
-    PowerMockito.mockStatic(XResponseProcessor.class);
-    PowerMockito.mockStatic(XInterfaceService.class);
-    xResponseProcessor = PowerMockito.mock(XResponseProcessor.class);
-    xinterfaceserviceInput = Mockito.mock(XinterfaceserviceInput.class);
-    validatorInput = Mockito.mock(ValidatorInput.class);
-    when(dbserviceInput.getDesignRequest()).thenReturn(designRequest);
-    when(xinterfaceserviceInput.getDesignRequest()).thenReturn(designRequest);
-    when(validatorInput.getDesignRequest()).thenReturn(designRequest);
-    when(designRequest.getRequestId()).thenReturn("123");
-    when(designRequest.getAction()).thenReturn("getDesigns");
-    when(designRequest.getPayload()).thenReturn("{\"artifact-contents\":\"\",\"userID\":\"user\"}");
-  }
+    @Before
+    public void setUp() throws Exception {
+        designServicesImpl = new DesignServicesImpl();
+        dbserviceInput = Mockito.mock(DbserviceInput.class);
+        designRequest = Mockito.mock(DesignRequest.class);
+        designDBService = Mockito.mock(DesignDBService.class);
+        xInterfaceService = Mockito.mock(XInterfaceService.class);
+        PowerMockito.mockStatic(DesignDBService.class);
+        PowerMockito.mockStatic(XResponseProcessor.class);
+        PowerMockito.mockStatic(XInterfaceService.class);
+        xResponseProcessor = PowerMockito.mock(XResponseProcessor.class);
+        xinterfaceserviceInput = Mockito.mock(XinterfaceserviceInput.class);
+        validatorInput = Mockito.mock(ValidatorInput.class);
+        when(dbserviceInput.getDesignRequest()).thenReturn(designRequest);
+        when(xinterfaceserviceInput.getDesignRequest()).thenReturn(designRequest);
+        when(validatorInput.getDesignRequest()).thenReturn(designRequest);
+        when(designRequest.getRequestId()).thenReturn("123");
+        when(designRequest.getAction()).thenReturn("getDesigns");
+        when(designRequest.getPayload()).thenReturn("{\"artifact-contents\":\"\",\"userID\":\"user\"}");
+    }
 
-  @Test
-  public void testDbService() throws Exception {
-    PowerMockito.when(DesignDBService.initialise()).thenReturn(designDBService);
-    when(designDBService.execute(eq("getDesigns"), anyString(), eq("123"))).thenReturn("success");
-    assertEquals("400",
-        designServicesImpl.dbservice(dbserviceInput).get().getResult().getStatus().getCode());
-  }
+    @Test
+    public void testDbService() throws Exception {
+        PowerMockito.when(DesignDBService.initialise()).thenReturn(designDBService);
+        when(designDBService.execute(eq("getDesigns"), anyString(), eq("123"))).thenReturn("success");
+        assertEquals("400",
+                designServicesImpl.dbservice(dbserviceInput).get().getResult().getStatus().getCode());
+    }
 
-  @Test
-  public void testDbServiceWithException() throws Exception {
-    PowerMockito.when(DesignDBService.initialise()).thenReturn(designDBService);
-    when(designDBService.execute(eq("getDesigns"), anyString(), eq("123")))
+    @Test
+    public void testDbServiceWithException() throws Exception {
+        PowerMockito.when(DesignDBService.initialise()).thenReturn(designDBService);
+        when(designDBService.execute(eq("getDesigns"), anyString(), eq("123")))
         .thenThrow(new Exception());
-    assertEquals("401",
-        designServicesImpl.dbservice(dbserviceInput).get().getResult().getStatus().getCode());
-  }
+        assertEquals("401",
+                designServicesImpl.dbservice(dbserviceInput).get().getResult().getStatus().getCode());
+    }
 
-  @Test
-  public void testXinterfaceservice() throws Exception {
-    PowerMockito.when(XInterfaceService.getInstance()).thenReturn(xInterfaceService);
-    PowerMockito.when(XResponseProcessor.getInstance()).thenReturn(xResponseProcessor);
-    when(xResponseProcessor.parseResponse(anyString(), eq("getDesigns"))).thenReturn("success");
-    assertEquals("400", designServicesImpl.xinterfaceservice(xinterfaceserviceInput).get()
-        .getResult().getStatus().getCode());
-  }
-  
-  @Test
-  public void testXinterfaceserviceWithException() throws Exception {
-    PowerMockito.when(XInterfaceService.getInstance()).thenReturn(xInterfaceService);
-    PowerMockito.when(XResponseProcessor.getInstance()).thenReturn(xResponseProcessor);
-    when(xResponseProcessor.parseResponse(anyString(), eq("getDesigns"))).thenThrow(new Exception());
-    assertEquals("401", designServicesImpl.xinterfaceservice(xinterfaceserviceInput).get()
-        .getResult().getStatus().getCode());
-  }
+    @Test
+    public void testXinterfaceservice() throws Exception {
+        PowerMockito.when(XInterfaceService.getInstance()).thenReturn(xInterfaceService);
+        PowerMockito.when(XResponseProcessor.getInstance()).thenReturn(xResponseProcessor);
+        when(xResponseProcessor.parseResponse(anyString(), eq("getDesigns"))).thenReturn("success");
+        assertEquals("400", designServicesImpl.xinterfaceservice(xinterfaceserviceInput).get()
+                .getResult().getStatus().getCode());
+    }
 
-  @Test
-  public void testValidatorJson() throws Exception {
-    when(designRequest.getDataType()).thenReturn("JSON");
-    assertEquals("400",
-        designServicesImpl.validator(validatorInput).get().getResult().getStatus().getCode());
-  }
+    @Test
+    public void testXinterfaceserviceWithException() throws Exception {
+        PowerMockito.when(XInterfaceService.getInstance()).thenReturn(xInterfaceService);
+        PowerMockito.when(XResponseProcessor.getInstance()).thenReturn(xResponseProcessor);
+        when(xResponseProcessor.parseResponse(anyString(), eq("getDesigns"))).thenThrow(new Exception());
+        assertEquals("401", designServicesImpl.xinterfaceservice(xinterfaceserviceInput).get()
+                .getResult().getStatus().getCode());
+    }
 
-  @Test
-  public void testValidatorXml() throws Exception {
-    when(designRequest.getPayload()).thenReturn("<artifact-contents></artifact-contents>");
-    when(designRequest.getDataType()).thenReturn("XML");
-    assertEquals("400",
-        designServicesImpl.validator(validatorInput).get().getResult().getStatus().getCode());
-  }
+    @Test
+    public void testValidatorJson() throws Exception {
+        when(designRequest.getDataType()).thenReturn("JSON");
+        assertEquals("400",
+                designServicesImpl.validator(validatorInput).get().getResult().getStatus().getCode());
+    }
 
-  @Test
-  public void testValidatorYaml() throws Exception {
-    when(designRequest.getPayload()).thenReturn("artifact-contents: 34843");
-    when(designRequest.getDataType()).thenReturn("YAML");
-    assertEquals("400",
-        designServicesImpl.validator(validatorInput).get().getResult().getStatus().getCode());
-  }
+    @Test
+    public void testValidatorXml() throws Exception {
+        when(designRequest.getPayload()).thenReturn("<artifact-contents></artifact-contents>");
+        when(designRequest.getDataType()).thenReturn("XML");
+        assertEquals("400",
+                designServicesImpl.validator(validatorInput).get().getResult().getStatus().getCode());
+    }
 
-  @Test
-  public void testValidatorVelocity() throws Exception {
-    when(designRequest.getPayload()).thenReturn("artifact-contents: 34843");
-    when(designRequest.getDataType()).thenReturn("VELOCITY");
-    assertEquals("400",
-        designServicesImpl.validator(validatorInput).get().getResult().getStatus().getCode());
-  }
+    @Test
+    public void testValidatorYaml() throws Exception {
+        when(designRequest.getPayload()).thenReturn("artifact-contents: 34843");
+        when(designRequest.getDataType()).thenReturn("YAML");
+        assertEquals("400",
+                designServicesImpl.validator(validatorInput).get().getResult().getStatus().getCode());
+    }
 
-  @Test
-  public void testValidatorInvalid() throws Exception {
-    when(designRequest.getDataType()).thenReturn("XYZ");
-    assertEquals("401",
-        designServicesImpl.validator(validatorInput).get().getResult().getStatus().getCode());
-  }
+    @Test
+    public void testValidatorVelocity() throws Exception {
+        when(designRequest.getPayload()).thenReturn("artifact-contents: 34843");
+        when(designRequest.getDataType()).thenReturn("VELOCITY");
+        assertEquals("400",
+                designServicesImpl.validator(validatorInput).get().getResult().getStatus().getCode());
+    }
+
+    @Test
+    public void testValidatorInvalid() throws Exception {
+        when(designRequest.getDataType()).thenReturn("XYZ");
+        assertEquals("401",
+                designServicesImpl.validator(validatorInput).get().getResult().getStatus().getCode());
+    }
 
 }
