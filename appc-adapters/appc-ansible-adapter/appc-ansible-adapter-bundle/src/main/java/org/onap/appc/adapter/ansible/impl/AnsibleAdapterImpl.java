136abdc93529572f42431ed9cd0dcd7aca19cd12
Driptaroop Das <driptaroop.das@in.ibm.com>
2019-02-19 00:11:28 +0530

    Sonar Fixes - AnsibleAdapterImpl.java
    Sonar Fixes - AnsibleAdapterImpl.java
    
    Issue-ID: APPC-1460
    Change-Id: I830c56b49936dc011f4401fcf5d0f9e4be13d700
    Signed-off-by: Driptaroop Das <driptaroop.das@in.ibm.com>

--- appc-adapters/appc-ansible-adapter/appc-ansible-adapter-bundle/src/main/java/org/onap/appc/adapter/ansible/impl/AnsibleAdapterImpl.java
+++ appc-adapters/appc-ansible-adapter/appc-ansible-adapter-bundle/src/main/java/org/onap/appc/adapter/ansible/impl/AnsibleAdapterImpl.java
@@ -5,6 +5,7 @@
  * Copyright (C) 2018-2019 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Copyright (C) 2017 Amdocs
+ * Modifications Copyright (C) 2019 Amdocs
  * =============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,11 +24,8 @@
 
 package org.onap.appc.adapter.ansible.impl;
 
-import java.util.Map;
-import java.util.Properties;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
+import com.att.eelf.configuration.EELFLogger;
+import com.att.eelf.configuration.EELFManager;
 import org.apache.commons.lang.StringUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -36,13 +34,16 @@ import org.onap.appc.adapter.ansible.model.AnsibleMessageParser;
 import org.onap.appc.adapter.ansible.model.AnsibleResult;
 import org.onap.appc.adapter.ansible.model.AnsibleResultCodes;
 import org.onap.appc.adapter.ansible.model.AnsibleServerEmulator;
+import org.onap.appc.encryption.EncryptionTool;
 import org.onap.appc.exceptions.APPCException;
 import org.onap.ccsdk.sli.core.sli.SvcLogicContext;
 import org.onap.ccsdk.sli.core.sli.SvcLogicException;
 
-import com.att.eelf.configuration.EELFLogger;
-import com.att.eelf.configuration.EELFManager;
-import org.onap.appc.encryption.EncryptionTool;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Properties;
 
 /**
  * This class implements the {@link AnsibleAdapter} interface. This interface
@@ -100,10 +101,6 @@ public class AnsibleAdapterImpl implements AnsibleAdapter {
      * The logger to be used
      */
     private static final EELFLogger logger = EELFManager.getInstance().getLogger(AnsibleAdapterImpl.class);
-    /**
-     * Connection object
-     **/
-    private ConnectionBuilder httpClient;
 
     /**
      * Ansible API Message Handlers
@@ -281,8 +278,9 @@ public class AnsibleAdapterImpl implements AnsibleAdapter {
             agentUrl = (String) jsonPayload.remove("AgentUrl");
             user = (String) jsonPayload.remove("User");
             password = (String)jsonPayload.remove(PASSWORD);
-            if(StringUtils.isNotBlank(password))
-            password = EncryptionTool.getInstance().decrypt(password);
+            if(StringUtils.isNotBlank(password)) {
+                password = EncryptionTool.getInstance().decrypt(password);
+            }
             id = jsonPayload.getString("Id");
             timeout = jsonPayload.getString("Timeout");
             if (StringUtils.isBlank(timeout))
@@ -322,10 +320,9 @@ public class AnsibleAdapterImpl implements AnsibleAdapter {
                 doFailure(ctx, testResult.getStatusCode(),
                         "Error posting request. Reason = " + testResult.getStatusMessage());
             }
-            String output = StringUtils.EMPTY;
             code = testResult.getStatusCode();
             message = testResult.getStatusMessage();
-            output = testResult.getOutput();
+            String output = testResult.getOutput();
             ctx.setAttribute(OUTPUT_ATTRIBUTE_NAME, output);
             String serverIp = testResult.getServerIp();
             if (StringUtils.isBlank(serverIp))
@@ -478,7 +475,7 @@ public class AnsibleAdapterImpl implements AnsibleAdapter {
 
         AnsibleResult testResult;
         ConnectionBuilder httpClient = getHttpConn(defaultSocketTimeout, "");
-        if (!testMode) {
+        if (!testMode && null != httpClient) {
             httpClient.setHttpContext(user, password);
             testResult = httpClient.post(agentUrl, payload);
             httpClient.close();
@@ -508,7 +505,7 @@ public class AnsibleAdapterImpl implements AnsibleAdapter {
             ConnectionBuilder httpClient = getHttpConn(defaultSocketTimeout, serverIP);
             logger.info("Querying ansible GetResult URL = " + agentUrl);
 
-            if (!testMode) {
+            if (!testMode && null != httpClient) {
                 httpClient.setHttpContext(user, password);
                 testResult = httpClient.get(agentUrl);
                 httpClient.close();
@@ -523,7 +520,8 @@ public class AnsibleAdapterImpl implements AnsibleAdapter {
             try {
                 Thread.sleep(defaultPollInterval);
             } catch (InterruptedException ex) {
-
+                logger.log(EELFLogger.Level.ERROR, ex.getMessage(), ex);
+                Thread.currentThread().interrupt();
             }
 
         }
