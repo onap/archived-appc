cc2baff0c049030435673249561ff193aa224a10
Jegadeesh Babu <jegabab1@in.ibm.com>
2019-02-20 18:09:40 +0530

    Seperated assignment statement from expression
    Sonar bug fix
    
    Issue-ID: APPC-1468
    Change-Id: I2ff7e01516cf7328357d2a48643947c782ce4fd9
    Signed-off-by: Jegadeesh Babu <jegabab1@in.ibm.com>

--- appc-config/appc-data-services/provider/src/main/java/org/onap/appc/data/services/db/DGGeneralDBService.java
+++ appc-config/appc-data-services/provider/src/main/java/org/onap/appc/data/services/db/DGGeneralDBService.java
@@ -6,6 +6,8 @@
  * ================================================================================
  * Copyright (C) 2017 Amdocs
  * =============================================================================
+ * Modifications Copyright (C) 2019 IBM.
+ * =============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -241,7 +243,8 @@ public class DGGeneralDBService {
 
             Set<String> keySet = ctx.getAttributeKeySet();
             for (String key : keySet) {
-                if (StringUtils.isNotBlank(key) && key.startsWith(prefix = ".")) {
+            	prefix = ".";
+                if (StringUtils.isNotBlank(key) && key.startsWith(prefix)) {
                     ctx.getAttributeKeySet().remove(key);
                 }
             }
