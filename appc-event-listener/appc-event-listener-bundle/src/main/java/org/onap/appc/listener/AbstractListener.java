70ed737df023f8d15067c1f59e9818257b271f87
Jegadeesh Babu <jegabab1@in.ibm.com>
2019-02-20 18:29:09 +0530

    Handled interrupted Exception
    Sonar Bug Fix
    
    Issue-ID: APPC-1469
    Change-Id: I3f1fe6b620e8b01659e1b927b1f5fb667be20648
    Signed-off-by: Jegadeesh Babu <jegabab1@in.ibm.com>

--- appc-event-listener/appc-event-listener-bundle/src/main/java/org/onap/appc/listener/AbstractListener.java
+++ appc-event-listener/appc-event-listener-bundle/src/main/java/org/onap/appc/listener/AbstractListener.java
@@ -6,6 +6,8 @@
  * ================================================================================
  * Copyright (C) 2017 Amdocs
  * =============================================================================
+ * Modifications Copyright (C) 2019 IBM
+ * =============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -93,6 +95,7 @@ public abstract class AbstractListener implements Listener {
             executor.awaitTermination(10, TimeUnit.SECONDS);
         } catch (InterruptedException e) {
             LOG.error("Listener graceful stop() failed", e);
+            Thread.currentThread().interrupt();
         }
         
         // close DMaaP clients
