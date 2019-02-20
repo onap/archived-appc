c31b83bd073b1ac1034a9e72460e552e0e94ec1a
Lathishbabu Ganesan <lathishbabu.ganesan@ericsson.com>
2019-02-20 10:16:29 -0500

    Added test case for Configuration class
    Issue-ID: APPC-1449
    Change-Id: I84bb511d121e3af1426d4422ad3ce520bb5fd29c
    Signed-off-by: Lathishbabu Ganesan <lathishbabu.ganesan@ericsson.com>

--- appc-core/appc-common-bundle/src/test/java/org/onap/appc/configuration/ConfigurationFactoryTest.java
+++ appc-core/appc-common-bundle/src/test/java/org/onap/appc/configuration/ConfigurationFactoryTest.java
@@ -1,15 +1,38 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * Copyright (C) 2019 Ericsson. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
 package org.onap.appc.configuration;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertNotNull;
+import java.util.Properties;
 import org.junit.Test;
 
-import static org.onap.appc.configuration.ConfigurationFactory.getConfiguration;
-
 public class ConfigurationFactoryTest {
-    @Test
-    public void should_returnDefaultConfiguration(){
-        Configuration conf = null;
 
-        Assert.assertTrue(getConfiguration() instanceof DefaultConfiguration);
+    @Test
+    public void testGetConfiguration() {
+        assertNotNull(ConfigurationFactory.getConfiguration(new Object()));
+    }
+
+    @Test
+    public void testGetConfigurationWithProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("key", "value");
+        assertNotNull(ConfigurationFactory.getConfiguration(properties));
     }
 }
