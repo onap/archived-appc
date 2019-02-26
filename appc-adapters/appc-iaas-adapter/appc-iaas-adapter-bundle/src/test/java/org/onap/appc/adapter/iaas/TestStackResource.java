cb0bdc23c3640b1557121fde6fe8a2e4aa52a0ff
Lathishbabu Ganesan <lathishbabu.ganesan@ericsson.com>
2019-02-26 11:47:11 -0500

    Added test case for Stack Resource
    Increased the coverage from 0% to 100%
    
    Issue-ID: APPC-1501
    Change-Id: Idb8d7b6efc7831dc0c6ca8094783cbc84ecfd14e
    Signed-off-by: Lathishbabu Ganesan <lathishbabu.ganesan@ericsson.com>

---
+++ appc-adapters/appc-iaas-adapter/appc-iaas-adapter-bundle/src/test/java/org/onap/appc/adapter/iaas/TestStackResource.java
@@ -0,0 +1,51 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : APPC
+ * ================================================================================
+ * Copyright (C) 2019 Ericsson
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.appc.adapter.iaas;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
+import org.onap.appc.adapter.openstack.heat.StackResource;
+import org.powermock.reflect.internal.WhiteboxImpl;
+import com.woorea.openstack.base.client.OpenStackClient;
+
+public class TestStackResource {
+
+    private StackResource stackResource;
+    private OpenStackClient client;
+
+    @Test
+    public void testShow() {
+        stackResource = new StackResource(client);
+        assertNotNull(stackResource.show("stackName", "123"));
+    }
+
+    @Test
+    public void testShowStack() throws Exception {
+        stackResource = new StackResource(client);
+        StackResource.ShowStack showStack = stackResource.new ShowStack("stackName", "111");
+        StringBuilder path = WhiteboxImpl.getInternalState(showStack, "path");
+        assertEquals("/stacks/stackName/111", path.toString());
+        assertNotNull(stackResource.new ShowStack("stackName", "111"));
+    }
+
+}
