7c9e66f686a9a0c8dce8d6c6a6cd8cc5e6ac829a
Jegadeesh Babu <jegabab1@in.ibm.com>
2019-02-26 15:55:01 +0530

    Handled interrupted exception Rechecked
    Rechecked sonar fix
    
    Issue-ID: APPC-1492
    Change-Id: I8cec7e0e0cb5614e959f6eaa35271f55ddbba24b
    Signed-off-by: Jegadeesh Babu <jegabab1@in.ibm.com>

--- appc-client/client-lib/src/main/java/org/onap/appc/client/impl/core/SyncRequestResponseHandler.java
+++ appc-client/client-lib/src/main/java/org/onap/appc/client/impl/core/SyncRequestResponseHandler.java
@@ -6,6 +6,8 @@
  * ================================================================================
  * Copyright (C) 2017 Amdocs
  * =============================================================================
+ * Modifications Copyright (C) 2019 IBM
+ * =============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -77,6 +79,7 @@ class SyncRequestResponseHandler<T> extends AbstractRequestResponseHandler {
             }
 
         } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
             throw new CoreException(e);
         } finally{
             coreManager.unregisterHandler(corrID);
