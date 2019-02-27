14b8738d2719d6eee695e651fadc1e8466ef0c23
Jegadeesh Babu <jegabab1@in.ibm.com>
2019-02-27 19:11:51 +0530

    Added null check to avoid null pointer exception
    Sonar fix
    
    Issue-ID: APPC-1505
    Change-Id: Iefd8a45aacb6e8de50c16c36fe9ab551401ca417
    Signed-off-by: Jegadeesh Babu <jegabab1@in.ibm.com>

--- appc-adapters/appc-ansible-adapter/appc-ansible-adapter-bundle/src/main/java/org/onap/appc/adapter/ansible/impl/AnsibleAdapterImpl.java
+++ appc-adapters/appc-ansible-adapter/appc-ansible-adapter-bundle/src/main/java/org/onap/appc/adapter/ansible/impl/AnsibleAdapterImpl.java
@@ -316,31 +316,33 @@ public class AnsibleAdapterImpl implements AnsibleAdapter {
             // post the test request
             logger.info("Posting ansible request = " + payload + " to url = " + agentUrl);
             AnsibleResult testResult = postExecRequest(agentUrl, payload, user, password,ctx);
-            logger.info("Received response on ansible post request " + testResult.getStatusMessage());
-            // Process if HTTP was successful
-            if (testResult.getStatusCode() == 200) {
-                testResult = messageProcessor.parsePostResponse(testResult.getStatusMessage());
-            } else {
-                doFailure(ctx, testResult.getStatusCode(),
-                        "Error posting request. Reason = " + testResult.getStatusMessage());
-            }
-            String output = StringUtils.EMPTY;
-            code = testResult.getStatusCode();
-            message = testResult.getStatusMessage();
-            output = testResult.getOutput();
-            ctx.setAttribute(OUTPUT_ATTRIBUTE_NAME, output);
-            String serverIp = testResult.getServerIp();
-            if (StringUtils.isBlank(serverIp))
-                ctx.setAttribute("ServerIP", serverIp);
-            else
-                ctx.setAttribute("ServerIP", "");
-            // Check status of test request returned by Agent
-            if (code == AnsibleResultCodes.PENDING.getValue()) {
-                logger.info(String.format("Submission of Test %s successful.", playbookName));
-                // test request accepted. We are in asynchronous case
-            } else {
-                doFailure(ctx, code, "Request for execution of playbook rejected. Reason = " + message);
-            }
+			if (testResult != null) {
+				logger.info("Received response on ansible post request " + testResult.getStatusMessage());
+				// Process if HTTP was successful
+				if (testResult.getStatusCode() == 200) {
+					testResult = messageProcessor.parsePostResponse(testResult.getStatusMessage());
+				} else {
+					doFailure(ctx, testResult.getStatusCode(),
+							"Error posting request. Reason = " + testResult.getStatusMessage());
+				}
+				String output = StringUtils.EMPTY;
+				code = testResult.getStatusCode();
+				message = testResult.getStatusMessage();
+				output = testResult.getOutput();
+				ctx.setAttribute(OUTPUT_ATTRIBUTE_NAME, output);
+				String serverIp = testResult.getServerIp();
+				if (StringUtils.isBlank(serverIp))
+					ctx.setAttribute("ServerIP", serverIp);
+				else
+					ctx.setAttribute("ServerIP", "");
+				// Check status of test request returned by Agent
+				if (code == AnsibleResultCodes.PENDING.getValue()) {
+					logger.info(String.format("Submission of Test %s successful.", playbookName));
+					// test request accepted. We are in asynchronous case
+				} else {
+					doFailure(ctx, code, "Request for execution of playbook rejected. Reason = " + message);
+				}
+			}
         } catch (APPCException e) {
             logger.error(APPC_EXCEPTION_CAUGHT, e);
             doFailure(ctx, AnsibleResultCodes.UNKNOWN_EXCEPTION.getValue(),
