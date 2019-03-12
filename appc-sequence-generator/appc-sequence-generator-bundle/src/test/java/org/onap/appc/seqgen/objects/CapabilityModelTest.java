a9c825cae70be3956866da63b8749d731f8df848
Sandeep J <sandeejh@in.ibm.com>
2019-03-12 15:57:05 +0530

    added file to test CapabilityModel.java
    to increase code coverage
    
    Issue-ID: APPC-1086
    Change-Id: I47987b8d304db6447fc34ce869f5b16f10c6c6e9
    Signed-off-by: Sandeep J <sandeejh@in.ibm.com>

---
+++ appc-sequence-generator/appc-sequence-generator-bundle/src/test/java/org/onap/appc/seqgen/objects/CapabilityModelTest.java
@@ -0,0 +1,58 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : APPC
+ * ================================================================================
+ * Copyright (C) 2019 IBM.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.appc.seqgen.objects;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CapabilityModelTest {
+
+    private CapabilityModel capabilityModel;
+
+    private List<String> vnfCapabilities = new ArrayList<>();
+    private List<String> vfModuleCapabilities = new ArrayList<>();
+    private Map<String, List<String>> vmCapabilities = new HashMap<>();
+    private List<String> vnfcCapabilities = new ArrayList<>();
+
+    @Before
+    public void setUp() {
+        capabilityModel = new CapabilityModel(vnfCapabilities, vfModuleCapabilities, vmCapabilities, vnfcCapabilities);
+    }
+
+    @Test
+    public void testVnfCapabilities() {
+        assertEquals(vnfCapabilities, capabilityModel.getVnfCapabilities());
+    }
+
+    @Test
+    public void testVfModuleCapabilities() {
+        assertEquals(vfModuleCapabilities, capabilityModel.getVfModuleCapabilities());
+    }
+
+}
