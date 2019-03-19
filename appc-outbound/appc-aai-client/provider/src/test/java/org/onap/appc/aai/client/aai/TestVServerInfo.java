d3f061bf257c5b431bac30308875e5bafeaef130
Sandeep J <sandeejh@in.ibm.com>
2019-03-19 13:52:55 +0530

    added file to test VServerInfo.java
    to increase code coverage
    
    Issue-ID: APPC-1086
    Change-Id: Id430a90d0a186c9a065f514efd316af300f690a0
    Signed-off-by: Sandeep J <sandeejh@in.ibm.com>

---
+++ appc-outbound/appc-aai-client/provider/src/test/java/org/onap/appc/aai/client/aai/TestVServerInfo.java
@@ -0,0 +1,66 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : APPC
+ * ================================================================================
+ * Copyright (C) 2019 IBM.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.appc.aai.client.aai;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestVServerInfo {
+
+    VServerInfo vServerInfo;
+
+    @Before
+    public void setUp() throws MissingParameterException {
+        Map<String, String> params = new HashMap<>();
+        params.put("vserverId", "testVserverId");
+        params.put("tenantId", "testTenantId");
+        params.put("cloudOwner", "testCloudOwner");
+        params.put("cloudRegionId", "testCloudRegionId");
+        vServerInfo = new VServerInfo(params);
+    }
+
+    @Test
+    public void testVserverId() {
+        assertEquals("testVserverId", vServerInfo.getVserverId());
+    }
+
+    @Test
+    public void testTenantId() {
+        assertEquals("testTenantId", vServerInfo.getTenantId());
+    }
+
+    @Test
+    public void testCloudOwner() {
+        assertEquals("testCloudOwner", vServerInfo.getCloudOwner());
+    }
+
+    @Test
+    public void testCloudRegionId() {
+        assertEquals("testCloudRegionId", vServerInfo.getCloudRegionId());
+    }
+
+}
