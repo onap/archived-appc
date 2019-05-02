f9b80fb2e13cad0da7847bb667c2fef710bc9859
Patrick Brady <patrick.brady@att.com>
2019-05-02 16:41:24 -0700

    Add method to escape json strings
    All backslash escape characters are being stripped out of strings.
    They need to be added back to strings containing json data.
    
    Change-Id: Ic8d9ba95da904b96f65598752133971f8c324694
    Signed-off-by: Patrick Brady <patrick.brady@att.com>
    Issue-ID: APPC-1576

---
+++ appc-config/appc-config-generator/provider/src/test/java/org/onap/sdnc/config/generator/tool/TestJSONTool.java
@@ -0,0 +1,42 @@
+package org.onap.sdnc.config.generator.tool;
+
+import org.codehaus.jettison.json.JSONException;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestJSONTool {
+
+    @Test
+    public void testEscapeInternalJson() {
+        String testData = "{\"test1\":\"value1\",\"test2\":\"{\"key1\":\"value\"}\"}";
+        String expectedOutput = "{\"test1\":\"value1\",\"test2\":\"{\\\"key1\\\":\\\"value\\\"}\"}";
+        try {
+            Assert.assertEquals(expectedOutput, JSONTool.escapeInternalJson(testData));
+        } catch (JSONException e) {
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void testEscapeInternalJson_alreadyEscaped() {
+        String testData = "{\"test1\":\"value1\",\"test2\":\"{\\\"key1\\\":\\\"value\\\"}\"}";
+        String expectedOutput = "{\"test1\":\"value1\",\"test2\":\"{\\\"key1\\\":\\\"value\\\"}\"}";
+        try {
+            Assert.assertEquals(expectedOutput, JSONTool.escapeInternalJson(testData));
+        } catch (JSONException e) {
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void testEscapeInternalJson_withNewLines() {
+        String testData = "{\"test1\":\"value1\",\"test2\":\"\n{\"key1\":\"value\"\n}\"}";
+        String expectedOutput = "{\"test1\":\"value1\",\"test2\":\"\n{\\\"key1\\\":\\\"value\\\"\n}\"}";
+        try {
+            Assert.assertEquals(expectedOutput, JSONTool.escapeInternalJson(testData));
+        } catch (JSONException e) {
+            Assert.fail();
+        }
+    }
+
+}
