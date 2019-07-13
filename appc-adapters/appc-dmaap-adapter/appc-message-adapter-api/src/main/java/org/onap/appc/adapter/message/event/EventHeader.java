d50893886ca55b68c8730125d1ce328fe5e0261c
jegadeeshbabu1 <jegabab1@in.ibm.com>
2019-07-13 19:56:09 +0530

    Marked classes serializable
    Marked classes serializable
    
    Issue-ID: APPC-1635
    Change-Id: I12230dde2bb9ab31ae023510792fef6498888a7c
    Signed-off-by: jegadeeshbabu1 <jegabab1@in.ibm.com>

--- appc-adapters/appc-dmaap-adapter/appc-message-adapter-api/src/main/java/org/onap/appc/adapter/message/event/EventHeader.java
+++ appc-adapters/appc-dmaap-adapter/appc-message-adapter-api/src/main/java/org/onap/appc/adapter/message/event/EventHeader.java
@@ -6,6 +6,8 @@
  * ================================================================================
  * Copyright (C) 2017 Amdocs
  * =============================================================================
+ * Modifications Copyright (C) 2019 IBM
+ * =============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -23,10 +25,13 @@
 
 package org.onap.appc.adapter.message.event;
 
+import java.io.Serializable;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class EventHeader {
+public class EventHeader implements Serializable {
 
+	private static final long serialVersionUID = 1L;
     @JsonProperty("eventTime")
     private final String eventTime;
 
