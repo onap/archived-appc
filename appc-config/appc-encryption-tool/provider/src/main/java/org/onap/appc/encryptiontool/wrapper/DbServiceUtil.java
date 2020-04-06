72abd31acb5a1f5fd3f8946fc5a78a539f5c340d
Rupinder <rupinsi1@in.ibm.com>
2020-04-06 16:51:06 +0530

    Lowered code smells
    Removed unused imports
    
    Issue-ID: APPC-1859
    Change-Id: Idcd49f4b9c0ee28ec46ccbb298d957c656900ad8
    Signed-off-by: Rupinder<rupinsi1@in.ibm.com>

--- appc-config/appc-encryption-tool/provider/src/main/java/org/onap/appc/encryptiontool/wrapper/DbServiceUtil.java
+++ appc-config/appc-encryption-tool/provider/src/main/java/org/onap/appc/encryptiontool/wrapper/DbServiceUtil.java
@@ -26,7 +26,6 @@ import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.ArrayList;
 import java.util.Properties;
 import javax.sql.rowset.CachedRowSet;
 import org.onap.ccsdk.sli.core.dblib.DBResourceManager;
